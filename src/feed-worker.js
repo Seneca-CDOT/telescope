c4317f203badc24b6b781f01fa11fee7e13b6842
UltimaBGD <ultimabgd@gmail.com>
2019-11-05 01:42:37 -0500

    Add ESLint with Airbnb style

--- src/feed-worker.js
+++ src/feed-worker.js
@@ -1,14 +1,15 @@
-const feedQueue = require('./feed-queue');
 const bent = require('bent');
+const feedQueue = require('./feed-queue');
+
 const request = bent('string');
 
-exports.start = function() {
+exports.start = function () {
   // Start processing jobs from the feed queue...
   feedQueue.process(async (job) => {
     const { url } = job.data;
     console.log(`Processing job - ${url}`);
 
     // For now, just get the feed data and dump to the console
-    return request(url).then(data => console.log(data + '\n\n'))
-  });  
+    return request(url).then((data) => console.log(`${data}\n\n`));
+  });
 };
