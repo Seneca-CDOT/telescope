259b31c11f926423304437065cecd538abd8b11e
Jatin Arora <jaarora45@gmaill.com>
2019-11-06 21:03:48 -0500

    Fixed Typo in comments

--- src/sentiment-analysis.js
+++ src/sentiment-analysis.js
@@ -1,7 +1,7 @@
 /* This file contains the code for analyzing blog posts text to identify the
  * negative or positve words being used in a post and return a summary of it
  * along with a score. The file uses a node module called sentiment to implement
- * the functionality of analyzing text of blogs. The function accepts striped
+ * the functionality of analyzing text of blogs. The function accepts stripped
  * HTML text as parameters i.e text containg no tags, and returns a promise
  * object whcih contains the result
 */
