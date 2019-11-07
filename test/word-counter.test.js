589a3dcf473b9b698291e89a9cd6a7f22fd95655
Anton Biriukov <birtony99@gmail.com>
2019-11-07 11:33:49 -0500

    Implemented basic word counter & test (#17)

---
+++ test/word-counter.test.js
@@ -0,0 +1,5 @@
+const { wordCounter } = require('../src/word-counter');
+
+test('counts the number of words in a 5-word long sentence', () => wordCounter('one two three four five').then((data) => {
+  expect(data).toBe(5);
+}));
