11c91cd604a8bc685e84c71791daecaf05cb2005
Luca Cataldo <lcataldo@myseneca.ca>
2019-11-07 20:57:35 -0500

    fixes #88

--- src/feed-worker.js
+++ src/feed-worker.js
@@ -1,14 +1,24 @@
 const bent = require('bent');
+const fs = require('fs');
 const feedQueue = require('./feed-queue');
 
 const request = bent('string');
 
 exports.start = function () {
+  // clears data in data.xml for writing
+  fs.writeFileSync('tempData.xml', '');
   // Start processing jobs from the feed queue...
   feedQueue.process(async (job) => {
     const { url } = job.data;
     console.log(`Processing job - ${url}`);
-    // For now, just get the feed data and dump to the console
-    return request(url).then((data) => console.log(`${data} + '\n\n'`)).catch((err) => { throw err; });
+    // For now, just get the feed data and dump to /temp/data.xml
+    return request(url).then((data) => {
+      try {
+        fs.appendFileSync('tempData.xml', data);
+      } catch (err) {
+        console.error(err);
+        throw (err);
+      }
+    });
   });
 };
