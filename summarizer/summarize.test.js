94324ac61dfa45c5bb929ba8a6c970dfb7bbbc57
Thomas Luu <thomasluu@Thomass-MacBook-Pro-2.local>
2019-11-11 11:11:38 -0500

    Implemented summarization in JS

---
+++ summarizer/summarize.test.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const summarize = require('./summarize');
+
+describe('summarize tests...', () => {
+  const text = fs.readFileSync('./summarizer/text.txt', 'utf-8');
+  var data;
+
+  it('summarize returns string', async () => {
+    data = await summarize(text, 5);
+    expect(typeof data).toBe('string');
+  })
+
+  it('summarize returns correct number of lines', () => {
+    expect(data.split('.').length).toBe(6);
+  })
+})
