b1c945181d9263219d83f5b1801488e59910a36f
Khang Nguyen <khangngocnamnguyen@outlook.com>
2019-11-10 23:04:44 -0500

    Fix issue #96: Combine EditConfig, ESLint, and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,33 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "env": {
-    "jest": true,
-    "browser": true,
-    "jquery": true,
-    "node": true,
-    "es2017": true
-  },
+    extends: ['airbnb-base', 'prettier'],
+    plugins: ['prettier'],
+    env: {
+        "jest": true,
+        "browser": true,
+        "jquery": true,
+        "node": true,
+        "es2017": true
+    },
+    rules: {
+        'prettier/prettier': ['error'],
 
-  rules: {
-    /**
-     * Disallow the use of console
-     * https://eslint.org/docs/rules/no-console
-     */
-    "no-console": "off",
+        /**
+         * Disallow the use of console
+         * https://eslint.org/docs/rules/no-console
+         */
+        "no-console": "off",
 
 
-    /**
-     * Disallow Reassignment of Function Parameters
-     * https://eslint.org/docs/rules/no-param-reassign
-     */
-    "no-param-reassign": ["error", { props: false }],
+        /**
+         * Disallow Reassignment of Function Parameters
+         * https://eslint.org/docs/rules/no-param-reassign
+         */
+        "no-param-reassign": ["error", { props: false }],
 
-    /**
-     * Require or disallow named function expressions
-     * https://eslint.org/docs/rules/func-names
-     */
-    "func-names": "off",
-
-  }
+        /**
+         * Require or disallow named function expressions
+         * https://eslint.org/docs/rules/func-names
+         */
+        "func-names": "off",
+    }
 };
