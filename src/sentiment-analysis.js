14857feaeaad8fcbdd6af66770a21fd7fa3b91d7
Robert Begna <rbegna@myseneca.ca>
2019-11-12 16:11:28 -0500

    added error handling to async function in sentiment.js

--- src/sentiment-analysis.js
+++ src/sentiment-analysis.js
@@ -9,5 +9,9 @@ const Sentiment = require('sentiment');
 
 module.exports.run = async function (text) {
   const sentiment = new Sentiment();
+  try{
   return Promise.resolve(sentiment.analyze(text));
+  }catch(error){
+    console.log(error);
+  }
 };
