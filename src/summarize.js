4ed25fe691be062935ac00be4a58183f8fe0fd72
Thomas Luu <thomasluu@Thomass-MBP-2.lan>
2019-11-13 13:36:55 -0500

    Removed promise.resolve wrapper

--- src/summarize.js
+++ src/summarize.js
@@ -2,7 +2,5 @@ const { SummarizerManager } = require('node-summarizer');
 
 module.exports = async function (text, numSentences = 3) {
   const summarizer = new SummarizerManager(text, numSentences);
-  return summarizer.getSummaryByRank().then(
-    (summaryObject) => Promise.resolve(summaryObject.summary),
-  );
+  return summarizer.getSummaryByRank().then((summaryObject) => summaryObject.summary);
 };
