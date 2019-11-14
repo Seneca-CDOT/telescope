accd9f0d07d2ef285f40518895b1abf81bb515fa
Pavan Kamra <pavankamra081796@gmail.com>
2019-11-14 13:13:16 -0500

    Fix #75: Added Fragmentation to HTML to text Parser

--- src/text-parser.js
+++ src/text-parser.js
@@ -5,8 +5,7 @@ const { JSDOM } = jsdom;
 // Simple function to convert html to text
 // Issue #75: https://github.com/Seneca-CDOT/telescope/issues/75
 exports.run = async function (html) {
-  const dom = new JSDOM(html);
-  const result = dom.window.document.querySelector('body').textContent;
-
-  return Promise.resolve(result);
+  const frag = JSDOM.fragment(html);
+  const result = await frag.textContent;
+  return result;
 };
