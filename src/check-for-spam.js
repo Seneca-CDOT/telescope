bfec89633da651945534a4a53f8c00f4d50a399c
jhui19 <jordanhui19@hotmail.com>
2019-11-14 16:40:56 -0500

    Added tests, various fixes, following eslint rules

--- src/check-for-spam.js
+++ src/check-for-spam.js
@@ -1,58 +1,41 @@
-const feedparser = require('feedparser-promised');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+// content refers to the contents of the blog post (article) sent by feedparser
+module.exports = function (content) {
+  // Minimum words set to 20, can be changed
+  const MINWORDS = 20;
 
-//Minimum words set to 20, can be changed
-MINWORDS = 20;
-
-//content refers to the contents of the blog post sent by feedparser, is a 
-function checkIfSpam(content)
-{
-    //The main body of the blog post with all HTML tags removed
-    noTagsDesc = removeTags(content.description);
-
-    //If the title is empty, post is considered spam
-    if (content.title != null)
-    {
-        //If the word count is under MINWORDS or all characters are capital, post is considered spam
-        if  (getWordCount(noTagsDesc) > MINWORDS && getCapitalLetters(noTagsDesc) < getAllLetters(noTagsDesc))
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+  // Functions
+  function removeTags(str) { // Removes HTML tags from a string, leaving only plaintext
+    if (str != null && str !== '') {
+      return str.toString().replace(/<[^>]*>/g, '');
     }
-    else
-    {
-        return true;
-    }
+    return '';
+  }
 
+  function getWordCount(str) { // Counts number of words in string
+    return str.split(' ').length;
+  }
 
-    //Functions
-    function removeTags(str)    //Removes HTML tags from a string, leaving only plaintext
-    {
-        if(str != null && str != '')
-        {
-            str = str.toString();
-            return str.replace(/<[^>]*>/g, '');
-        }
-        return "";
+  function getCapitalLetters(str) { // Counts number of capital letters in string
+    return str.replace(/[^A-Z]/g, '').length;
+  }
+
+  function getAllLetters(str) { // Counts number of alphabetical characters in string
+    return str.replace(/[^A-Za-z]/g, '').length;
+  }
+
+  // The main body of the blog post with all HTML tags removed
+  const noTagsDesc = removeTags(content.description);
+
+  // If the title is empty, post is considered spam
+  if (content.title != null) {
+    // If the word count is under MINWORDS or all characters are capital, post is considered spam
+    if (getWordCount(noTagsDesc) > MINWORDS
+        && getCapitalLetters(noTagsDesc) < getAllLetters(noTagsDesc)) {
+      return false;
     }
 
-    function getWordCount(str)  //Counts number of words in string
-    {
-        return str.split(" ").length;
-    }
+    return true;
+  }
 
-    function getCapitalLetters(str) //Counts number of capital letters in string
-    {
-        return str.replace(/[^A-Z]/g, "").length
-    }
-
-    function getAllLetters(str) //Counts number of alphabetical characters in string
-    {
-        return str.replace(/[^A-Za-z]/g, "").length
-    }
-}
+  return true;
+};
--- test/check-for-spam.test.js
+++ test/check-for-spam.test.js
@@ -1,14 +1,31 @@
-const checkForSpam = require ("../src/check-for-spam")
+const feedparser = require('feedparser-promised');
+const checkForSpam = require('../src/check-for-spam');
+
 const url = 'https://telescopetestblog.blogspot.com/feeds/posts/default?alt=rss';
 
 describe('Spam detection checks', () => {
+  // Running checkIfSpam() on each item returned by feedparser.parse(url)
+  // should return false, true, true, true in that order
 
-    //Running checkIfSpam() on each item returned by feedparser.parse(url) should return false, true, true, true in that order
-    //Using feedparser on the url
-    /*var parsedContent = feedparser.parse(url).then(items => 
-        items.forEach(item => {
-            checkIfSpam(item);
-        })
-    );*/
-    
+  let parsedContent;
+
+  beforeAll(async () => {
+    parsedContent = await feedparser.parse(url);
+  });
+
+  test('Should return false on first post', async () => {
+    expect(checkForSpam(parsedContent[0])).toBe(false);
+  });
+
+  test('Should return true on second post', async () => {
+    expect(checkForSpam(parsedContent[1])).toBe(true);
+  });
+
+  test('Should return true on third post', async () => {
+    expect(checkForSpam(parsedContent[2])).toBe(true);
+  });
+
+  test('Should return true on fourth post', async () => {
+    expect(checkForSpam(parsedContent[3])).toBe(true);
+  });
 });
