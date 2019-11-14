06e3d67066543915e96e5c655061296cfaa8f438
Ryan Wilson <rwilson31@myseneca.ca>
2019-11-14 11:03:10 -0500

    Travis CI

--- src/email-sender.js
+++ src/email-sender.js
@@ -1,9 +1,10 @@
 const nodemailer = require('nodemailer');
 
 /*
-                             HOW TO USE                                
+                             HOW TO USE
  Import this file - const sendEmail = require('./email-sender);
- send email - sendEmail.sendMessage('emailName@hotmail.com','Seneca had an error', '<p>An Error had occured</p>');
+ send email - sendEmail.sendMessage('emailName@hotmail.com','Seneca had an error',
+ '<p>An Error had occured</p>');
 
  Parameters examples
  receipiants - 'emailName@hotmail.com,emailName2@hotmail.com'
@@ -16,10 +17,13 @@ exports.sendMessage = async function (receipiants, subjectMessage, message) {
 
     // Credientials to send an email from
     const transporter = nodemailer.createTransport({
-      service: process.env.NODEMAILER_SERVER, // Email Server (.env variable must be used refer to the Contribution.md)
+      // Email Server (.env variable must be used refer to the Contribution.md)
+      service: process.env.NODEMAILER_SERVER,
       auth: {
-        user: process.env.NODEMAILER_USERNAME, // Email Name (.env variables must be used refer to the Contribution.md)
-        pass: process.env.NODEMAILER_PASSWORD, // Email Pass (.env variables must be used refer to the Contribution.md)
+        // Email Name (.env variables must be used refer to the Contribution.md)
+        user: process.env.NODEMAILER_USERNAME,
+        // Email Pass (.env variables must be used refer to the Contribution.md)
+        pass: process.env.NODEMAILER_PASSWORD,
       },
     });
 
