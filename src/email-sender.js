074487f19277a1afadc6c40f5edbf2c3a5f8d06f
Ryan Wilson <rwilson31@myseneca.ca>
2019-11-14 11:00:12 -0500

    Travis CI Fix

--- src/email-sender.js
+++ src/email-sender.js
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
 
 /*
-                             HOW TO USE                                 
- Import this file - const sendEmail = require('./email-sender);         
- send email - sendEmail.sendMessage('emailName@hotmail.com','Seneca had an error', '<p>An Error had occured</p>');  
+                             HOW TO USE                                
+ Import this file - const sendEmail = require('./email-sender);
+ send email - sendEmail.sendMessage('emailName@hotmail.com','Seneca had an error', '<p>An Error had occured</p>');
 
- Parameters examples 
+ Parameters examples
  receipiants - 'emailName@hotmail.com,emailName2@hotmail.com'
  subjectMessage - 'Seneca Telescope had an error'
  message - '<p>An Error has occurred <br/>Error Message:</p>'
@@ -13,32 +13,31 @@ const nodemailer = require('nodemailer');
 
 exports.sendMessage = async function (receipiants, subjectMessage, message) {
   return new Promise((resolve, reject) => {
- 
-  // Credientials to send an email from
-  const transporter = nodemailer.createTransport({
-    service: process.env.NODEMAILER_SERVER, //Email Server (.env variable must be used refer to the Contribution.md)
-    auth: {
-      user: process.env.NODEMAILER_USERNAME, // Email Name (.env variables must be used refer to the Contribution.md)
-      pass: process.env.NODEMAILER_PASSWORD, // Email Pass (.env variables must be used refer to the Contribution.md)
-    },
+
+    // Credientials to send an email from
+    const transporter = nodemailer.createTransport({
+      service: process.env.NODEMAILER_SERVER, // Email Server (.env variable must be used refer to the Contribution.md)
+      auth: {
+        user: process.env.NODEMAILER_USERNAME, // Email Name (.env variables must be used refer to the Contribution.md)
+        pass: process.env.NODEMAILER_PASSWORD, // Email Pass (.env variables must be used refer to the Contribution.md)
+      },
+    });
+
+    // Email Content
+    const mailOptions = {
+      from: '', // Email Name
+      to: receipiants, // People to send to
+      subject: subjectMessage, // Subject Line
+      html: message, // Message Body
+    };
+
+    // Send the email with the email content
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
   });
-
-  // Email Content
-  const mailOptions = {
-    from: '', // Email Name
-    to: receipiants, //People to send to
-    subject: subjectMessage, // Subject Line
-    html: message, // Message Body
-  };
-
-  // Send the email with the email content
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(info);
-    }
-  });
-
-});
 };
