66ce98cac9c8b90cc4e2ddbd2127a9760bb16e0e
Ryan Wilson <rwilson31@myseneca.ca>
2019-11-14 10:57:20 -0500

    Async Function Promise and Env File

--- src/email-sender.js
+++ src/email-sender.js
@@ -1,39 +1,44 @@
 const nodemailer = require('nodemailer');
 
 /*
-*                             HOW TO USE                                 *
-* Import this file - const sendEmail = require('./email-sender);         *
-* send email - sendEmail.sendMessage("Put your error message in here");  *
+                             HOW TO USE                                 
+ Import this file - const sendEmail = require('./email-sender);         
+ send email - sendEmail.sendMessage('emailName@hotmail.com','Seneca had an error', '<p>An Error had occured</p>');  
+
+ Parameters examples 
+ receipiants - 'emailName@hotmail.com,emailName2@hotmail.com'
+ subjectMessage - 'Seneca Telescope had an error'
+ message - '<p>An Error has occurred <br/>Error Message:</p>'
 */
 
-exports.sendMessage = async function (errorMessage) {
-  // Recipients list of users to send the email too
-  // receipiants is commas separated (emailName@hotmail.com,emailName2@hotmail.com)
-  const receipiants = '';
-
+exports.sendMessage = async function (receipiants, subjectMessage, message) {
+  return new Promise((resolve, reject) => {
+ 
   // Credientials to send an email from
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: process.env.NODEMAILER_SERVER, //Email Server (.env variable must be used refer to the Contribution.md)
     auth: {
-      user: '', // Email Name (.env variables must be used refer to issue#60)
-      pass: '', // Email Pass (.env variables must be used refer to issue#60)
+      user: process.env.NODEMAILER_USERNAME, // Email Name (.env variables must be used refer to the Contribution.md)
+      pass: process.env.NODEMAILER_PASSWORD, // Email Pass (.env variables must be used refer to the Contribution.md)
     },
   });
 
   // Email Content
   const mailOptions = {
     from: '', // Email Name
-    to: receipiants,
-    subject: 'Seneca Telescope had an error', // Subject Line
-    html: `<p>An Error has occurred <br/>Error Message: ${errorMessage} </p>`, // Message Body
+    to: receipiants, //People to send to
+    subject: subjectMessage, // Subject Line
+    html: message, // Message Body
   };
 
   // Send the email with the email content
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
-      console.log(err);
+      reject(err);
     } else {
-      console.log(info);
+      resolve(info);
     }
   });
+
+});
 };
