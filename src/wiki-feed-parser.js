60050f2caca8ff66ad8b0261052d5be22df5450a
Igor Naperkovskiy <igor@Igors-MacBook-Pro.local>
2019-11-14 00:34:22 -0500

    fixed requested changes

--- src/wiki-feed-parser.js
+++ src/wiki-feed-parser.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
 const bent = require('bent');
 const jsdom = require('jsdom');
+require('./config.js');
 
 const request = bent('string');
 const { JSDOM } = jsdom;
@@ -11,17 +11,13 @@ const { JSDOM } = jsdom;
 * Splits the data into lines so its easier to process as a string array
 * That data is then returned as a Promise
 */
-function getData() {
-  const list = request(process.env.FEED_URL)
+module.exports.getData = function () {
+  return request(process.env.FEED_URL)
     .then((data) => {
       const dom = new JSDOM(data);
-      let feedList = '';
-      feedList = dom.window.document.querySelector('pre').textContent;
-      return feedList.split(/\r\n|\r|\n/);
+      return dom.window.document.querySelector('pre').textContent.split(/\r\n|\r|\n/);
     }).catch((err) => { throw err; });
-
-  return list;
-}
+};
 
 /*
 * parseData() returns Promise { <pending> }
@@ -29,32 +25,27 @@ function getData() {
 * Then processes it to remove square brackets from links and 'name=' in front of a name
 * That data is then returned as a Promise
 */
-function parseData() {
+module.exports.parseData = function () {
   const nameCheck = /^name/i;
   const commentCheck = /^#/;
 
-  let feed = '';
   let line = '';
 
-  const parsedData = getData().then((data) => {
+  return this.getData().then((data) => {
+    const feed = [];
     data.forEach((element) => {
       if (!commentCheck.test(element)) {
         if (element.startsWith('[')) {
           // eslint-disable-next-line no-useless-escape
           line = element.replace(/[\[\]']/g, '');
-          feed += `${line}\n`;
+          feed.push(`${line}`);
         }
         if (nameCheck.test(element)) {
           line = element.replace(/^\s*name\s*=\s*/, '');
-          feed += `${line}\n`;
+          feed.push(`${line}`);
         }
       }
-
-      return feed;
     });
+    return feed;
   }).catch((err) => { throw err; });
-
-  return parsedData;
-}
-
-parseData();
+};
