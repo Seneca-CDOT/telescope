5b724a0a745b7d913896e7e16bad3d7af2dade11
Rafi Ungar <raungar@gmail.com>
2019-11-15 00:14:14 -0500

    Added testing for inactive blog checking (#197)

---
+++ test/inactive-blog-filter.test.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const inactiveFilter = require('../src/inactive-blog-filter');
+
+const redlist = JSON.parse(fs.readFileSync('../feeds-redlist.json'));
+
+describe('Redlisted feed checking', () => {
+  it('should return false for bad feed URLs', async () => {
+    const badURLs = [redlist[0].slice(0, -1), '', undefined];
+    badURLs.forEach((url) => {
+      inactiveFilter.check(url, (callback, result) => {
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  it('should return true for all feed URLs in feeds-redlist.json', async () => {
+    redlist.forEach((feed) => {
+      inactiveFilter.check(feed.url, (callback, result) => {
+        expect(result).toBe(true);
+      });
+    });
+  });
+});
