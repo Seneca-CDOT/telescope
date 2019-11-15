211e47f64e732e4af83228342606b285e8f6f7c4
Chris Kerley <chriskerley@palolo.ca>
2019-11-14 19:56:31 -0500

    unit tests have been added for the feed parser

--- src/feed-parser.js
+++ src/feed-parser.js
@@ -1,7 +1,30 @@
-const feedparser = require('feedparser-promised');
+const feedParser = require('feedparser-promised');
+// made into a class for easy test mocking.
+class FeedParser {
+  constructor() {
+    this.feedParser = feedParser;
+  }
 
-// feed: url for the feed
-// Accepts a url feed and parses it, on success will return an array
-module.exports = async function (feed) {
-  return feedparser.parse(feed);
-};
+  /**
+    Dependency injection to mock the response of feedparser.
+  */
+  injectParser(parser) {
+    this.feedParser = parser;
+  }
+
+  async parse(urlFeed) {
+    try {
+      return await this.feedParser.parse(urlFeed);
+    } catch (err) {
+      if (/.*ENOTFOUND.*/.test(err.message)) {
+        throw new Error('That URL does not exist.');
+      } else if (/Not a feed/.test(err.message)) {
+        throw new Error('That URL is not a valid feed.');
+      } else {
+        throw (err);
+      }
+    } // end catch.
+  }
+} // end class.
+
+module.exports = FeedParser;
---
+++ test/feed-parser.test.js
@@ -0,0 +1,58 @@
+const FeedParser = require('../src/feed-parser');
+
+let parser;
+
+beforeEach(() => {
+  parser = new FeedParser();
+});
+
+const assertValidFeed = (feed) => {
+  expect(Array.isArray(feed)).toBeTruthy();
+  expect(feed.length > 0).toBeTruthy();
+};
+
+
+test('Non existant feed failure case.', async () => {
+  try {
+    await parser.parse('http://doesnotexists___.com');
+  } catch (err) {
+    expect(err.message).toBe('That URL does not exist.');
+  }
+});
+
+test('Not a feed failure case', async () => {
+  try {
+    const nonFeedURL = 'https://kerleysblog.blogspot.com';
+    await parser.parse(nonFeedURL);
+  } catch (err) {
+    expect(/.*That URL is not a valid feed.*/.test(err.message)).toBeTruthy();
+  }
+});
+
+
+test('Unrecognized error case', async () => {
+  const fakeParser = {
+    parse: () => {
+      throw new Error('Unrecognized error.');
+    },
+  };
+  parser.injectParser(fakeParser);
+  try {
+    await parser.parse('some url');
+  } catch (err) {
+    expect(err.message).toBe('Unrecognized error.');
+  }
+});
+
+
+test('Blogger feed success case', async () => {
+  const validFeed = 'https://kerleysblog.blogspot.com/feeds/posts/default?alt=rss';
+  const feed = await parser.parse(validFeed);
+  assertValidFeed(feed);
+});
+
+test('Wordpress site feed success case', async () => {
+  const validFeed = 'https://medium.com/feed/@Medium';
+  const feed = await parser.parse(validFeed);
+  assertValidFeed(feed);
+});
