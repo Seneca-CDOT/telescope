8b9159e69209e1def444076b159550564119b379
Neil Ong <neil.ong31@gmail.com>
2019-11-15 12:28:49 -0500

    Added comments to tester and removed email address from .env file

--- test/email-sender.test.js
+++ test/email-sender.test.js
@@ -1,6 +1,12 @@
 const emailsender = require('../src/email-sender');
 
+// This tests sendMessage from /src/email-sender.js:
+// It gives a test email and recipient to the sendMessage function
+// and checks if it's resolve returns what is to be expected,
+// which an array of the testRecipient since it returns
+// an array of addresses it sent the email too
 test('Tests if sendMessage resolves with expected info', async () => {
+  // Feeds test data into sendMessage function
   const testSubjectMessage = 'Test';
   const testHTML = '<h1>Hello World</h1>';
   const testRecipient = process.env.NODEMAILER_TESTRECIPIENT;
@@ -9,5 +15,6 @@ test('Tests if sendMessage resolves with expected info', async () => {
     testSubjectMessage,
     testHTML,
   );
+  // Tests if the expected resolve is correct
   expect(testReturnValue).resolves.toBe([testRecipient]);
 });
