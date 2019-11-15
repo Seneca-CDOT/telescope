0aeac02e650e961db460ad7526a6187c5f55fda4
varshannagarajan <48139461+varshannagarajan@users.noreply.github.com>
2019-11-15 12:49:45 -0500

    fix issue #62 added bull board (#200)

--- src/feed-queue.js
+++ src/feed-queue.js
@@ -1,6 +1,11 @@
 const Bull = require('bull');
+const { setQueues } = require('bull-board');
+
 require('./config');
 
 const queue = new Bull('feed-queue', process.env.REDIS_URL);
+
+// For visualizing queues using bull board
+setQueues(queue);
 
 module.exports = queue;
