0faf498d389424f1e7b0bf8bc41bb00b62231cd0
Nick Skuybeda <mskuybeda@myseneca.ca>
2019-11-16 01:58:29 -0500

    updated storage.js file and problematic test

--- src/storage.js
+++ src/storage.js
@@ -1,34 +1,28 @@
-//get config
-require('./config');
-//get redis constant searches redis globally 
+const Redis = require('ioredis');
 
-const Redis = require('ioredis'); //chnage to ioredis
-//connecting client
 const client = new Redis(process.env.REDIS_URL);
 
-//result from require, displays externally
 module.exports = {
-    addFeed: function(name, url, f) {
-      const feed = { 'name': name, 'url': url }; //insted of json store id to a hash
-      client.pipeline().incr('user_id').get('user_id', function(err, id) {
-          if (err)
-            f(err, id);
-          else
-            client.hset('feeds', id, JSON.stringify(feed), f); //transform json object into string and save to redis feeds
-      }).exec();
-    },
+  addFeed: (name, url) => {
+    let feedID = 1000;
+    const feed = { name, url };
+    return client
+      .get('feed_id')
+      .then((result) => {
+        if (!result) client.set('feed_id', 1000);
+        else feedID = result;
+      })
+      .then(() => {
+        client
+          .pipeline()
+          .set(feedID, JSON.stringify(feed))
+          .sadd('feeds', feedID)
+          .incr('feed_id')
+          .exec();
+      });
+  },
 
-    //sends to read and waits for completion (async method)
-    getFeeds: function(f) {
-        client.hgetall('feeds', function(err, data) {
-            if (typeof(data) != 'undefined') {
-    //removes id from display
-                result = [];
-                for (var k in data)
-                    result.push(JSON.parse(data[k]));
-                data = result;
-            }
-            f(err, data);
-        });
-    }
-}
+  getFeeds: () => client.smembers('feeds'),
+
+  getFeed: (feedID) => client.get(feedID),
+};
