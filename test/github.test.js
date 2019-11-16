d45ba07acef66235adae5b6cb21642be9860b5a7
Josue <josue.quilon-barrios@senecacollege.ca>
2019-11-16 00:10:43 -0500

    Added mock responses for node-fetch

--- test/github.test.js
+++ test/github.test.js
@@ -1,10 +1,15 @@
 const gh = require('../src/github-url');
+global.fetch = require('node-fetch');
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
 
 /**
  * Test to validate fetching data when passing a user URL.
  * It uses the Seneca-CDOT URL
  */
-test.skip('test fetching data for a valid user URL', async () => {
+test('test fetching data for a valid user URL', async () => {
   const validUserUrl = 'https://github.com/Seneca-CDOT';
   const validUserData = {
     user: expect.any(String),
@@ -16,6 +21,16 @@ test.skip('test fetching data for a valid user URL', async () => {
     bio: expect.any(String),
   };
 
+  fetch.mockResponseOnce(JSON.stringify({
+    login: 'foo',
+    avatar_url: 'foo',
+    name: 'foo',
+    company: 'foo',
+    blog: 'foo',
+    email: 'foo',
+    bio: 'foo',
+  }, { status: 200 }));
+
   const user = await gh.getGithubUrlData(validUserUrl);
   Object.keys(validUserData).map((property) => expect(user).toHaveProperty(property));
 });
@@ -24,7 +39,7 @@ test.skip('test fetching data for a valid user URL', async () => {
  * Test to validate fetching data when passing a repo URL.
  * It uses the telescope URL
  */
-test.skip('test fetching data for a valid repository URL', async () => {
+test('test fetching data for a valid repository URL', async () => {
   const validRepoUrl = 'https://github.com/Seneca-CDOT/telescope';
   const validRepoData = {
     avatarURL: expect.any(String),
@@ -41,6 +56,21 @@ test.skip('test fetching data for a valid repository URL', async () => {
     createdAt: expect.any(String),
     language: expect.any(String),
   };
+  fetch.mockResponseOnce(JSON.stringify({
+    owner: { avatar_url: 'foo' },
+    description: 'foo',
+    license: {
+      key: 'foo',
+      name: 'foo',
+      spdx_id: 'foo',
+      url: 'foo',
+      node_id: 'foo',
+    },
+    open_issues: 0,
+    forks: 0,
+    created_at: 'foo',
+    language: 'foo',
+  }, { status: 200 }));
 
   const repo = await gh.getGithubUrlData(validRepoUrl);
   Object.keys(validRepoData).map((property) => expect(repo).toHaveProperty(property));
@@ -50,7 +80,7 @@ test.skip('test fetching data for a valid repository URL', async () => {
  * Test to validate fetching data when passing a repo URL.
  * It uses telescope's issue 2
  */
-test.skip('test fetching data for a valid issue URL', async () => {
+test('test fetching data for a valid issue URL', async () => {
   const validIssueUrl = 'https://github.com/Seneca-CDOT/telescope/issues/2';
   const validIssueData = {
     login: expect.any(String),
@@ -61,6 +91,16 @@ test.skip('test fetching data for a valid issue URL', async () => {
     repo: expect.any(String),
   };
 
+  fetch.mockResponseOnce(JSON.stringify({
+    user:
+    {
+      login: 'foo',
+      avatar_url: 'foo',
+    },
+    body: 'foo',
+    created_at: 'foo',
+  }, { status: 200 }));
+
   const issue = await gh.getGithubUrlData(validIssueUrl);
   Object.keys(validIssueData).map((property) => expect(issue).toHaveProperty(property));
 });
@@ -70,7 +110,7 @@ test.skip('test fetching data for a valid issue URL', async () => {
  * Test to validate fetching data when passing a pull request URL.
  * It uses telescope's pull request 1
  */
-test.skip('test fetching data for a valid pull request URL', async () => {
+test('test fetching data for a valid pull request URL', async () => {
   const validPullRequestUrl = 'https://github.com/Seneca-CDOT/telescope/pull/1';
   const validPullRequestData = {
     login: expect.any(String),
@@ -80,6 +120,16 @@ test.skip('test fetching data for a valid pull request URL', async () => {
     branch: expect.any(String),
     repo: expect.any(String),
   };
+
+  fetch.mockResponseOnce(JSON.stringify({
+    user:
+    {
+      login: 'foo',
+      avatar_url: 'foo',
+    },
+    body: 'foo',
+    created_at: 'foo',
+  }, { status: 200 }));
 
   const pr = await gh.getGithubUrlData(validPullRequestUrl);
   Object.keys(validPullRequestData).map((property) => expect(pr).toHaveProperty(property));
