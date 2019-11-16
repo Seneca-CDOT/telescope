7277957fe7a4db9e4c117ac977f723e2a485db6b
Jordan Sie <42614134+jrdnlx@users.noreply.github.com>
2019-11-15 20:49:45 -0500

    added OPML generator

--- src/backend/web/routes/index.js
+++ src/backend/web/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const admin = require('./admin');
+const opml = require('./opml');
 
 const router = express.Router();
 
 router.use(express.static(path.join(__dirname, '../../../frontend')));
 
 router.use('/admin', admin);
+router.use('/opml', opml);
 
 module.exports = router;
