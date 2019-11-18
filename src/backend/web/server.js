f1d00ceaa68fe530550b009de43fd1c3b974e8dc
Miguel Roncancio <mroncancio19@gmail.com>
2019-11-17 23:13:37 -0500

    Fix 274 Add pino logging to backend

--- src/backend/web/server.js
+++ src/backend/web/server.js
@@ -1,15 +1,16 @@
 require('../../config');
 const app = require('./app.js');
 
+const { logger } = app.get('logger');
 const HTTP_PORT = process.env.PORT || 3000;
 
 const server = app.listen(HTTP_PORT, () => {
-  console.log(`Telescope listening on port ${HTTP_PORT}`);
+  logger.info(`Telescope listening on port ${HTTP_PORT}`);
 });
 
-process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION:  Shutting down...');
-  console.log(err.name, err.message);
+process.on('unhandledRejection', ({ name, message }) => {
+  logger.error('UNHANDLED REJECTION:  Shutting down...');
+  logger.error(`${name}: ${message}`);
   server.close(() => {
     process.exit(1);
   });
