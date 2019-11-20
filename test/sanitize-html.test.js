e22e4dcada2e16922869b4984f5975dc289afaab
Hayley <hayley@shmooey.ca>
2019-11-19 22:46:51 -0500

    Made requested changes to sanitize-html.js and sanitize-html.test.js

--- test/sanitize-html.test.js
+++ test/sanitize-html.test.js
@@ -3,6 +3,6 @@
 const sanitizeHTML = require('../src/sanitize-html');
 
 test('Tests for sanitized HTML', async () => {
-  const data = await sanitizeHTML.run('<img src=x onerror=alert(1)//>');
+  const data = await sanitizeHTML.run('<img src=x onerror=alert(1)>');
   expect(data).toBe('<img src="x">');
 });
