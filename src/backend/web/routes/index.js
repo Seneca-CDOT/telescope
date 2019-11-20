da292ceeaa4bfb598a7bd7cb78c7dde1b6fa6c83
miguel <mroncancio19@gmail.com>
2019-11-20 17:25:18 -0500

    clean up messy code

--- src/backend/web/routes/index.js
+++ src/backend/web/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const admin = require('./admin');
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../../frontend/index.html'));
 });
 
-router.use('/admin', require('./admin'));
+router.use('/admin', admin);
 
 module.exports = router;
