3345f47ad3fd6d7670b0987449d9437e5f4a0030
Cindy Le <cindyledev@gmail.com>
2019-11-21 11:06:17 -0500

    added express static middleware

--- src/backend/web/routes/index.js
+++ src/backend/web/routes/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
-const admin = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../frontend/index.html'));
-});
+router.use(express.static(path.join(__dirname, '../../../frontend')));
 
-router.use('/admin', admin);
+router.use('/admin', require('./admin'));
 
 module.exports = router;
