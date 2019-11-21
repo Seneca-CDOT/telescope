4407ef1ecc1ec8ef79bf1011e75e754a0e840f28
Cindy Le <cindyledev@gmail.com>
2019-11-21 15:26:52 -0500

    moved admin module back to top

--- src/backend/web/routes/index.js
+++ src/backend/web/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const admin = require('./admin');
 
 const router = express.Router();
 
 router.use(express.static(path.join(__dirname, '../../../frontend')));
 
-router.use('/admin', require('./admin'));
+router.use('/admin', admin);
 
 module.exports = router;
