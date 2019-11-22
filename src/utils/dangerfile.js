174720a07282e403191988c6a483028d5ec7210e
c3ho <calvinho125@gmail.com>
2019-11-22 12:29:13 -0500

    still testing

--- src/utils/dangerfile.js
+++ src/utils/dangerfile.js
@@ -7,5 +7,5 @@ const tests = includes(danger.git.fileMatch, '../../test/*.test.js');
 if (apps.modified && !tests.modified) {
   const message = `Changes were made to file in src, but not to the test file folder`;
   const idea = `Perhaps check if tests for the src file needs to be changed?`;
-  warn(`${message} - <i>${idea}</i>`);
+  warn(`${message} - <i>${idea}</i> `);
 }
