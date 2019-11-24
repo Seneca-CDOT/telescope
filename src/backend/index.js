c455eaeddd76ffd9c9e57e615da5470ca4b9987d
Cryolis <naperkovskiy@gmail.com>
2019-11-24 12:54:54 -0500

    inserted wiki parser into feed enque

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,8 +1,8 @@
 require('./lib/config.js');
-const fs = require('fs');
 const feedQueue = require('./feed/queue');
 const feedWorker = require('./feed/worker');
 const { logger } = require('./utils/logger');
+const wikiFeed = require('./utils/wiki-feed-parser');
 
 const log = logger.child({ module: 'main' });
 
@@ -75,58 +75,31 @@ process.on('uncaughtException', err => {
 });
 
 /**
- * Process a string into a list of Objects, each with a feed URL
- * @param {String} lines
- */
-function processFeedUrls(lines) {
-  return (
-    lines
-      // Divide the file into separate lines
-      .split(/\r?\n/)
-      // Basic filtering to remove any ines that don't look like a feed URL
-      .filter(line => line.startsWith('http'))
-      // Convert this into an Object of the form expected by our queue
-      .map(url => ({ url }))
-  );
-}
-
-/**
  * Adds feed URL jobs to the feed queue for processing
  * @param {Array[Object]} feedJobs - list of feed URL jobs to be processed
  */
-async function enqueueFeedJobs(feedJobs) {
-  feedJobs.forEach(async feedJob => {
-    log.info(`Enqueuing Job - ${feedJob.url}`);
-    await feedQueue.add(feedJob, {
-      attempts: process.env.FEED_QUEUE_ATTEMPTS || 8,
-      backoff: {
-        type: 'exponential',
-        delay: process.env.FEED_QUEUE_DELAY_MS || 60 * 1000,
-      },
-      removeOnComplete: true,
-      removeOnFail: true,
-    });
-  });
+async function enqueueWikiFeed() {
+  const data = await wikiFeed.parseData();
+  Promise.all(
+    data.map(async feedJob => {
+      log.info(`Enqueuing feed job for ${feedJob.url}`);
+      await feedQueue.add(feedJob, {
+        attempts: 8,
+        backoff: {
+          type: 'exponential',
+          delay: 60 * 1000,
+        },
+        removeOnComplete: true,
+        removeOnFail: true,
+      });
+    })
+  ).catch(err => log.error({ err }, 'Error queuing wiki feeds'));
 }
 
-/**
- * For now, do something simple and just read a few feeds from a text file
- */
-fs.readFile('feeds.txt', 'utf8', (err, lines) => {
-  if (err) {
-    log.error('unable to read initial list of feeds', err.message);
-    process.exit(-1);
-    return;
-  }
-
-  // Process this text file into a list of URL jobs, and enqueue for download
-  const feedJobs = processFeedUrls(lines);
-
-  enqueueFeedJobs(feedJobs)
-    .then(() => {
-      feedWorker.start();
-    })
-    .catch(error => {
-      log.error(error);
-    });
-});
+enqueueWikiFeed()
+  .then(() => {
+    feedWorker.start();
+  })
+  .catch(error => {
+    log.error({ error }, 'Unable to enqueue wiki feeds');
+  });
