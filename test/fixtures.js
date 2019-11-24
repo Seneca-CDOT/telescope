142672ac1ec0c2154267e807faf5f96ecc411155
David Humphrey <david.humphrey@senecacollege.ca>
2019-11-23 19:05:06 -0500

    Format XML/HTML strings over multiple lines, per review

--- test/fixtures.js
+++ test/fixtures.js
@@ -12,12 +12,49 @@ const getHtmlUri = () => 'https://test321.blogspot.com/blog';
 // Remove leading protocol from a URI
 const stripProtocol = uri => uri.replace(/^https?:\/\//, '');
 
-const getFeedBody = () =>
-  '<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"><channel><title>W3Schools Home Page</title><link>https://www.w3schools.com</link><description>Free web building tutorials</description><item><title>RSS Tutorial</title><link>https://www.w3schools.com/xml/xml_rss.asp</link><description>New RSS tutorial on W3Schools</description></item><item><title>XML Tutorial</title><link>https://www.w3schools.com/xml</link><description>New XML tutorial on W3Schools</description></item></channel></rss>';
-const getInvalidFeedBody = () =>
-  '<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"><channel></channel></rss>';
-const getHtmlBody = () =>
-  '<!DOCTYPE html><html><head><title>HTML Page</title></head><body>HTML, NOT XML</body></html>';
+const getValidFeedBody = () =>
+  `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <rss version="2.0">
+    <channel>
+      <title>W3Schools Home Page</title>
+      <link>https://www.w3schools.com</link>
+      <description>Free web building tutorials</description>
+      <item>
+        <title>RSS Tutorial</title>
+        <link>https://www.w3schools.com/xml/xml_rss.asp</link>
+        <description>New RSS tutorial on W3Schools</description>
+      </item>
+      <item>
+        <title>XML Tutorial</title>
+        <link>https://www.w3schools.com/xml</link>
+        <description>New XML tutorial on W3Schools</description>
+      </item>
+    </channel>
+  </rss>
+  `;
+
+const getEmptyFeedBody = () =>
+  `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <rss version="2.0">
+    <channel/>
+  </rss>
+  `;
+
+const getValidHtmlBody = () =>
+  `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf8" />
+      <title>HTML Page</title>
+    </head>
+    <body>
+      <p>HTML, NOT XML</p>
+    </body>
+  </html>
+  `;
 
 /**
  * Generic network nock request, used below to define all our mock requests.
@@ -41,24 +78,24 @@ exports.getRssUri = getRssUri;
 exports.getHtmlUri = getHtmlUri;
 exports.stripProtocol = stripProtocol;
 
-exports.getFeedBody = getFeedBody;
-exports.getInvalidFeedBody = getInvalidFeedBody;
-exports.getHtmlBody = getHtmlBody;
+exports.getValidFeedBody = getValidFeedBody;
+exports.getEmptyFeedBody = getEmptyFeedBody;
+exports.getValidHtmlBody = getValidHtmlBody;
 
 exports.nockValidAtomResponse = function() {
-  nockResponse(getAtomUri(), getFeedBody(), 200, 'application/rss+xml');
+  nockResponse(getAtomUri(), getValidFeedBody(), 200, 'application/rss+xml');
 };
 
 exports.nockValidRssResponse = function() {
-  nockResponse(getRssUri(), getFeedBody(), 200, 'application/rss+xml');
+  nockResponse(getRssUri(), getValidFeedBody(), 200, 'application/rss+xml');
 };
 
 exports.nockInvalidRssResponse = function() {
-  nockResponse(getRssUri(), getInvalidFeedBody(), 200, 'application/rss+xml');
+  nockResponse(getRssUri(), getEmptyFeedBody(), 200, 'application/rss+xml');
 };
 
 exports.nockValidHtmlResponse = function() {
-  nockResponse(getHtmlUri(), getHtmlBody(), 200, 'text/html');
+  nockResponse(getHtmlUri(), getValidHtmlBody(), 200, 'text/html');
 };
 
 exports.nock404Response = function() {
