c3f0e1dee42d5f95ad1ee90f3f3bf53c372e782b
Sbaah <sbaah12@gmail.com>
2019-11-28 13:07:33 -0500

    Bug Fix for issue-289

--- src/backend/utils/logger.js
+++ src/backend/utils/logger.js
@@ -11,39 +11,51 @@ let logger;
  * Enable prettyPrint option and translate time from epoch time to local time.
  * Set log level to LOG_LEVEL environment variable with 'debug' as default level.
  */
-if (process.env.NODE_ENV === 'development') {
-  logger = pino({
-    prettyPrint: {
-      translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
-      colorize: !(os.type() === 'Windows_NT'),
-    },
-    level: process.env.LOG_LEVEL || 'debug',
-  });
-} else if (process.env.LOG_FILE) {
-  /* If in Production mode:
-   * Write logs to a specified path.
-   * Set log level to LOG_LEVEL environment variable with 'info' as default level.
-   */
-  logger = pino(
-    {
-      level: process.env.LOG_LEVEL || 'info',
+
+/*Created a local variable to to hold the value form process.env.LOG_LEVEL.toLowerCase()
+ * The if statement will check the verible if the verible is anything other than levels
+ * stated within file it will automaticaly set to info.
+ * https://getpino.io/#/docs/api?id=api Look for the title ``` level (String) ```
+ */
+var local_LOG = process.env.LOG_LEVEL.toLowerCase();
+if (local_LOG != 'info') {
+  local_LOG = 'info';
+} else {
+  if (process.env.NODE_ENV === 'development') {
+    logger = pino({
+      prettyPrint: {
+        translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
+        colorize: !(os.type() === 'Windows_NT'),
+      },
+      level: process.env.LOG_LEVEL || 'debug',
+    });
+  } else if (process.env.LOG_FILE) {
+    /* If in Production mode:
+     * Write logs to a specified path.
+     * Set log level to LOG_LEVEL environment variable with 'info' as default level.
+     */
+    logger = pino(
+      {
+        //level: process.env.LOG_LEVEL || 'info',
+        level: local_LOG || 'info',
+        prettyPrint: {
+          translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
+          colorize: false,
+        },
+      },
+      pino.destination(process.env.LOG_FILE)
+    );
+  } else {
+    logger = pino({
+      //level: process.env.LOG_LEVEL || 'info',
+      level: local_LOG || 'info',
       prettyPrint: {
         translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
         colorize: false,
       },
-    },
-    pino.destination(process.env.LOG_FILE)
-  );
-} else {
-  logger = pino({
-    level: process.env.LOG_LEVEL || 'info',
-    prettyPrint: {
-      translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
-      colorize: false,
-    },
-  });
+    });
+  }
 }
-
 const expressLogger = expressPino({ logger });
 
 module.exports = expressLogger;
