a27d2263643e228c3b321a6a4e0644c89d98a858
Julie Philip James <41449664+jpjjulie@users.noreply.github.com>
2019-11-29 00:37:22 -0500

    Update logger.test.js

--- test/logger.test.js
+++ test/logger.test.js
@@ -8,4 +8,7 @@ test('logger.methods to be functions', () => {
   expect(typeof logger.info).toBe('function');
   expect(typeof logger.child).toBe('function');
   expect(typeof logger.error).toBe('function');
+  expect(typeof logger.trace).toBe('function');
+  expect(typeof logger.warn).toBe('function');
+  expect(typeof logger.fatal).toBe('function');
 });
