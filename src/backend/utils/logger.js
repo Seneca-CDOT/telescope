44aa18c3a0b87fae193599a729e9add2144ec50a
Sbaah <sbaah12@gmail.com>
2019-11-29 15:31:35 -0500

    Update logger.js
    - removes the else if.
    - changelocal variable name
    - improve the check stament to include the pino's defualt levels

--- src/backend/utils/logger.js
+++ src/backend/utils/logger.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-lonely-if */
 const pino = require('pino');
 const expressPino = require('express-pino-logger');
 const os = require('os');
@@ -5,13 +6,28 @@ const os = require('os');
 require('../lib/config');
 
 let logger;
+let logLevel;
 
 /* If in Development mode:
  * Output logs to console (which is by default)
  * Enable prettyPrint option and translate time from epoch time to local time.
  * Set log level to LOG_LEVEL environment variable with 'debug' as default level.
  */
-if (process.env.NODE_ENV === 'development') {
+
+/* Created a local variable to to hold the value form process.env.LOG_LEVEL.toLowerCase()
+ * The if statement will check the variable if the variable is anything other than levels
+ * stated within file it will automatically set to info.
+ * https://getpino.io/#/docs/api?id=level-string Look for the title ``` level (String) ```
+ */
+
+logLevel = (process.env.LOG_LEVEL || 'info').toLowerCase();
+if (!pino.levels.values[logLevel]) {
+  logLevel = 'info';
+}
+
+if (logLevel !== 'info') {
+  logLevel = 'info';
+} else if (process.env.NODE_ENV === 'development') {
   logger = pino({
     prettyPrint: {
       translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
@@ -19,29 +35,31 @@ if (process.env.NODE_ENV === 'development') {
     },
     level: process.env.LOG_LEVEL || 'debug',
   });
-} else if (process.env.LOG_FILE) {
-  /* If in Production mode:
-   * Write logs to a specified path.
-   * Set log level to LOG_LEVEL environment variable with 'info' as default level.
-   */
-  logger = pino(
-    {
-      level: process.env.LOG_LEVEL || 'info',
+} else {
+  if (process.env.LOG_FILE) {
+    /* If in Production mode:
+     * Write logs to a specified path.
+     * Set log level to LOG_LEVEL environment variable with 'info' as default level.
+     */
+    logger = pino(
+      {
+        level: logLevel || 'info',
+        prettyPrint: {
+          translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
+          colorize: false,
+        },
+      },
+      pino.destination(process.env.LOG_FILE)
+    );
+  } else {
+    logger = pino({
+      level: logLevel || 'info',
       prettyPrint: {
         translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
         colorize: false,
       },
-    },
-    pino.destination(process.env.LOG_FILE)
-  );
-} else {
-  logger = pino({
-    level: process.env.LOG_LEVEL || 'info',
-    prettyPrint: {
-      translateTime: 'SYS: yyyy-mm-dd HH:MM:ss.l ',
-      colorize: false,
-    },
-  });
+    });
+  }
 }
 
 const expressLogger = expressPino({ logger });
