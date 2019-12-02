cfcfc395a67b193888e13bc7899cf6989538b923
Jatin Arora <jaarora45@gmaill.com>
2019-12-01 20:50:10 -0500

    Fixes #259. Added Post class to store data for each post

--- src/backend/feed/worker.js
+++ src/backend/feed/worker.js
@@ -1,24 +1,47 @@
 const feedQueue = require('./queue');
 const feedParser = require('./parser');
 const extractUrls = require('../utils/extract-urls');
+const Post = require('../post');
+const textParser = require('../utils/text-parser');
+const wordCount = require('../utils/word-counter');
+
+async function processContent(post) {
+  let postObj;
+  try {
+    await textParser.run(post.description).then(function(data) {
+      postObj = new Post(
+        post.author,
+        post.title,
+        data,
+        post.date,
+        post.pubDate,
+        post.link,
+        post.guid
+      );
+      extractUrls.extract(post.description);
+    });
+  } catch (err) {
+    console.log(`Failed to parse text from post: ${post.guid}`);
+  }
+  try {
+    postObj.wordCount = await wordCount.wordCounter(postObj.content);
+  } catch (err) {
+    console.log(`Failed to get word count from the post: ${post.guid}`);
+  }
+  return postObj;
+}
 
 exports.workerCallback = async function(job) {
   const { url } = job.data;
   try {
     const posts = await feedParser(url);
     const processedPosts = [];
+
     if (posts.length > 0) {
       posts.forEach(post => {
-        // We can extract any other information from the post that we need here.
-        const processedPost = {
-          author: post.author,
-          date: post.date,
-          title: post.title,
-          description: post.description,
-          postURL: post.link,
-        };
-        processedPosts.push(processedPost);
-        extractUrls.extract(post.description);
+        processContent(post).then(function(processedPost) {
+          processedPosts.push(processedPost);
+        });
       });
       return Promise.resolve(processedPosts);
     }
---
+++ src/backend/post.js
@@ -0,0 +1,20 @@
+/**
+ * This file contains the class Post with methods of it.
+ * Class Post is used to generate objects that contain
+ * data of each blog post.
+ */
+
+class Post {
+  constructor(author, title, content, datePublished, dateUpdated, postLink, guid) {
+    this.author = author;
+    this.title = title;
+    this.content = content;
+    this.datePublished = datePublished;
+    this.dateUpdated = dateUpdated;
+    this.postLink = postLink;
+    this.guid = guid;
+    this.wordCount = 0;
+  }
+}
+
+module.exports = Post;
