bbddb6197b93a97d920d86e72e79da1dcfa095a0
Pavan <pavankamra96@gmail.com>
2019-12-02 07:42:36 -0500

    Add Logging to feed parser
    I added logging to the feed parser which shows the title, author, and summary.

--- src/backend/feed/parser.js
+++ src/backend/feed/parser.js
@@ -1,7 +1,15 @@
 const feedparser = require('feedparser-promised');
+const { logger } = require('../utils/logger');
 
+const log = logger.child({ module: 'inactive-blog-filter' });
 // feed: url for the feed
 // Accepts a url feed and parses it, on success will return an array
 module.exports = async function(feed) {
-  return feedparser.parse(feed);
+  const result = feedparser.parse(feed);
+  await result.then(items => {
+    items.forEach(item =>
+      log.info(`title: ${item.title}, author: ${item.author}, summary: ${item.summary} `)
+    );
+  });
+  return result;
 };
