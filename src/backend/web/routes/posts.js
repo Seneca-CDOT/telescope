ac8b4e7e862e826807d7ac08d98636768a137849
Josue <josue.quilon-barrios@senecacollege.ca>
2019-12-02 23:45:27 -0500

    Refactor: Removed unncessary step when getting posts from redis

--- src/backend/web/routes/posts.js
+++ src/backend/web/routes/posts.js
@@ -4,12 +4,9 @@ const { getPost, getPosts } = require('../../utils/storage');
 const posts = express.Router();
 
 posts.get('/', async (req, res) => {
-  const arrayOfPosts = [];
-
   const redisGuids = await getPosts();
 
-  redisGuids.forEach(guid => arrayOfPosts.push(getPost(guid)));
-  const processedPosts = await Promise.all(arrayOfPosts);
+  const processedPosts = await Promise.all(redisGuids.map(guid => getPost(guid)));
 
   const listOfPosts = processedPosts
     // Return id and url for a specific post
