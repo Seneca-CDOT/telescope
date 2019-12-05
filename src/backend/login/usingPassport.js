a6848b6efcbf25b8390f2a0efacfccaac0acdf29
James Inkster <jinkster@myseneca.ca>
2019-12-04 19:01:28 -0500

    fix #424 added ability to login and callback

--- src/backend/login/usingPassport.js
+++ src/backend/login/usingPassport.js
@@ -1,36 +1,36 @@
-const passport = require('passport');
-const OneLoginStrategy = require('passport-openid').Strategy;
+const saml = require('passport-saml');
 require('../lib/config');
+const fs = require('fs');
+const path = require('path');
 
-/* Set environmental variables for these
-https://www.onelogin.com/ to create your account.
-Tutorial is here: https://developers.onelogin.com/quickstart/authentication/nodejs
-OIDC_BASE_URI=https://openid-connect.onelogin.com/oidc
-OIDC_CLIENT_ID=ddf97d-xxx-xxxx-e92920
-OIDC_CLIENT_SECRET=f021f63bxxxx71f2c6a90c9b
-OIDC_REDIRECT_URI=http://localhost:3000/oauth/callback
+/*
+Tutorial is here: https://medium.com/disney-streaming/setup-a-single-sign-on-saml-test-environment-with-docker-and-nodejs-c53fc1a984c9
+SAML2_BASE_URI=https://openid-connect.onelogin.com/oidc
+SAML2_CLIENT_ID=ddf97d-xxx-xxxx-e92920
+SAML2_CLIENT_SECRET=f021f63bxxxx71f2c6a90c9b
+SAML2_REDIRECT_URI=http://localhost:3000/oauth/callback
 */
 
-// Configure the OpenId Connect Strategy
-// with credentials obtained from OneLogin
-const { OIDC_BASE_URI, OIDC_CLIENT_ID, OIDC_CLIENT_SECRET, OIDC_REDIRECT_URI } = process.env;
+const { SAML2_BASE_URI, SAML2_REDIRECT_URI } = process.env;
 
-passport.use(
-  new OneLoginStrategy(
-    {
-      issuer: OIDC_BASE_URI,
-      clientID: OIDC_CLIENT_ID,
-      clientSecret: OIDC_CLIENT_SECRET,
-      authorizationURL: `${OIDC_BASE_URI}/auth`,
-      userInfoURL: `${OIDC_BASE_URI}/me`,
-      tokenURL: `${OIDC_BASE_URI}/token`,
-      callbackURL: OIDC_REDIRECT_URI,
-      passReqToCallback: true,
-    },
-    // This function returns 'issuer:' issuer, 'userId:' userId, 'accessToken:' accessToken, 'refreshToken:' refreshToken, 'params:' params
-    (req, issuer, userId, profile, accessToken, refreshToken, params, callback) => {
-      req.session.accessToken = accessToken;
-      return callback(null, profile);
-    }
-  )
+const samlStrategy = new saml.Strategy(
+  {
+    callbackUrl: SAML2_REDIRECT_URI,
+    entryPoint: SAML2_BASE_URI,
+    issuer: 'saml-poc',
+    identifierFormat: null,
+    decryptionPvk: fs.readFileSync(path.join(__dirname, '/certs/certskey.pem'), 'utf8'),
+    privateCert: fs.readFileSync(path.join(__dirname, '/certs/certskey.pem'), 'utf8'),
+  },
+  function(profile, done) {
+    return done(null, profile);
+  }
 );
+
+// Configure the Saml Strategy
+// with credentials obtained from Saml2 Docker
+// eslint-disable-next-line no-unused-expressions
+// eslint-disable-next-line no-sequences
+module.exports = {
+  samlStrategy,
+};
--- src/backend/web/app.js
+++ src/backend/web/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const healthcheck = require('express-healthcheck');
-
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
 const logger = require('../utils/logger');
 const router = require('./routes');
+const { samlStrategy } = require('../login/usingPassport');
 
 const app = express();
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }));
+
+passport.use('samlStrategy', samlStrategy);
+app.use(passport.initialize({}));
+app.use(passport.session({}));
 
 app.set('logger', logger);
 
@@ -12,5 +26,48 @@ app.use(logger);
 app.use('/health', healthcheck());
 
 app.use('/', router);
+
+app.get(
+  '/login',
+  function(req, res, next) {
+    console.log('-----------------------------');
+    console.log('/Start login handler');
+    next();
+  },
+  passport.authenticate('samlStrategy')
+);
+
+app.post(
+  '/login/callback',
+  function(req, res, next) {
+    console.log('-----------------------------');
+    console.log('/Start login callback ');
+    next();
+  },
+  passport.authenticate('samlStrategy'),
+  function(req, res) {
+    console.log('-----------------------------');
+    console.log('login call back dumps');
+    console.log(req.user);
+    console.log('-----------------------------');
+    res.send('Log in Callback Success');
+  }
+);
+
+passport.serializeUser(function(user, done) {
+  console.log('-----------------------------');
+  console.log('serialize user');
+  console.log(user);
+  console.log('-----------------------------');
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log('-----------------------------');
+  console.log('deserialize user');
+  console.log(user);
+  console.log('-----------------------------');
+  done(null, user);
+});
 
 module.exports = app;
