7eb5cf0172d1aab9b8c3ad9fc64b450b8e4b7ef1
Josue <josue.quilon-barrios@senecacollege.ca>
2019-12-05 01:11:29 -0500

    Altered tests to match changes in storage

--- test/storage.test.js
+++ test/storage.test.js
@@ -1,99 +1,107 @@
-const storage = require('../src/backend/utils/storage');
+const {
+  addFeed,
+  getFeed,
+  getFeeds,
+  getFeedsCount,
+  addPost,
+  getPost,
+  getPosts,
+  getPostsCount,
+} = require('../src/backend/utils/storage');
 
-describe('Tests for storage', () => {
+describe('Tests for feeds', () => {
   const feed = { name: 'James Smith', url: 'http://seneca.co/jsmith' };
   const feed2 = { name: 'James Smith 2', url: 'http://seneca.co/jsmith/2' };
 
   it('inserting a feed returns a feed id', async () => {
-    const feedId = await storage.addFeed(feed.name, feed.url);
+    const feedId = await addFeed(feed.name, feed.url);
     expect(typeof feedId).toBe('number');
   });
 
   it('should allow retrieving a feed by id after inserting', async () => {
-    const feedId = await storage.addFeed(feed2.name, feed2.url);
-    const result = await storage.getFeed(feedId);
+    const feedId = await addFeed(feed2.name, feed2.url);
+    const result = await getFeed(feedId);
     expect(result).toEqual(feed2);
   });
 
   it('check feed count', async () => {
-    const count1 = await storage.getFeedsCount();
-    await storage.addFeed(feed.name, feed.url);
-    const count2 = await storage.getFeedsCount();
+    const count1 = await getFeedsCount();
+    await addFeed(feed.name, feed.url);
+    const count2 = await getFeedsCount();
     expect(count2 - count1).toEqual(1);
   });
 
   it('should return ids of all the feeds', async () => {
-    const feedId1 = await storage.addFeed(feed.name, feed.url);
-    const feedId2 = await storage.addFeed(feed2.name, feed2.url);
-    const result = await storage.getFeeds();
+    const feedId1 = await addFeed(feed.name, feed.url);
+    const feedId2 = await addFeed(feed2.name, feed2.url);
+    const result = await getFeeds();
     expect(result).toContain(feedId1.toString());
     expect(result).toContain(feedId2.toString());
   });
+});
 
-  const post = {
-    guid: 'tag:blogger.com,1999:blog-7100164112302197371.post-522285656016053350',
-    author: 'Neil David',
-    title: 'My First Blog',
-    link: 'http://nadavid.blogspot.com/2008/09/my-first-blog.html',
-    content:
-      "I have never done this before, so let's make this short. This post is just a test on how my blog post will look like.",
-    text: 'post one text',
+describe('Tests for posts', () => {
+  const testPost = {
+    guid: '0',
+    author: 'foo',
+    title: 'foo',
+    link: 'foo',
+    content: 'foo',
+    text: 'foo',
     updated: new Date('2009-09-07T22:23:00.544Z'),
     published: new Date('2009-09-07T22:20:00.000Z'),
-    url: 'http://seneca.co/jsmith',
-    site: 'wordpress.com',
+    url: 'foo',
+    site: 'foo',
   };
 
-  const post2 = {
-    guid: '2tag:blogger.com,1999:blog-7100164112302197371.post-522285656016053350',
-    author: 'Neil David2',
-    title: 'My First Blog2',
-    link: 'http://nadavid2.blogspot.com/2008/09/my-first-blog.html',
-    content:
-      "I have never done this before, so let's make this short. This post is just a test on how my blog post will look like.",
-    text: 'post one text2',
-    updated: new Date('2008-09-07T22:12:00.544Z'),
-    published: new Date('2008-09-07T22:09:00.000Z'),
-    url: 'http://seneca.co/jsmith',
-    site: 'wordpress.com',
+  const testPost2 = {
+    guid: '1',
+    author: 'foo',
+    title: 'foo',
+    link: 'foo',
+    content: 'foo',
+    text: 'foo',
+    updated: new Date('2009-09-07T22:23:00.544Z'),
+    published: new Date('2009-09-07T22:20:00.000Z'),
+    url: 'foo',
+    site: 'foo',
   };
 
-  const post3 = {
-    guid: '3tag:blogger.com,1999:blog-7100164112302197371.post-522285656016053350',
-    author: 'Neil David3',
-    title: 'My First Blog3',
-    link: 'http://nadavid2.blogspot.com/2008/09/my-first-blog.html',
-    content:
-      "I have never done this before, so let's make this short. This post is just a test on how my blog post will look like.",
-    text: 'post one text3',
-    updated: new Date('2008-09-07T22:12:00.544Z'),
-    published: new Date('2008-09-07T22:09:00.000Z'),
-    url: 'http://seneca.co/jsmith',
-    site: 'wordpress.com',
+  const testPost3 = {
+    guid: '2',
+    author: 'foo',
+    title: 'foo',
+    link: 'foo',
+    content: 'foo',
+    text: 'foo',
+    updated: new Date('2009-09-07T22:23:00.544Z'),
+    published: new Date('2009-09-07T22:20:00.000Z'),
+    url: 'foo',
+    site: 'foo',
   };
+
+  beforeAll(() => {
+    Promise.all([testPost, testPost2, testPost3].map(post => addPost(post)));
+  });
 
   it('should allow retrieving a post by guid after inserting', async () => {
-    await storage.addPost(post);
-    const result = await storage.getPost(post.guid);
-    expect(result.link).toEqual(post.link);
+    const result = await getPost(testPost.guid);
+    expect(result.link).toEqual(testPost.link);
   });
 
   it('get all posts returns current number of posts', async () => {
-    await storage.addPost(post2);
-    const result = await storage.getPosts(0, -1);
-    expect(result.length).toEqual(2);
+    const result = await getPosts(0, 0);
+    expect(result.length).toEqual(3);
   });
 
   it('get all posts returns sorted posts by date', async () => {
-    const result = await storage.getPosts(0, -1);
-    expect(result[0]).toEqual(post.guid);
-    expect(result[1]).toEqual(post2.guid);
+    const result = await getPosts(0, 0);
+    expect(result[0]).toEqual(testPost3.guid);
+    expect(result[1]).toEqual(testPost2.guid);
   });
 
   it('check post count', async () => {
-    const count1 = await storage.getPostsCount();
-    await storage.addPost(post3);
-    const count2 = await storage.getPostsCount();
-    expect(count2 - count1).toEqual(1);
+    const count = await getPostsCount();
+    expect(count).toEqual(3);
   });
 });
