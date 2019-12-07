76672396cb87970d4d11191ff2e94e123ae0b51d
Calvin <calvinho125@gmail.com>
2019-12-07 00:17:32 -0500

    fixes issue-462, Integrate inactive blog url and include logic to filter inactive feeds
    added functions for inactive feedsd

--- src/backend/utils/storage.js
+++ src/backend/utils/storage.js
@@ -5,6 +5,7 @@ const FEED_ID = 'feed_id';
 const FEEDS = 'feeds';
 
 const POSTS = 'posts';
+const INACTIVE_FEEDS = 'inactive_feeds';
 
 module.exports = {
   addFeed: async (name, url) => {
@@ -69,4 +70,34 @@ module.exports = {
   getPostsCount: () => redis.zcard(POSTS),
 
   getPost: async guid => redis.hgetall(guid),
+
+  /**
+   * Moves a feed from active(FEEDS) to inactive(INACTIVE_FEEDS)
+   * @param feedId lower index
+   * @return feedId of feed being moved
+   */
+  addInactiveFeed: async feedId => {
+    if ((await redis.exists(INACTIVE_FEEDS)) === 1) {
+      await redis.smove(FEEDS, INACTIVE_FEEDS, feedId);
+    } else {
+      const name = await redis.hget(feedId, 'name');
+      const url = await redis.hget(feedId, 'url');
+      await redis
+        .multi()
+        // Using hmset() until hset() fully supports multiple fields:
+        // https://github.com/stipsan/ioredis-mock/issues/345
+        // https://github.com/luin/ioredis/issues/551
+        .hmset(feedId, 'name', name, 'url', url)
+        .sadd(INACTIVE_FEEDS, feedId)
+        .exec();
+      return feedId;
+    }
+    return feedId;
+  },
+
+  getInactiveFeeds: () => redis.smembers(INACTIVE_FEEDS),
+
+  getInactiveFeed: feedID => redis.hgetall(feedID),
+
+  getInactiveFeedsCount: () => redis.scard(INACTIVE_FEEDS),
 };
