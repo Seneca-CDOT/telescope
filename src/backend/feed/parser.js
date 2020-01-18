8135a3568d3eda5d59ed14c0d6e2f3faae5fc688
David Humphrey <david.humphrey@senecacollege.ca>
2020-01-18 10:07:54 -0500

    Refactor src/backend/feed/parser.js to use package directly

--- src/backend/feed/parser.js
+++
@@ -1,5 +0,0 @@
-const feedparser = require('feedparser-promised');
-
-// feed: url for the feed
-// Accepts a url feed and parses it, on success will return an array
-module.exports = feedparser.parse;
