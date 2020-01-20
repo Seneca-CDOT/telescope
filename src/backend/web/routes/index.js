77d82ef9fa4b5b91376220b6b37ed97848f727fa
David Humphrey <david.humphrey@senecacollege.ca>
2020-01-20 15:08:48 -0500

    Remove server sent events

--- src/backend/web/routes/index.js
+++ src/backend/web/routes/index.js
@@ -5,7 +5,6 @@ const admin = require('./admin');
 const opml = require('./opml');
 const planet = require('./planet');
 const posts = require('./posts');
-const sse = require('./server-sent-events');
 const login = require('./login');
 
 const router = express.Router();
@@ -16,7 +15,6 @@ router.use('/admin', admin);
 router.use('/opml', opml);
 router.use('/planet', planet);
 router.use('/posts', posts);
-router.use('/feed-updates', sse);
 router.use('/login', login);
 
 module.exports = router;
