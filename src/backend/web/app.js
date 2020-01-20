26ce738acb33f2006cd11e460dca8cac0a0c8a34
Josue <josue.quilon-barrios@senecacollege.ca>
2020-01-20 18:25:46 -0500

    Put Apollo server stuff together for better readability

--- src/backend/web/app.js
+++ src/backend/web/app.js
@@ -5,13 +5,21 @@ const healthcheck = require('express-healthcheck');
 const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
 
-// Required for GraphQL
-const { typeDefs, resolvers } = require('./graphql');
-
 const logger = require('../utils/logger');
 const router = require('./routes');
 
+// Required for GraphQL
+const { typeDefs, resolvers } = require('./graphql');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const app = express();
+
+// Add the Apollo server to app and define GraphQL's endpoint
+server.applyMiddleware({ app, path: '/graphql' });
 
 // Template rendering for legacy "planet" view of posts
 app.engine('handlebars', expressHandlebars());
@@ -22,14 +30,6 @@ app.set('logger', logger);
 app.use(logger);
 
 app.use(cors());
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-// Add the Apollo server to app and define GraphQL's endpoint
-server.applyMiddleware({ app, path: '/graphql' });
 
 app.use('/health', healthcheck());
 
