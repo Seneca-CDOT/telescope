759f60a5634dcc41d614bb00d16f0b17d28c97d7
Josue <manekenpix@fastmail.com>
2020-01-21 13:15:44 -0500

    Removed standardizing keys

--- test/storage.test.js
+++ test/storage.test.js
@@ -9,8 +9,6 @@ const {
   getPostsCount,
 } = require('../src/backend/utils/storage');
 
-const standardize = require('../src/backend/utils/standardize');
-
 describe('Storage tests for feeds', () => {
   const feed = { name: 'James Smith', url: 'http://seneca.co/jsmith' };
   const feed2 = { name: 'James Smith 2', url: 'http://seneca.co/jsmith/2' };
@@ -84,7 +82,7 @@ describe('Storage tests for posts', () => {
   beforeAll(() => Promise.all([testPost, testPost2, testPost3].map(post => addPost(post))));
 
   it('should allow retrieving a post by guid after inserting', async () => {
-    const result = await getPost(standardize(testPost.guid));
+    const result = await getPost(testPost.guid);
     expect(result.guid).toEqual(testPost.guid);
   });
 
@@ -95,8 +93,8 @@ describe('Storage tests for posts', () => {
 
   it('get all posts returns sorted posts by date', async () => {
     const result = await getPosts(0, 0);
-    expect(result[0]).toEqual(standardize(testPost3.guid));
-    expect(result[1]).toEqual(standardize(testPost2.guid));
+    expect(result[0]).toEqual(testPost3.guid);
+    expect(result[1]).toEqual(testPost2.guid);
   });
 
   it('check post count', async () => {
