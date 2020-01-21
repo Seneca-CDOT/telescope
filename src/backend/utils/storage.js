1cd68413ad37fb964a870c4be0e338dabebb925a
Josue <manekenpix@fastmail.com>
2020-01-21 15:06:11 -0500

    Refactored code for better readability and fix comment

--- src/backend/utils/storage.js
+++ src/backend/utils/storage.js
@@ -3,26 +3,28 @@ const normalizeUrl = require('normalize-url');
 const { logger } = require('./logger');
 const { redis } = require('../lib/redis');
 
-const standardize = (url, type) => {
-  let namespace = 't:post:';
-  let processed = url;
-
-  if (type === 'feed') {
-    namespace = 't:feed:';
-    processed = normalizeUrl(url);
-  }
+const createKey = (url, prefix) => {
+  const namespace = `t:${prefix}:`;
 
   try {
     return namespace.concat(
       crypto
         .createHash('sha256')
-        .update(processed)
+        .update(url)
         .digest('base64')
     );
   } catch (error) {
     logger.error(`There was an error processing ${url}`);
     throw error;
   }
+};
+
+const createPostKey = uri => {
+  return createKey(normalizeUrl(uri), 'post');
+};
+
+const createFeedKey = uri => {
+  return createKey(uri, 'feed');
 };
 
 // Redis Keys
@@ -32,16 +34,16 @@ const POSTS = 'posts';
 
 module.exports = {
   addFeed: async (name, url) => {
-    const feedId = standardize(url, 'feed');
+    const key = createFeedKey(url, 'feed');
     await redis
       .multi()
       // Using hmset() until hset() fully supports multiple fields:
       // https://github.com/stipsan/ioredis-mock/issues/345
       // https://github.com/luin/ioredis/issues/551
-      .hmset(feedId, 'name', name, 'url', url)
-      .sadd(FEEDS, feedId)
+      .hmset(key, 'name', name, 'url', url)
+      .sadd(FEEDS, key)
       .exec();
-    return feedId;
+    return key;
   },
 
   getFeeds: () => redis.smembers(FEEDS),
@@ -51,7 +53,7 @@ module.exports = {
   getFeedsCount: () => redis.scard(FEEDS),
 
   addPost: async post => {
-    const key = standardize(post.guid, 'post');
+    const key = createPostKey(post.guid, 'post');
 
     await redis
       .multi()
@@ -92,20 +94,20 @@ module.exports = {
     const keys = await redis.zrevrange(POSTS, from, to - 1);
 
     /**
-     * 'zrevrange returns an array of encrypted hashed guids.
+     * 'zrevrange' returns an array of encoded, hashed guids.
      * This array is used to return the 'guid' property in
-     * Post objects, which contains the unencrypted, unhashed version
+     * Post objects, which contains the decoded, unhashed version
      * of the guid
      */
     return Promise.all(
       keys.map(async key => {
         const { guid } = await redis.hgetall(key);
-        return guid.replace('/t:post:/', '');
+        return guid.replace('/^t:post:/', '');
       })
     );
   },
 
   getPostsCount: () => redis.zcard(POSTS),
 
-  getPost: guid => redis.hgetall(standardize(guid, 'post')),
+  getPost: guid => redis.hgetall(createPostKey(guid, 'post')),
 };
