b8788f2df94183d89932b0e65fdbaed40237cf70
Miguel Roncancio <mroncancio19@gmail.com>
2020-02-03 12:58:26 -0500

    add posts and infinite scroll
    add live data and infinite scroll
    
    try axios with https
    
    add app options in web server and use fetch
    
    pass tests
    
    fix duplicate posts bug

--- src/frontend/src/components/Layout/Layout.js
+++ src/frontend/src/components/Layout/Layout.js
@@ -8,8 +8,6 @@ import Logo from '../Logo';
 
 import logo from '../../images/logo.svg';
 
-import data from '../../../../../dummyData.json';
-
 import './Layout.css';
 
 const items = [
@@ -18,23 +16,56 @@ const items = [
   { link: '#', text: 'About' },
 ];
 
+const isScrollBottom = () =>
+  window.innerHeight + window.scrollY >= document.documentElement.scrollHeight;
+
 class Layout extends Component {
   constructor(props) {
     super(props);
     this.state = {
       sideDrawerOpen: false,
       scrolled: false,
+      numPages: 1,
+      posts: [],
     };
     this.drawerToggle = this.drawerToggle.bind(this);
     this.backdropClick = this.backdropClick.bind(this);
   }
 
   componentDidMount() {
+    this.getPosts(this.state.numPages);
     window.addEventListener('scroll', () => {
       const isTop = window.scrollY > 320;
       if (isTop) this.setState({ scrolled: true });
       else this.setState({ scrolled: false });
+
+      if (isScrollBottom()) {
+        this.setState(prevState => ({ numPages: prevState.numPages + 1 }));
+        this.getPosts(this.state.numPages);
+      }
     });
+  }
+
+  async getPosts(pageNum = 1) {
+    let postsData = [];
+    try {
+      const res = await fetch(`http://dev.telescope.cdot.systems/posts?page=${pageNum}`);
+      const postsUrls = await res.json();
+      postsData = await Promise.all(
+        postsUrls.map(async ({ url }) => {
+          const tmp = await fetch(`http://dev.telescope.cdot.systems${url}`);
+          const post = tmp.json();
+          return post;
+        })
+      );
+    } catch (err) {
+      console.log({ err });
+    }
+
+    this.setState(prevState => ({
+      posts: [...prevState.posts, ...postsData],
+      numPages: prevState.numPages + 1,
+    }));
   }
 
   drawerToggle() {
@@ -43,7 +74,6 @@ class Layout extends Component {
 
   backdropClick() {
     this.setState({ sideDrawerOpen: false });
-    console.log('click');
   }
 
   render() {
@@ -63,7 +93,7 @@ class Layout extends Component {
         />
         <Backdrop click={this.backdropClick} show={this.state.sideDrawerOpen} />
         <main className="main">
-          <TextArea className="text-area" style="height: 100%" posts={data} />
+          <TextArea className="text-area" style="height: 100%" posts={this.state.posts} />
         </main>
         <footer>© {new Date().getFullYear()}</footer>
       </>
