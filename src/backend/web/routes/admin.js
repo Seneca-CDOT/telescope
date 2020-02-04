331778703ce11b68ee065e7f02a226ce5a3228da
Josue <josue.quilon-barrios@senecacollege.ca>
2020-02-04 00:46:02 -0500

    Add /log endpoint to request telescope's logs

--- src/backend/web/routes/admin.js
+++ src/backend/web/routes/admin.js
@@ -1,8 +1,35 @@
+require('../../lib/config');
 const express = require('express');
 const { UI } = require('bull-board');
+const fs = require('fs');
+const { logger } = require('../../utils/logger');
 
 const router = express.Router();
 
 router.use('/queues', UI);
+
+router.get('/log', (req, res) => {
+  let readStream;
+  if (!process.env.LOG_FILE) {
+    res.send('LOG_FILE undefined in .env file');
+    return;
+  }
+  try {
+    readStream = fs.createReadStream(process.env.LOG_FILE);
+
+    res.append('Content-type', 'text/plain');
+    readStream.pipe(res).on('error', error => {
+      logger.error({ error });
+      readStream.destroy();
+    });
+
+    res.on('error', error => {
+      logger.error({ error });
+      readStream.destroy();
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
 module.exports = router;
