5547ec8ca90d22e735292a16016e5dbff72a5aa3
Krystyna Lopez <klopez8@myseneca.ca>
2020-02-05 01:11:26 -0500

    Fix #580: updating package.json, package.json in src/frontend and gatsby-config.js

--- src/frontend/gatsby-config.js
+++ src/frontend/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Telescope`,
+    description: `A tool for tracking blogs in orbit around Seneca's open source involvement`,
+    author: `ICT Students and professors`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-for-telescope`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -27,8 +27,5 @@ module.exports = {
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
