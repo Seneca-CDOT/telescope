cfe8b3f6f283472a86bc20068acfde5a63a2abc5
Rafi <raungar@gmail.com>
2020-02-06 20:00:17 -0800

    add structural test for wiki feeds

--- test/feeds-wiki.test.js
+++ test/feeds-wiki.test.js
@@ -1,15 +1,24 @@
 const request = require('supertest');
 const app = require('../src/backend/web/app');
+const Feed = require('../src/backend/data/feed');
 
 describe('GET "/feed/wiki" endpoint', () => {
   let response;
+  const testFeedAuthor = 'humphd';
+  const testFeedURL = 'https://blog.humphd.org/tag/seneca/rss/';
 
   beforeAll(async () => {
+    // Add test feed to mock Redis
+    await new Feed(testFeedAuthor, testFeedURL).save();
     response = await request(app).get('/feed/wiki');
   });
 
   it('should respond with status 200 and content-type text/plain', () => {
     expect(response.status).toEqual(200);
     expect(response.get('Content-type')).toContain('text/plain');
+  });
+
+  it('should respond with text that matches the expected format for wiki feeds', () => {
+    expect(response.text.includes(`[${testFeedURL}]\nname=${testFeedAuthor}`)).toBe(true);
   });
 });
