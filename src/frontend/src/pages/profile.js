95cff5cbdb62d973a9673e456d51acd915a16dd5
James Inkster <jinkster@myseneca.ca>
2020-02-07 03:46:13 -0500

    fix #635 #636 #614 some serious dog food!

--- src/frontend/src/pages/profile.js
+++ src/frontend/src/pages/profile.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+import { isLoggedIn, loginUser, getUser } from '../services/auth';
 
-export default function ProfilePage() {
-  return (
-    <>
-      <button type="button" className="btn btn-primary">
-        Login
-      </button>
-    </>
-  );
+class Profile extends Component {
+  // Initialize the state
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+    };
+  }
+
+  clickMe() {
+    window.location.replace('/auth/login');
+  }
+
+  componentDidMount() {
+    this.setState({ user: getUser() });
+    try {
+      loginUser();
+    } catch (Exception) {
+      console.log(Exception);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {isLoggedIn() ? (
+          <div style={{ backgroundColor: 'white' }}>
+            <li style={{ fontSize: '24px;' }}>E-mail: {this.state.user.email}</li>
+          </div>
+        ) : (
+          <button type="button" onClick={this.clickMe}>
+            Login
+          </button>
+        )}
+      </div>
+    );
+  }
 }
+
+export default Profile;
--- src/frontend/src/services/auth.js
+++ src/frontend/src/services/auth.js
@@ -1,21 +1,36 @@
 export const isBrowser = () => typeof window !== 'undefined';
 export const getUser = () =>
-  isBrowser() && window.localStorage.getItem('gatsbyUser')
-    ? JSON.parse(window.localStorage.getItem('gatsbyUser'))
+  isBrowser() && window.localStorage.getItem('telescopeUser')
+    ? JSON.parse(window.localStorage.getItem('telescopeUser'))
     : {};
-const setUser = user => window.localStorage.setItem('gatsbyUser', JSON.stringify(user));
-export const handleLogin = ({ username }) => {
-  if (username === `john`) {
-    return setUser({
-      email: `johnny@example.org`,
-    });
-  }
-  return false;
-};
 export const isLoggedIn = () => {
   const user = getUser();
   return !!user.email;
 };
+
+export const setUser = user => window.localStorage.setItem('telescopeUser', JSON.stringify(user));
+export const loginUser = () => {
+  fetch('../user/info', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw Error(`HTTP ${response.status}, ${response.statusText}`);
+    })
+    .then(response => {
+      return setUser(response);
+    })
+    .catch(error => {
+      // Handles an error thrown above, as well as network general errors
+      console.log(error);
+    });
+};
+
 export const logout = callback => {
   setUser({});
   callback();
