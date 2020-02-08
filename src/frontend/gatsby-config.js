25ffe01ad26c0d8f5adb3cf094e3442fe1663525
Krystyna Lopez <49209351+lozinska@users.noreply.github.com>
2020-02-07 22:29:45 -0500

    Fix #580: updating package.json, package.json in src/frontend and gatsby-config.js (#656)
    * Fix #580: updating package.json, package.json in src/frontend and gatsby-config.js
    
    * Fix #580: Remove redundant information from frontend/package.json and gatsby-config.js
    
    * Fix #580: Updating src/frontend/package.json and gatsby-config.js
    
    * Fix #580: Updating src/frontend/package.json and gatsby-config.js
    
    * Fix #580: Updating src/frontend/package.json and gatsby-config.js

--- src/frontend/gatsby-config.js
+++ src/frontend/gatsby-config.js
@@ -2,9 +2,9 @@ const proxy = require(`http-proxy-middleware`);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Telescope`,
+    description: `A tool for tracking blogs in orbit around Seneca's open source involvement`,
+    author: `SDDS Students and professors`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-for-telescope`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -29,9 +29,6 @@ module.exports = {
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
   developMiddleware: app => {
     app.use(
