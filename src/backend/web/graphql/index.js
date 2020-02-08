83463f3dc5d5ac591c19d669e38d3e0613f4a787
Calvin <calvinho125@gmail.com>
2020-02-07 22:28:26 -0500

    hackity hack hack

--- src/backend/web/graphql/index.js
+++ src/backend/web/graphql/index.js
@@ -1,5 +1,6 @@
 const { GraphQLDate } = require('graphql-iso-date');
 const { gql: graphql } = require('apollo-server-express');
+const normalize = require('normalize-url');
 
 const {
   getFeedsCount,
@@ -112,37 +113,56 @@ module.exports.resolvers = {
      * @return Array of 'perPage' number of Post objects
      */
     getPosts: async (parent, { filter, page, perPage }) => {
+      let postIds;
+      const numOfPosts = await getPostsCount();
       const prPage = perPage > maxPostsPerPage ? maxPostsPerPage : perPage;
 
-      const numOFPosts = await getPostsCount();
-      const first = page * prPage;
-
-      if (first < numOFPosts) {
-        const last = first + prPage < numOFPosts ? first + prPage : numOFPosts;
-        const postIds = await getPosts(first, last);
-
+      // For Date Filter
+      if (filter) {
+        if (filter.fromDate || filter.toDate) {
+          const fromDate = filter.fromDate ? filter.fromDate : new Date();
+          const toDate = filter.toDate ? filter.toDate : new Date();
+          postIds = await getPostsByDate(fromDate, toDate); // Get all results within the date
+          // check if # of filtered results are less than max results allowed per page.
+          if (postIds.length < prPage) {
+            return Promise.all(postIds.map(id => Post.byId(id))); // return all the posts' info
+          }
+          // Otherwise do some pagination here
+          const pageResult = postIds.slice(page - 1 * perPage, page * prPage);
+          return Promise.all(pageResult.map(id => Post.byId(id)));
+        }
+        // Other filters use the getPosts() instead of getPostsByDate()
+        postIds = await getPosts(0, numOfPosts);
         const result = await Promise.all(postIds.map(id => Post.byId(id)));
 
-        if (filter) {
-          // check if author name is equal to what we're searching for
-          if (filter.author) {
-            return result.filter(post => post.author === filter.author);
+        if (filter.url) {
+          const authorResults = result.filter(post => post.author === filter.author);
+          // check if # of filtered results are less than max results allowed per page.
+          if (authorResults.length < prPage) {
+            return authorResults;
           }
-          // check if post's date is between two provided dates
-          if (filter.fromDate || filter.toDate) {
-            const fromDate = filter.fromDate ? filter.fromDate : new Date();
-            const toDate = filter.toDate ? filter.toDate : new Date();
-            const datePostIds = await getPostsByDate(fromDate, toDate);
-            return result.filter(post => datePostIds.includes(post.id));
-          }
-          // check if url is equal to what we're searching for
-          if (filter.url) {
-            return result.filter(post => post.url === filter.url);
-          }
+          return authorResults.slice(page - 1 * perPage, page * prPage);
         }
 
-        return result;
+        if (filter.author) {
+          const urlResults = result.filter(post => post.url === normalize(filter.url));
+          // check if # of filtered results are less than max results allowed per page.
+          if (urlResults.length < prPage) {
+            return urlResults;
+          }
+          return urlResults.slice(page - 1 * perPage, page * prPage);
+        }
       }
+
+      const first = page * prPage;
+
+      if (first < numOfPosts) {
+        const last = first + prPage < numOfPosts ? first + prPage : numOfPosts;
+        postIds = await getPosts(first, last);
+
+        return Promise.all(postIds.map(id => Post.byId(id)));
+      }
+
       return [];
     },
 
