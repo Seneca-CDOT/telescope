a9fa96f935f2497feb61d40f6480100fd2077b58
David Humphrey <david.humphrey@senecacollege.ca>
2020-02-09 10:44:46 -0500

    Fix #664: Use API_URL from .env for telescope API

--- src/frontend/src/components/Login/Login.jsx
+++ src/frontend/src/components/Login/Login.jsx
@@ -1,21 +1,30 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
+import useSiteMetadata from '../../hooks/use-site-metadata';
+
 import './Login.css';
 
 function LoggedIn(props) {
+  const { telescopeUrl } = useSiteMetadata();
+  const logoutUrl = `${telescopeUrl}/auth/logout`;
+
   return (
     <div>
-      Welcome {props.email} <a href="/auth/logout"> Logout </a>
+      Welcome {props.email} <a href={logoutUrl}> Logout </a>
       <img className="avatar" src={`https://unavatar.now.sh/${props.email}`} alt={props.email} />
     </div>
   );
 }
 
 function LoggedOut() {
+  const { telescopeUrl } = useSiteMetadata();
+  const loginUrl = `${telescopeUrl}/auth/login`;
+
   return (
     <div>
-      <a href="/auth/login">Login</a>
+      <a href={loginUrl}>Login</a>
     </div>
   );
 }
