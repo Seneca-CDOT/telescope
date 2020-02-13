95b29afdef80dcf9cc1441ac90cde5a3a45e5fcf
Josue <josue.quilon-barrios@senecacollege.ca>
2020-02-13 17:31:51 -0500

    Add elasticsearch

--- src/backend/feed/worker.js
+++ src/backend/feed/worker.js
@@ -1,8 +1,10 @@
+require('../lib/config');
 const { cpus } = require('os');
 const path = require('path');
 
 const feedQueue = require('./queue');
 const { logger } = require('../utils/logger');
+const { checkConnection } = require('../lib/elastic');
 
 /**
  * We determine the number of parallel feed processor functions to run
@@ -25,7 +27,39 @@ function getFeedWorkersCount() {
   return Math.min(count, cpuCount);
 }
 
-exports.start = function() {
+exports.start = async function() {
+  /**
+   * Elasticsearch needs time after deployment for setting up all its components.
+   * Here we set a timer using 'setTimeout' and check for connectivity during the countdown so elasticsearch
+   * has time to be fully prepared to start indexing posts.
+   */
+  const DELAY = process.env.ELASTIC_DELAY || 10000;
+  let intervalId;
+  let timerId;
+
+  const timer = new Promise((resolve, reject) => {
+    timerId = setTimeout(reject, DELAY);
+  });
+
+  const connectivity = new Promise(resolve => {
+    intervalId = setInterval(() => {
+      checkConnection()
+        .then(resolve)
+        .catch(() => logger.info('Attempting to connect to elasticsearch...'));
+    }, 500);
+  });
+
+  try {
+    await Promise.race([timer, connectivity]);
+    logger.info('Connected to elasticsearch!');
+  } catch (error) {
+    logger.error(
+      `Unable to connect to initialize Elasticsearch before deployment, some posts might not have been indexed: ${error}`
+    );
+  }
+  clearInterval(intervalId);
+  clearTimeout(timerId);
+
   const concurrency = getFeedWorkersCount();
   logger.info(`Starting ${concurrency} instance${concurrency > 1 ? 's' : ''} of feed processor.`);
   feedQueue.process(concurrency, path.resolve(__dirname, 'processor.js'));
---
+++ src/backend/lib/elastic.js
@@ -0,0 +1,85 @@
+require('../lib/config');
+
+const { ELASTIC_URL, ELASTIC_PORT, ELASTIC_MAX_RESULTS } = process.env;
+const { Client } = require('@elastic/elasticsearch');
+
+const elasticUrl = `${ELASTIC_URL}:${ELASTIC_PORT}` || 'http://localhost:9200';
+const esClient = new Client({ node: elasticUrl });
+
+const logger = require('../utils/logger');
+
+const index = 'posts';
+const type = 'post';
+
+/**
+ * Indexes the text  and id from a post
+ * @param text from a post
+ * @param postId same id used to store on redis the post object where the text is from
+ */
+const indexPost = (text, postId) => {
+  try {
+    esClient.index({
+      index,
+      type,
+      id: postId,
+      body: {
+        text,
+      },
+    });
+  } catch (error) {
+    logger.error(`There was an error indexing a post for id ${postId}: ${error}`);
+  }
+};
+
+/**
+ * Searches text in elasticsearch
+ * @param textToSearch
+ * @return all the results matching the passed text
+ */
+const getIndexResults = async textToSearch => {
+  const query = {
+    query: {
+      match: {
+        text: {
+          query: textToSearch,
+          operator: 'AND',
+          fuzziness: 'auto',
+        },
+      },
+    },
+  };
+
+  const {
+    body: { hits },
+  } = await esClient.search({
+    from: 0,
+    size: ELASTIC_MAX_RESULTS || 100,
+    index,
+    type,
+    body: query,
+  });
+  const results = hits.total.value;
+  const values = hits.hits.map(({ _id, _source, _score }) => {
+    return {
+      id: _id,
+      text: _source.text,
+      score: _score,
+    };
+  });
+
+  return {
+    results,
+    values,
+  };
+};
+
+/**
+ * Checks elasticsearch's connectivity
+ */
+const checkConnection = () => esClient.cluster.health();
+
+module.exports = {
+  indexPost,
+  checkConnection,
+  getIndexResults,
+};
