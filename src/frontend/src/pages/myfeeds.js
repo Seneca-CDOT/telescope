b3162f9e1a5b5c960cf08f30f13ab55bc4c264d1
Rafi <raungar@gmail.com>
2020-02-16 19:41:21 -0800

    initial MaterialUI templating for Add Feeds component

---
+++ src/frontend/src/pages/myfeeds.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Container, Box, Typography, TextField, Grid, Card, IconButton } from '@material-ui/core';
+import { AccountCircle, RssFeed, HelpOutline, Add } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(2),
+  },
+  button: {
+    padding: '3px 0 3px 0',
+  },
+}));
+
+export default function MyFeeds() {
+  const classes = useStyles();
+  return (
+    <div className={classes.margin}>
+      <Container maxWidth="xs" bgcolor="aliceblue">
+        <Card>
+          <Box px={2} py={1}>
+            <Typography variant="h3" component="h3" align="center">
+              My Feeds
+            </Typography>
+            <Grid container spacing={5}>
+              <Grid item>
+                <Grid container spacing={1} alignItems="flex-end">
+                  <Grid item>
+                    <AccountCircle />
+                  </Grid>
+                  <Grid item>
+                    <TextField disabled label="John Doe" />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container spacing={1} alignItems="flex-end">
+                  <Grid item>
+                    <RssFeed />
+                  </Grid>
+                  <Grid item>
+                    <TextField label="Blog feed URL" />
+                  </Grid>
+                  <Grid item>
+                    <IconButton color="primary" classes={{ root: classes.button }}>
+                      <HelpOutline />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                  <Grid item>
+                    <IconButton classes={{ root: classes.button }}>
+                      <Add />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+        </Card>
+      </Container>
+    </div>
+  );
+}
