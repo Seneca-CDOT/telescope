3bf6e0b17b0734b1c2c18817b54a6f60be5bc6c2
c3ho <calvinho125@gmail.com>
2020-02-24 14:26:19 -0500

    fixed with review from cindy

--- src/frontend/src/components/AuthorResult/AuthorResult.jsx
+++ src/frontend/src/components/AuthorResult/AuthorResult.jsx
@@ -6,12 +6,14 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
-import { deepOrange } from '@material-ui/core/colors';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,49 +21,47 @@ const useStyles = makeStyles(theme => ({
   },
   paper: {
     color: theme.palette.text.secondary,
-    maxWidth: 500,
     margin: `${theme.spacing(1)}px auto`,
   },
   imageBox: {
     height: 110,
     width: 128,
-    backgroundColor: '#9E9E9E',
+    backgroundColor: '#444444',
   },
   avatar: {
     height: 100,
     width: 100,
-    color: theme.palette.getContrastText(deepOrange[500]),
-    backgroundColor: deepOrange[500],
+    margin: 'auto',
+    backgroundColor: '#3670A5',
   },
   icons: {
-    marginTop: '-2px',
-    paddingLeft: '20px',
-    paddingRight: '10px',
+    color: '#3670A5',
   },
   infoBox: {
-    marginTop: '10px',
+    backgroundColor: '#9E9E9E',
   },
   infoLine: {
     paddingBottom: '5px',
+    marginTop: '-12px',
+    marginBottom: '-10px',
   },
   font: {
     color: '#002944',
     fontFamily: 'Roboto',
-    fontSize: '14px',
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    marginTop: '-20px',
-    marginBottom: '',
+    marginLeft: '-15px',
   },
   expand: {
-    marginTop: '',
     marginBottom: '-20px',
   },
 }));
 
-const AuthorResult = props => {
+export default function AuthorResult() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className={classes.root}>
@@ -71,58 +71,45 @@ const AuthorResult = props => {
             <Avatar className={classes.avatar}>CS</Avatar>
           </Grid>
           <Grid xs container direction="column" spacing={0} className={classes.infoBox}>
-            <Grid container direction="row" className={classes.infoLine} spacing={0}>
-              <Grid item>
-                <PermContactCalendarIcon className={classes.icons} />
-              </Grid>
-              <Grid item className={classes.font}>
-                {
-                  // username
-                }
-              </Grid>
-            </Grid>
-            <Grid container direction="row" className={classes.infoLine} spacing={0}>
-              <Grid item>
-                <EventIcon className={classes.icons} />
-              </Grid>
-              <Grid item className={classes.font}>
-                Date of Last Post:
-                {
-                  // last post date
-                }
-              </Grid>
-            </Grid>
-            <Grid container direction="row" className={classes.infoLine} spacing={0}>
-              <Grid item>
-                <CreateIcon className={classes.icons} />
-              </Grid>
-              <Grid item className={classes.font}>
-                Latest Post:
-                {
-                  // latest post info
-                }
-              </Grid>
-              <ExpansionPanel className={classes.expand}>
-                <ExpansionPanelSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1c-content"
-                  id="panel1c-header"
-                >
-                  <Typography className={classes.heading}>More</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                  <Typography>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-                    lacus ex, sit amet blandit leo lobortis eget.
-                  </Typography>
-                </ExpansionPanelDetails>
-              </ExpansionPanel>
+            <Grid container direction="row" spacing={0}>
+              <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                className={classes.root}
+              >
+                <ListItem button className={classes.infoLine}>
+                  <ListItemIcon className={classes.icons}>
+                    <PermContactCalendarIcon />
+                  </ListItemIcon>
+                  <ListItemText className={classes.font} primary="Username" />
+                </ListItem>
+                <ListItem button className={classes.infoLine}>
+                  <ListItemIcon className={classes.icons}>
+                    <EventIcon />
+                  </ListItemIcon>
+                  <ListItemText className={classes.font} primary="Date of Last Post: " />
+                </ListItem>
+                <ListItem button className={classes.infoLine} onClick={handleClick}>
+                  <ListItemIcon className={classes.icons}>
+                    <CreateIcon />
+                  </ListItemIcon>
+                  <ListItemText className={classes.font} primary="Latest Post: " />
+                  <Typography>More</Typography>
+                  {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon />
+                      <ListItemText className={classes.font} primary="Another Post" />
+                    </ListItem>
+                  </List>
+                </Collapse>
+              </List>
             </Grid>
           </Grid>
         </Grid>
       </Paper>
     </div>
   );
-};
-
-export default AuthorResult;
+}
