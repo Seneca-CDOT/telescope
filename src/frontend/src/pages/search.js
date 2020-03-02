465288e57d481bf7da18d8a5e509b66af25211d0
Ana Garcia <agarcia-caicedo@myseneca.ca>
2020-03-01 22:06:08 -0500

    Temp fix to prevent search bar being hidden

--- src/frontend/src/pages/search.js
+++ src/frontend/src/pages/search.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      scrolled: false,
-    };
-  }
-
-  render() {
-    return (
-      <>
-        <Header className="header" scrolled={this.state.scrolled} />
-        <SearchBar />
-      </>
-    );
-  }
-}
+const Search = () => {
+  return (
+    <div>
+      <Header />
+      <div
+        style={{
+          height: '12vh',
+        }}
+      ></div>
+      <SearchBar />
+    </div>
+  );
+};
 
 export default Search;
