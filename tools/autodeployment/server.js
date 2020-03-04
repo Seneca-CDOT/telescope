806e4ba5e6623c3222fa913e754edeb69dca26a8
josue <josue.quilon-barrios@senecacollege.ca>
2020-03-03 22:43:20 -0500

    Add automatic deployment

---
+++ tools/autodeployment/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+const shell = require('shelljs');
+
+const { SECRET, REPO_NAME, DEPLOY_PORT } = process.env;
+
+const handler = createHandler({ path: '/', secret: SECRET });
+
+http.createServer((req, res) => {
+  handler(req, res, err => {
+    res.statusCode = 404
+    res.end('Page not found');
+  })
+}).listen(CD_PORT, () => {
+    console.log(`Server listening on port ${DEPLOY_PORT}`);
+});
+
+handler.on('error', err => {
+  console.error('Error:', err.message)
+});
+
+handler.on('push', event => {
+  const { name } = event.payload.repository;
+
+  if (name === REPO_NAME) {
+    shell.exec('./deploy_stage');
+  }
+});
