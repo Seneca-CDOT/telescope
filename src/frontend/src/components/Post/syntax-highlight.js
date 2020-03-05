ba07974cb0d8b1086cdb4a9b778942b5dc65eaa6
David Humphrey <david.humphrey@senecacollege.ca>
2020-03-04 21:53:12 -0500

    Add automatic syntax highlighting for <pre> post elements

---
+++ src/frontend/src/components/Post/syntax-highlight.js
@@ -0,0 +1,66 @@
+import hljs from 'highlight.js/lib/highlight';
+
+// Tweak the language list here, see https://highlightjs.org/usage/
+import cpp from 'highlight.js/lib/languages/cpp';
+import sql from 'highlight.js/lib/languages/sql';
+import powershell from 'highlight.js/lib/languages/powershell';
+import cs from 'highlight.js/lib/languages/cs';
+import go from 'highlight.js/lib/languages/go';
+import css from 'highlight.js/lib/languages/css';
+import makefile from 'highlight.js/lib/languages/makefile';
+import markdown from 'highlight.js/lib/languages/markdown';
+import swift from 'highlight.js/lib/languages/swift';
+import armasm from 'highlight.js/lib/languages/armasm';
+import diff from 'highlight.js/lib/languages/diff';
+import python from 'highlight.js/lib/languages/python';
+import dockerfile from 'highlight.js/lib/languages/dockerfile';
+import http from 'highlight.js/lib/languages/http';
+import typescript from 'highlight.js/lib/languages/typescript';
+import ini from 'highlight.js/lib/languages/ini';
+import bash from 'highlight.js/lib/languages/bash';
+import nginx from 'highlight.js/lib/languages/nginx';
+import java from 'highlight.js/lib/languages/java';
+import javascript from 'highlight.js/lib/languages/javascript';
+import rust from 'highlight.js/lib/languages/rust';
+import json from 'highlight.js/lib/languages/json';
+import x86asm from 'highlight.js/lib/languages/x86asm';
+import kotlin from 'highlight.js/lib/languages/kotlin';
+import xml from 'highlight.js/lib/languages/xml';
+import shell from 'highlight.js/lib/languages/shell';
+import yaml from 'highlight.js/lib/languages/yaml';
+
+import 'highlight.js/styles/github.css';
+
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('powershell', powershell);
+hljs.registerLanguage('cs', cs);
+hljs.registerLanguage('go', go);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('makefile', makefile);
+hljs.registerLanguage('markdown', markdown);
+hljs.registerLanguage('swift', swift);
+hljs.registerLanguage('armasm', armasm);
+hljs.registerLanguage('diff', diff);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('dockerfile', dockerfile);
+hljs.registerLanguage('http', http);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('ini', ini);
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('nginx', nginx);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('rust', rust);
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('x86asm', x86asm);
+hljs.registerLanguage('kotlin', kotlin);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('yaml', yaml);
+
+export default function(node) {
+  if (node) {
+    node.querySelectorAll('pre').forEach(pre => hljs.highlightBlock(pre));
+  }
+}
