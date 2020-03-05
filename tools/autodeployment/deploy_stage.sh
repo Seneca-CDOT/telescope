8c02211b0312cfc03cbd72cba4519b09c70a3a25
Josue <josue.quilon-barrios@senecacollege.ca>
2020-03-05 11:57:37 -0500

    Closes #699: Automatic deploys to staging on commit to master (#800)
    * Add automatic deployment

---
+++ tools/autodeployment/deploy_stage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Shutdown
+cd ../telescope
+
+docker-compose -f docker-compose-staging.yml down
+
+docker system prune -af
+
+
+# Delete and clone
+cd ..
+
+rm -rf telescope
+
+git clone https://github.com/Seneca-CDOT/telescope.git
+
+
+# Deploy
+cd telescope/
+
+cp env.staging .env
+
+docker-compose -f docker-compose-staging.yml up -d
---
+++ tools/autodeployment/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+const shell = require('shelljs');
+
+const { SECRET, REPO_NAME, DEPLOY_PORT } = process.env;
+
+const handler = createHandler({ path: '/', secret: SECRET });
+
+http
+  .createServer((req, res) => {
+    handler(req, res);
+  })
+  .listen(DEPLOY_PORT, () => {
+    console.log(`Server listening on port ${DEPLOY_PORT}`);
+  });
+
+handler.on('error', err => {
+  console.error('Error:', err.message);
+});
+
+handler.on('push', event => {
+  const { name } = event.payload.repository;
+
+  if (name === REPO_NAME) {
+    shell.exec('./deploy_stage.sh');
+  }
+});
