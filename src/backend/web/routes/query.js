4f89d0eebc99b0e6c99c208bc20d1e25d3886684
Calvin <calvinho125@gmail.com>
2020-03-11 15:43:43 -0400

    added elasticSearch function

--- src/backend/web/routes/query.js
+++ src/backend/web/routes/query.js
@@ -1,15 +1,33 @@
 const express = require('express');
 
 const { logger } = require('../../utils/logger');
+const { search } = require('../../utils/elastic');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // Github rule, query is more than 256 chars or contains more than 5 operators
-  // return a validation failed message
-  if (req.query.q.length > 256 || req.query.q.match(/&& | \|\| | !/g).length > 5) {
-    res.send('Validation failed');
-    logger.info('Validation failed');
+router.get('/', async (req, res, next) => {
+  // Github rule, query is greater than 256 chars return a validation failed message
+  const maxSearchLength = 256;
+
+  if (!req.query.search) {
+    const error = new Error('Required query param missing when using query route');
+    logger.error({ error });
+    error.status = 400;
+    res.status(error);
+    return next(error);
+  }
+
+  if (req.query.search.length > maxSearchLength) {
+    const error = 'Validation failed, query length is greater than 256 characters.';
+    res.send(error);
+    logger.error(error);
+  }
+
+  try {
+    res.send(await search(req.query.search));
+  } catch (error) {
+    res.send('There was an error while executing your query.');
+    logger.error({ error }, 'Something went wrong with search indexing');
   }
 });
 
