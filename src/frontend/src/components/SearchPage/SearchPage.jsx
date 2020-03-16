2ceb0e19f0b0e843febf8bbe7f4c0645d9bb1751
Calvin <calvinho125@gmail.com>
2020-03-16 01:24:07 -0400

    initial work on no results/no query string/results

--- src/frontend/src/components/SearchPage/SearchPage.jsx
+++ src/frontend/src/components/SearchPage/SearchPage.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { useLazyQuery } from '@apollo/react-hooks';
+import { makeStyles } from '@material-ui/core/styles';
 import gql from 'graphql-tag';
 import Header from '../Header';
 import SearchBar from '../SearchBar';
 import AuthorResult from '../AuthorResult';
 
+const useStyles = makeStyles(() => ({
+  searchReply: {
+    margin: 'auto',
+    padding: '2rem',
+    color: 'coral',
+    fontWeight: 400,
+  },
+  divAfterHeader: {
+    height: '12vh',
+    minHeight: '12em',
+  },
+}));
+
 const SearchPage = () => {
+  const classes = useStyles();
   const SEARCH_QUERY = gql`
     query SearchAuthorQuery($author: String!) {
       getPosts(page: 0, perPage: 25, filter: { author: $author }) {
@@ -22,10 +37,12 @@ const SearchPage = () => {
 
   const [searchText, setSearchText] = useState('');
   const [results, setResults] = useState([]);
+  const [searchTextIsEmpty, setSearchTextIsEmpty] = useState(true);
 
   const [executeSearch, { data }] = useLazyQuery(SEARCH_QUERY, {
     // setting author variable here to use in the query above
     variables: { author: searchText },
+
     onCompleted: () => {
       const returnedIds = [];
       // We only want to display an author once, this would avoid the issue if
@@ -53,39 +70,46 @@ const SearchPage = () => {
   });
 
   // Hook that will re-render the page only if the state of results changes
-  useEffect(() => {}, [results]);
+  useEffect(() => {}, []);
+
+  function onChangeHandler(event) {
+    setSearchText(event.target.value);
+    if (searchText.length > 0) {
+      setSearchTextIsEmpty(false);
+    } else {
+      setSearchTextIsEmpty(true);
+    }
+  }
+
+  const displayResults = () => {
+    // The initial state of results is going to be 0 and we can't map 0,
+    //  this will check if there are any results and return AuthorResult component
+    //  for each with feed guid as key, otherwise default to "No Results"
+    if (results.length > 0)
+      return results.map(result => (
+        <AuthorResult key={result.id} author={result.author} post={result.post} />
+      ));
+    if (searchText.length > 0 && results.length < 1)
+      return <h1 className={classes.searchReply}>No results found</h1>;
+  };
 
   function onClickHandler() {
     executeSearch();
   }
 
-  function onChangeHandler(event) {
-    setSearchText(event.target.value);
-  }
-
   return (
     <div>
       <Header />
-      <div
-        style={{
-          height: '12vh',
-        }}
-      ></div>
+      <div className={classes.divAfterHeader}></div>
       <SearchBar
         onClickHandler={onClickHandler}
         searchText={searchText}
         onChangeHandler={onChangeHandler}
       />
-      {// The initial state of results is going to be 0 and we can't map 0,
-      //  this will check if there are any results and return AuthorResult component
-      //  for each with feed guid as key, otherwise default to "No Results"
-      results.length > 0 ? (
-        results.map(result => {
-          console.log(results);
-          return <AuthorResult key={result.id} author={result.author} post={result.post} />;
-        })
+      {!searchTextIsEmpty ? (
+        displayResults()
       ) : (
-        <h1>No Results</h1>
+        <h1 className={classes.searchReply}>No search terms entered</h1>
       )}
     </div>
   );
