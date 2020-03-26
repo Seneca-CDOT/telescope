e3f430dd7832336d450ae1afbf29e710666e1cff
David Humphrey <david.humphrey@senecacollege.ca>
2020-03-26 16:38:22 -0400

    Fix tests that broke with changes to /health

--- test/server.test.js
+++ test/server.test.js
@@ -4,9 +4,19 @@ const app = require('../src/backend/web/app');
 jest.mock('../src/backend/utils/elastic');
 
 describe('Health Check', () => {
-  it('should return a JSON object with property "uptime"', async () => {
+  it('should return a JSON object with property "status"', async () => {
     const res = await request(app).get('/health');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('uptime');
+    expect(res.body).toHaveProperty('status');
+    expect(res.body.status).toEqual('ok');
+  });
+
+  it('should return a JSON object with property "info"', async () => {
+    const res = await request(app).get('/health');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('info');
+    expect(res.body.info).toHaveProperty('gitHubUrl');
+    expect(res.body.info).toHaveProperty('sha');
+    expect(res.body.info).toHaveProperty('version');
   });
 });
