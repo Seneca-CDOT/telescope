cd2e004e113023d327f65cb6797c6d78bba0c8ab
Cindy Le <cindyledev@gmail.com>
2020-03-29 00:21:23 -0400

    getting started with markdown
        pick 5410d12 getting started with markdown
        pick d3c8b97 got the about page but no Header or styling
        pick be61e41 added header to about page

--- src/frontend/src/pages/about.js
+++ src/frontend/src/pages/about.js
@@ -1,9 +1,22 @@
 import React from 'react';
+<<<<<<< HEAD
 
 import PageBase from './PageBase';
 
 const About = () => {
   return <PageBase title="About"></PageBase>;
+=======
+import { Link } from 'gatsby';
+import Header from '../components/Header';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <Link to="/blog/my-first-post">my-first-post</Link>
+    </div>
+  );
+>>>>>>> cdfb9ba... getting started with markdown
 };
 
 export default About;
