2a74d174939812ebc58673d624d3ce6eceaf1340
Rafi <raungar@gmail.com>
2020-04-04 15:26:30 -0400

    link username to MyFeeds page (#923)

--- src/frontend/src/components/Login/LoggedIn.jsx
+++ src/frontend/src/components/Login/LoggedIn.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Typography } from '@material-ui/core';
+import { Link } from 'gatsby';
 
 import useSiteMetadata from '../../hooks/use-site-metadata';
 
@@ -32,7 +33,9 @@ function LoggedIn(props) {
           Logout
         </a>
       </Button>
-      <Typography variant="h4">| Welcome {props.name}</Typography>
+      <Link to="/myfeeds" className={classes.links}>
+        <Typography variant="h4">| {props.name}</Typography>
+      </Link>
       <img
         className={classes.avatar}
         src={`https://unavatar.now.sh/${props.name}`}
