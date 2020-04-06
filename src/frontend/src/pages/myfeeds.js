f8400aaea7484ac378433c382fb169da120cd585
Rafi Ungar <raungar@gmail.com>
2020-04-06 01:05:20 -0400

    fetch user feeds into separate form controls

--- src/frontend/src/pages/myfeeds.js
+++ src/frontend/src/pages/myfeeds.js
@@ -66,6 +66,8 @@ export default function MyFeeds() {
   const [feedAuthor, setFeedAuthor] = useState('');
   const [feedUrl, setFeedUrl] = useState('');
   const [submitStatus, setSubmitStatus] = useState({ message: '', isError: false });
+  const [userInfo, setUserInfo] = useState({});
+  const [userFeeds, setUserFeeds] = useState([]);
 
   const { telescopeUrl } = useSiteMetadata();
 
@@ -73,7 +75,30 @@ export default function MyFeeds() {
   const feedUrlRef = React.createRef();
   const formRef = React.createRef();
 
+  async function fetchUserFeeds() {
+    const [user, feedItems] = await Promise.all([
+      fetch(`${telescopeUrl}/user/info`).then((res) => res.json()),
+      fetch(`${telescopeUrl}/feeds`).then((res) => res.json()),
+    ]);
+    const allFeeds = await Promise.all(
+      feedItems.map((item) => fetch(`${telescopeUrl}${item.url}`))
+    ).then((responses) => Promise.all(responses.map((res) => res.json())));
+
+    console.log(allFeeds.length, user.id);
+
+    if (allFeeds.length && user) {
+      const feeds = allFeeds.filter((feed) => {
+        return feed.user === user.id;
+      });
+      setUserInfo(user);
+      setUserFeeds(feeds);
+    }
+  }
+
   useEffect(() => {
+    (async function () {
+      await fetchUserFeeds();
+    })();
     ValidatorForm.addValidationRule('isUrl', (value) => !!isWebUri(value));
     return ValidatorForm.removeValidationRule.bind('isUrl');
   }, []);
@@ -86,7 +111,7 @@ export default function MyFeeds() {
     setFeedUrl(url);
   }
 
-  async function addFeed() {
+  async function saveFeeds() {
     try {
       setSubmitStatus({ message: 'Saving feeds, please wait...' });
       const response = await fetch(`${telescopeUrl}/feeds`, {
@@ -95,6 +120,7 @@ export default function MyFeeds() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+          user: userInfo.id,
           author: feedAuthor,
           url: feedUrl,
         }),
@@ -131,16 +157,76 @@ export default function MyFeeds() {
     }
   }
 
+  function buildFormControls() {
+    if (userFeeds.length > 0) {
+      return userFeeds.map((feed) => (
+        <Grid container spacing={5} key={feed.id}>
+          <Grid item>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <AccountCircle />
+              </Grid>
+              <Grid item>
+                <TextValidator
+                  disabled={!userInfo.isAdmin}
+                  label="Blog feed author"
+                  name="author"
+                  // ref={feedAuthorRef}
+                  value={feed.author}
+                  // onBlur={handleBlur}
+                  // onChange={handleChange}
+                  type="string"
+                  validators={['required', 'trim']}
+                  errorMessages={"Please enter the blog's author."}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <RssFeed />
+              </Grid>
+              <Grid item>
+                <TextValidator
+                  label="Blog feed URL"
+                  name="url"
+                  // ref={feedUrlRef}
+                  value={feed.url}
+                  // onBlur={handleBlur}
+                  // onChange={handleChange}
+                  type="url"
+                  validators={['required', 'isUrl']}
+                  errorMessages={"Please enter the blog's feed URL."}
+                />
+              </Grid>
+              <Grid item>
+                <HelpPopoverButton />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      ));
+    }
+    console.log(`No feeds associated with user ${userInfo.id} found.`);
+    return (
+      <Typography align="center">
+        <em>(It looks like you have not added any blog feeds yet.)</em>
+      </Typography>
+    );
+  }
+
   return (
     <PageBase title="My Feeds">
       <div className={classes.margin}>
-        <ValidatorForm ref={formRef} onSubmit={addFeed}>
+        <ValidatorForm ref={formRef} onSubmit={saveFeeds}>
           <Container maxWidth="xs" bgcolor="aliceblue">
             <Card>
               <Box px={2} py={1}>
                 <Typography variant="h3" component="h3" align="center">
                   My Feeds
                 </Typography>
+                {buildFormControls()}
                 <Grid container spacing={5}>
                   <Grid item>
                     <Grid container spacing={1} alignItems="flex-end">
@@ -149,6 +235,7 @@ export default function MyFeeds() {
                       </Grid>
                       <Grid item>
                         <TextValidator
+                          disabled={!userInfo.isAdmin}
                           label="Blog feed author"
                           name="author"
                           ref={feedAuthorRef}
