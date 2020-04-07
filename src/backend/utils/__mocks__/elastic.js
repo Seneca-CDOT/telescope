2137283e50974d3e07c55d75c45cb27ea58902cc
David Humphrey <david.humphrey@senecacollege.ca>
2020-04-07 13:44:31 -0400

    Add elasticsearch delete for posts, update elastic mock

--- src/backend/utils/__mocks__/elastic.js
+++ src/backend/utils/__mocks__/elastic.js
@@ -1,3 +1,33 @@
-exports.indexPost = () => Promise.resolve();
+// Mock storage for our es data
+const db = {};
 
-exports.checkConnection = () => Promise.resolve();
+const indexPost = (text, postId) => {
+  db[postId] = text;
+  return Promise.resolve();
+};
+
+const deletePost = (postId) => {
+  delete db[postId];
+  return Promise.resolve();
+};
+
+// TODO
+const search = () =>
+  Promise.resolve({
+    results: 0,
+    values: [],
+  });
+
+const checkConnection = () => Promise.resolve();
+
+const waitOnReady = () => Promise.resolve();
+
+module.exports = {
+  // Expose the internal db for testing
+  db,
+  indexPost,
+  deletePost,
+  checkConnection,
+  search,
+  waitOnReady,
+};
