0ddc93afac9e57c5f6988010497ecc7a5d8f0696
Cindy Le <cindyledev@gmail.com>
2020-04-08 13:14:11 -0400

    remove frontformatter___date to appease Zeit so that it doesn't  panicOnBuild()

--- src/frontend/gatsby-node.js
+++ src/frontend/gatsby-node.js
@@ -7,7 +7,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
   const result = await graphql(`
     {
-      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+      allMarkdownRemark(sort: { order: DESC }, limit: 1000) {
         edges {
           node {
             frontmatter {
