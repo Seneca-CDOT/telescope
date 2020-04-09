510f0905f5164acc3c49548175299c3cee51a36a
Rafi <raungar@gmail.com>
2020-04-08 22:41:50 -0400

    Fix #746, #760, #946: enhance the My Feeds page (#933)
    * add form logic, help modal to My Feeds page
    
    * fetch user feeds into separate form controls
    
    * add delete feed buttons, refactor logic
    
    * address review comments
    
    * remove call to refresh window upon feed deletion
    
    * refactor a useState call for consistency
    
    * redirect non-logged-in users to 404 page without rendering My Feeds page
    
    * add feed deletion logic to DeleteFeedDialogButton
    
    * implement getUserFeeds() stop-gap
    
    * fix DeleteFeedDialogButton props
    
    * correct props destructuring in 3435b76a31872b2e19a5d9c6082a481edda2c32a
    
    * remove commented-out proptypes

---
+++ src/frontend/src/components/MyFeedsPage/DeleteFeedDialogButton.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+} from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import useSiteMetadata from '../../hooks/use-site-metadata';
+
+const useStyles = makeStyles(() => ({
+  button: {
+    padding: '3px 0 3px 0',
+  },
+}));
+
+function DeleteFeedDialogButton({ feed }) {
+  const { id, url } = feed;
+  const classes = useStyles();
+  const { telescopeUrl } = useSiteMetadata();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const removeFeed = async () => {
+    console.log(`Removing feed hosted at URL ${url}...`);
+    try {
+      const response = await fetch(`${telescopeUrl}/feeds/${id}`, { method: 'DELETE' });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      console.log(`Feed removed successfully`);
+    } catch (error) {
+      console.log(`Error removing feed with ID ${id}`, error);
+      throw error;
+    }
+  };
+
+  return (
+    <div>
+      <IconButton classes={{ root: classes.button }} onClick={handleClickOpen}>
+        <Delete color="secondary" />
+      </IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{`Remove feed hosted at ${url}?`}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Telescope will no longer display blog posts from this feed.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary" variant="outlined" autoFocus>
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              handleClose();
+              removeFeed();
+            }}
+            color="primary"
+            variant="contained"
+          >
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+DeleteFeedDialogButton.propTypes = {
+  feed: PropTypes.object,
+};
+
+export default DeleteFeedDialogButton;
--- src/frontend/src/pages/myfeeds.js
+++ src/frontend/src/pages/myfeeds.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-import { Container, Box, Typography, TextField, Grid, Card, IconButton } from '@material-ui/core';
-import { AccountCircle, RssFeed, HelpOutline, Add } from '@material-ui/icons';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Box, Card, Container, Grid, IconButton, Typography } from '@material-ui/core';
+import { AccountCircle, AddCircle, RssFeed } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { isWebUri } from 'valid-url';
 
 import PageBase from './PageBase';
 import useSiteMetadata from '../hooks/use-site-metadata';
+import ExistingFeedList from '../components/MyFeedsPage/ExistingFeedList.jsx';
+import HelpPopoverButton from '../components/MyFeedsPage/HelpPopoverButton.jsx';
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -17,17 +21,93 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MyFeeds() {
   const classes = useStyles();
-  const [feedAuthor, setFeedAuthor] = useState('');
-  const [feedUrl, setFeedUrl] = useState('');
+  const [newFeedAuthor, setNewFeedAuthor] = useState('');
+  const [newFeedUrl, setNewFeedUrl] = useState('');
+  const [submitStatus, setSubmitStatus] = useState({ message: '', isError: false });
+  const [userInfo, setUserInfo] = useState({});
+  const [feedHash, updateFeedHash] = useState({}); // { id1: {author: '...', url: '...'}, id2: {...}, ... }
+
   const { telescopeUrl } = useSiteMetadata();
 
-  function handleAuthorChange(author) {
-    setFeedAuthor(author);
-  }
+  const newFeedAuthorRef = React.useRef();
+  const newFeedUrlRef = React.useRef();
 
-  function handleUrlChange(url) {
-    setFeedUrl(url);
-  }
+  useEffect(() => {
+    (async function fetchUserInfo() {
+      try {
+        const response = await fetch(`${telescopeUrl}/user/info`);
+
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+
+        const user = await response.json();
+        setUserInfo(user);
+        setNewFeedAuthor(user.name);
+      } catch (error) {
+        console.log('Failed to fetch user information', error);
+        window.location.href = `${telescopeUrl}/404`;
+      }
+    })();
+
+    ValidatorForm.addValidationRule('isUrl', (value) => !!isWebUri(value));
+    return ValidatorForm.removeValidationRule.bind('isUrl');
+  }, [telescopeUrl]);
+
+  const getUserFeeds = useCallback(async () => {
+    try {
+      const response = await fetch(`${telescopeUrl}/user/feeds`);
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.log('Failed to fetch /user/feeds', error);
+    }
+    // TODO remove the following try/catch block once GET /user/feeds is implemented
+    try {
+      console.log('Attempting to fetch user feeds via /feeds endpoint');
+      const response = await fetch(`${telescopeUrl}/feeds`);
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const feedItems = await response.json();
+
+      const allFeedsData = await Promise.all(
+        feedItems.map((item) => fetch(`${telescopeUrl}${item.url}`))
+      );
+      const allFeeds = await Promise.all(allFeedsData.map((res) => res.json()));
+
+      return allFeeds.filter((feed) => feed.user === userInfo.id);
+    } catch (error) {
+      console.log('Failed to fetch user feeds via /feeds endpoint', error);
+      throw error;
+    }
+  }, [telescopeUrl, userInfo]);
+
+  useEffect(() => {
+    if (userInfo.id || submitStatus.isError === false) {
+      (async function hashUserFeeds() {
+        try {
+          const userFeeds = await getUserFeeds();
+
+          const userFeedHash = userFeeds.reduce((hash, feed) => {
+            hash[feed.id] = { author: feed.author, url: feed.url };
+            return hash;
+          }, {});
+
+          updateFeedHash(userFeedHash);
+        } catch (error) {
+          console.log('Error hashing user feeds', error);
+          throw error;
+        }
+      })();
+    }
+  }, [telescopeUrl, userInfo, submitStatus, getUserFeeds]);
 
   async function addFeed() {
     try {
@@ -37,73 +117,111 @@ export default function MyFeeds() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          author: feedAuthor,
-          url: feedUrl,
+          user: userInfo.id,
+          author: newFeedAuthor,
+          url: newFeedUrl,
         }),
       });
-      console.log(response);
-      return response;
+      setSubmitStatus(
+        response.ok
+          ? { message: 'Feed added successfully', isError: false }
+          : { message: `Error: ${response.status} ${response.statusText}`, isError: true }
+      );
     } catch (error) {
-      console.log({ error });
-      return { error };
+      setSubmitStatus({ message: error.message, isError: true });
+      console.log('Error adding feeds', error);
     }
   }
 
-  return (
+  function handleChange(event) {
+    const { name, value } = event.target;
+    if (name === 'author') {
+      setNewFeedAuthor(value);
+    } else {
+      setNewFeedUrl(value);
+    }
+  }
+
+  function handleBlur(event, ref) {
+    ref.current.validate(event.target.value, true);
+  }
+
+  return userInfo.id ? (
     <PageBase title="My Feeds">
       <div className={classes.margin}>
-        <Container maxWidth="xs" bgcolor="aliceblue">
-          <Card>
-            <Box px={2} py={1}>
-              <Typography variant="h3" component="h3" align="center">
-                My Feeds
-              </Typography>
-              <Grid container spacing={5}>
-                <Grid item>
-                  <Grid container spacing={1} alignItems="flex-end">
-                    <Grid item>
-                      <AccountCircle />
+        <ValidatorForm onSubmit={addFeed}>
+          <Container maxWidth="xs" bgcolor="aliceblue">
+            <Card>
+              <Box px={2} py={1}>
+                <Typography variant="h3" component="h3" align="center">
+                  My Feeds
+                </Typography>
+                <ExistingFeedList feedHash={feedHash} />
+                <Grid container spacing={5}>
+                  <Grid item>
+                    <Grid container spacing={1} alignItems="flex-end">
+                      <Grid item>
+                        <AccountCircle />
+                      </Grid>
+                      <Grid item>
+                        <TextValidator
+                          label="Blog feed author"
+                          name="author"
+                          ref={newFeedAuthorRef}
+                          value={newFeedAuthor}
+                          onBlur={(event) => handleBlur(event, newFeedAuthorRef)}
+                          onChange={handleChange}
+                          type="string"
+                          validators={['required', 'trim']}
+                          errorMessages={"Please enter the blog's author."}
+                        />
+                      </Grid>
                     </Grid>
-                    <Grid item>
-                      <TextField
-                        id="author"
-                        label="John Doe"
-                        onBlur={(event) => handleAuthorChange(event.target.value)}
-                      />
+                  </Grid>
+                  <Grid item>
+                    <Grid container spacing={1} alignItems="flex-end">
+                      <Grid item>
+                        <RssFeed />
+                      </Grid>
+                      <Grid item>
+                        <TextValidator
+                          label="Blog feed URL"
+                          name="url"
+                          ref={newFeedUrlRef}
+                          value={newFeedUrl}
+                          onBlur={(event) => handleBlur(event, newFeedUrlRef)}
+                          onChange={handleChange}
+                          type="url"
+                          validators={['required', 'isUrl']}
+                          errorMessages={"Please enter the blog's feed URL."}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <HelpPopoverButton />
+                      </Grid>
+                    </Grid>
+                    <Grid container spacing={2}>
+                      <Grid item>
+                        <IconButton classes={{ root: classes.button }} type="submit">
+                          <AddCircle color="secondary" />
+                        </IconButton>
+                        <Typography
+                          color={submitStatus.isError ? 'error' : 'textPrimary'}
+                          align="center"
+                        >
+                          {submitStatus.message}
+                        </Typography>
+                      </Grid>
                     </Grid>
                   </Grid>
                 </Grid>
-                <Grid item>
-                  <Grid container spacing={1} alignItems="flex-end">
-                    <Grid item>
-                      <RssFeed />
-                    </Grid>
-                    <Grid item>
-                      <TextField
-                        id="url"
-                        label="Blog feed URL"
-                        onBlur={(event) => handleUrlChange(event.target.value)}
-                      />
-                    </Grid>
-                    <Grid item>
-                      <IconButton color="primary" classes={{ root: classes.button }}>
-                        <HelpOutline />
-                      </IconButton>
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={2}>
-                    <Grid item>
-                      <IconButton classes={{ root: classes.button }} onClick={() => addFeed()}>
-                        <Add />
-                      </IconButton>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Box>
-          </Card>
-        </Container>
+              </Box>
+            </Card>
+          </Container>
+        </ValidatorForm>
       </div>
     </PageBase>
+  ) : (
+    <></>
   );
 }
