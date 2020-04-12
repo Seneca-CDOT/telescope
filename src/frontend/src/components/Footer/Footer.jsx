3b4084ca736f3505c764ef9a8c16d6c99e6cd9c0
Cindy Le <cindyledev@gmail.com>
2020-04-12 05:23:18 -0400

    added link to Seneca CDOT website

--- src/frontend/src/components/Footer/Footer.jsx
+++ src/frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Link } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,7 +20,16 @@ const Footer = () => {
     <Grid container className={classes.root}>
       <Grid item xs={12}>
         <Typography variant="h6" className={classes.footer}>
-          Copyright © {new Date().getFullYear()} Seneca’s Centre for Development of Open Technology
+          Copyright © {new Date().getFullYear()}{' '}
+          <Link
+            variant="h6"
+            className={classes.footer}
+            href="https://cdot.senecacollege.ca/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Seneca’s Centre for Development of Open Technology
+          </Link>
         </Typography>
       </Grid>
     </Grid>
