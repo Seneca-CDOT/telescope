ab0b2131a3816f257d8034da853ac97c2e5fe8ed
David Humphrey <david.humphrey@senecacollege.ca>
2020-04-17 14:07:48 -0400

    Add /status to deploy server, fix callback missing bug (#1020)
    * Add /status to deploy server, fix callback missing bug
    
    * WIP adding logging and proxying from main node app
    
    * Hard code DEPLOY_PORT and use HTTP for proxy
    
    * Fix order or args to handleEventType
    
    * Add docs about auto deployment
    
    * ESlint fixes

--- src/backend/web/routes/admin.js
+++ src/backend/web/routes/admin.js
@@ -1,6 +1,7 @@
 require('../../lib/config');
 const express = require('express');
 const { UI } = require('bull-board');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const fs = require('fs');
 
 const { protectAdmin } = require('../authentication');
@@ -35,5 +36,33 @@ router.get('/log', protectAdmin(true), (req, res) => {
     res.send(error.message);
   }
 });
+
+/**
+ * Proxy a few useful requests from the auto deployment server here.  The first
+ * is for getting status info about the state of the deploy server.  The second
+ * is for piping build logs in real-time.  Get the port for the deploy server
+ * from the tools/autodeployment/.env file.
+ */
+const DEPLOY_PORT = 4000; // we assume this, see tools/autodeployment/.env
+router.use(
+  '/build/status',
+  protectAdmin(true),
+  createProxyMiddleware({
+    target: `http://localhost:${DEPLOY_PORT}`,
+    pathRewrite: {
+      '^/admin/build/status': '/status',
+    },
+  })
+);
+router.use(
+  '/build/log',
+  protectAdmin(true),
+  createProxyMiddleware({
+    target: `http://localhost:${DEPLOY_PORT}`,
+    pathRewrite: {
+      '^/admin/build/log': '/log',
+    },
+  })
+);
 
 module.exports = router;
