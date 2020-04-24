6018acb6f04f009289f7edb1620dc503ee994499
David Humphrey <david.humphrey@senecacollege.ca>
2020-04-24 10:00:23 -0400

    Remove second meta viewport declaration

--- src/frontend/plugins/gatsby-plugin-top-layout/TopLayout.js
+++ src/frontend/plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../../src/theme';
@@ -8,9 +7,6 @@ import theme from '../../src/theme';
 export default function TopLayout(props) {
   return (
     <>
-      <Helmet>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Helmet>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         {props.children}
