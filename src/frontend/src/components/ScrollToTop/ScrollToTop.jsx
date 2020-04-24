748b3c94bbc85e425b3e92dc0c59dc97bbe37b28
Cindy Le <cindyledev@gmail.com>
2020-04-24 12:33:49 -0400

    cleaned up ScrollToTop imports

--- src/frontend/src/components/ScrollToTop/ScrollToTop.jsx
+++ src/frontend/src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CssBaseline, Toolbar } from '@material-ui/core/Toolbar';
+import { Fab, Toolbar, useScrollTrigger, Zoom } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Fab from '@material-ui/core/Fab';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import Zoom from '@material-ui/core/Zoom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
