9166da2ddde758e91b78978904b6cccde4998b11
Cindy Le <cindyledev@gmail.com>
2020-04-26 13:11:23 -0400

    added zIndex of 1100 to ScrollToTop

--- src/frontend/src/components/ScrollToTop/ScrollToTop.jsx
+++ src/frontend/src/components/ScrollToTop/ScrollToTop.jsx
@@ -9,6 +9,7 @@ const useStyles = makeStyles((theme) => ({
     position: 'fixed',
     bottom: theme.spacing(2),
     right: theme.spacing(2),
+    zIndex: 1100,
   },
   anchor: {
     minHeight: '1px',
