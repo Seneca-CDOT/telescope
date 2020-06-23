8d8b427c3669066cbc0322db86f3295c6ccc807e
Cindy Le <cindyledev@gmail.com>
2020-06-23 06:31:29 -0400

    added mobile stepper carousel effect

--- src/frontend/src/templates/template.js
+++ src/frontend/src/templates/template.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Button, Grid, MobileStepper, Paper, Typography } from '@material-ui/core';
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 import { graphql } from 'gatsby';
 
 import PageBase from '../pages/PageBase';
 import AboutFooter from '../components/AboutFooter';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const tutorialSteps = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+  },
+  {
+    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,15 +50,33 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '16px',
     maxWidth: '785px',
   },
-  image: {
-    width: 128,
-    height: 128,
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    paddingLeft: theme.spacing(4),
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
   },
   img: {
-    margin: 'auto',
+    height: 255,
     display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+    maxWidth: 400,
+    overflow: 'hidden',
+    width: '100%',
+    justifyContent: 'center',
+  },
+  stepper: {
+    iconColor: 'white',
+  },
+  footer: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: theme.palette.primary.main,
+  },
+  footerButton: {
+    color: 'white',
   },
 }));
 
@@ -35,12 +86,75 @@ export default function Template({
   const { markdownRemark } = data; // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark;
   const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = tutorialSteps.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
 
   return (
     <PageBase title={frontmatter.title}>
       <Grid container className={classes.root}>
-        <Grid container direction="column" item xs={12} justify="center" alignItems="center">
-          <Grid item xs={12}>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Paper square elevation={0} className={classes.header}>
+              <Typography variant="h4">{tutorialSteps[activeStep].label}</Typography>
+            </Paper>
+            <AutoPlaySwipeableViews
+              axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+              index={activeStep}
+              onChangeIndex={handleStepChange}
+              enableMouseEvents
+            >
+              {tutorialSteps.map((step, index) => (
+                <div key={step.label}>
+                  {Math.abs(activeStep - index) <= 2 ? (
+                    <img className={classes.img} src={step.imgPath} alt={step.label} />
+                  ) : null}
+                </div>
+              ))}
+            </AutoPlaySwipeableViews>
+            <MobileStepper
+              steps={maxSteps}
+              position="static"
+              variant="dots"
+              className={classes.footer}
+              activeStep={activeStep}
+              nextButton={
+                <Button
+                  size="large"
+                  className={classes.footerButton}
+                  onClick={handleNext}
+                  disabled={activeStep === maxSteps - 1}
+                >
+                  Next
+                  {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                </Button>
+              }
+              backButton={
+                <Button
+                  size="large"
+                  className={classes.footerButton}
+                  onClick={handleBack}
+                  disabled={activeStep === 0}
+                >
+                  {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                  Back
+                </Button>
+              }
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
             <div className={classes.markdownBody}>
               <h1>{frontmatter.title}</h1>
               <div dangerouslySetInnerHTML={{ __html: html }} />
