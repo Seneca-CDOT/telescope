75e9ce977e494a61f15394b0a304180656071d03
c3ho <calvinho125@gmail.com>
2020-10-08 23:57:26 -0400

    added length assertion and encodeURIcomponent

--- test/query.test.js
+++ test/query.test.js
@@ -4,17 +4,19 @@ const app = require('../src/backend/web/app');
 jest.mock('../src/backend/utils/elastic');
 
 describe('Testing query route', () => {
-  test('Testing with valid characters', async () => {
-    const searchTerm = 'I Love Telescope';
+  test('Testing with valid length of characters', async () => {
+    const searchTerm = encodeURIComponent('I Love Telescope');
     const res = await request(app).get(`/query?search=${searchTerm}`);
     expect(res.status).toBe(200);
   });
 
-  test('Testing with invalid characters', async () => {
+  test('Testing with invalid length of characters', async () => {
     // searchTerm is 257 chars long
-    const searchTerm =
-      '8l4XOYWZ3SA9aevIozTcEAng3GOSCAiiDARThEkAFn2F2YtBexA3lcg1O38SGSHILQrrNYReKWOC6RM4ZQQIGqZoLSOLlbbYqlfSkIDM83aeGDYW7KU8OSLbIXUIWIF4TINwrjxi453biwyjgYsJeqFx9ORd0EIw3dMwGPWhoMbvTIxUWXV032qgPRmohLbTf8xnMyttPjIOk3rHBXpukWSnkZiKyBMsUniZZnxYPw7yIhfoaS77jIPRUuiQufDdO';
+    const searchTerm = encodeURIComponent(
+      '8l4XOYWZ3SA9aevIozTcEAng3GOSCAiiDARThEkAFn2F2YtBexA3lcg1O38SGSHILQrrNYReKWOC6RM4ZQQIGqZoLSOLlbbYqlfSkIDM83aeGDYW7KU8OSLbIXUIWIF4TINwrjxi453biwyjgYsJeqFx9ORd0EIw3dMwGPWhoMbvTIxUWXV032qgPRmohLbTf8xnMyttPjIOk3rHBXpukWSnkZiKyBMsUniZZnxYPw7yIhfoaS77jIPRUuiQufDdO'
+    );
     const res = await request(app).get(`/query?search=${searchTerm}`);
+    expect(searchTerm.length).toBeGreaterThan(256);
     expect(res.status).toBe(400);
   });
 });
