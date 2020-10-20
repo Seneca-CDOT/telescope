6952d73a8a7363a8f352cb71dd4fab9447b40c59
David Humphrey <david.andrew.humphrey@gmail.com>
2020-10-20 13:07:24 -0400

    Experiment using swr and reworking Posts/Post loading

--- src/frontend/src/pages/index.js
+++ src/frontend/src/pages/index.js
@@ -5,6 +5,8 @@ import Banner from '../components/Banner';
 import Posts from '../components/Posts';
 import ScrollToTop from '../components/ScrollToTop';
 
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
 export default function IndexPage() {
   return (
     <PageBase title="Home">
