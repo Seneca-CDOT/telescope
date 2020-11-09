171bdcaa48be4aefe8e9362c0c727446fe4ba21b
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2020-11-09 11:01:06 -0500

    Update: Tests are passing Added another test case Added another case for pre tags with <br> elements

--- src/backend/utils/html/modify-pre.js
+++ src/backend/utils/html/modify-pre.js
@@ -3,7 +3,7 @@
  */
 function insertCodeElement(pre, document) {
   const innerElem = document.createElement('code');
-  innerElem.innerText = pre.innerText;
+  innerElem.innerHTML = pre.innerHTML;
   pre.innerHTML = innerElem.outerHTML;
 }
 /**
@@ -17,6 +17,11 @@ module.exports = function (dom) {
   }
 
   dom.window.document.querySelectorAll('pre').forEach((pre) => {
+    // if pre has no child elements
     if (pre.children.length <= 0) insertCodeElement(pre, dom.window.document);
+
+    // pre has child nodes but they are <br> tags
+    if (pre.hasChildNodes() && pre.children[0].nodeName == 'BR')
+      insertCodeElement(pre, dom.window.document);
   });
 };
--- test/modify-pre.test.js
+++ test/modify-pre.test.js
@@ -9,24 +9,52 @@ function fixEmpties(htmlData) {
 
 describe('modify empty pre tag tests', () => {
   test('html body without pre tags should not be changed', () => {
-    const og = '<html><head><title>A title</title></head><body><p>Hello World</p></body></html>';
+    const og = '<p>Hello World</p>';
     const res = fixEmpties(og);
     expect(res).toEqual(og);
   });
 
   test('pre tags with inner <code></code> elements should not be changed', () => {
-    const og =
-      '<html><head><title>A title</title></head><body><pre><code>console.log("Hello World")</code></pre></body></html>';
+    const og = '<pre><code>console.log("Hello World")</code></pre>';
     const res = fixEmpties(og);
     expect(res).toEqual(og);
   });
 
   test('pre tags without inner <code></code> elements should be fixed', () => {
-    const og =
-      '<html><head><title>A title</title></head><body><pre>console.log("Hello World")</pre></body></html>';
+    const og = '<pre>console.log("Hello World")</pre>';
     const res = fixEmpties(og);
-    const fix =
-      '<html><head><title>A title</title></head><body><pre><code>console.log("Hello World")</code></pre></body></html>';
+    const fix = '<pre><code>console.log("Hello World")</code></pre>';
+    expect(res).toEqual(fix);
+  });
+
+  test('pre tags without inner <code></code> elements should be fixed', () => {
+    const og = `<pre>theme: ThemeData(
+      primarySwatch: Colors.blue,
+      accentColor: Colors.red,
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: TextTheme(
+        //Title
+        headline1: TextStyle(color: Colors.white, fontSize: 22, fontWeight:FontWeight.w800),
+        //Author
+        bodyText1: TextStyle(color: Colors.white, fontSize: 23,fontWeight: FontWeight.w800,),
+        //Information
+        bodyText2: TextStyle(color: Colors.grey, fontSize: 20, letterSpacing: 0.9),
+      ),
+    ),</pre>`;
+    const res = fixEmpties(og);
+    const fix = `<pre><code>theme: ThemeData(
+      primarySwatch: Colors.blue,
+      accentColor: Colors.red,
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: TextTheme(
+        //Title
+        headline1: TextStyle(color: Colors.white, fontSize: 22, fontWeight:FontWeight.w800),
+        //Author
+        bodyText1: TextStyle(color: Colors.white, fontSize: 23,fontWeight: FontWeight.w800,),
+        //Information
+        bodyText2: TextStyle(color: Colors.grey, fontSize: 20, letterSpacing: 0.9),
+      ),
+    ),</code></pre>`;
     expect(res).toEqual(fix);
   });
 });
