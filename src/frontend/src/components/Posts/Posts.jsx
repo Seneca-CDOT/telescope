16bb9f9c8aec272a8a7add50584aa57ba25ae320
zjjiang2 <zjjiang2@myseneca.ca>
2020-11-10 02:10:40 -0500

    Error Handling Display Format Fixes

--- src/frontend/src/components/Posts/Posts.jsx
+++ src/frontend/src/components/Posts/Posts.jsx
@@ -7,8 +7,6 @@ import SentimentDissatisfiedRoundedIcon from '@material-ui/icons/SentimentDissat
 import Timeline from './Timeline.jsx';
 import useSiteMetaData from '../../hooks/use-site-metadata';
 
-import SentimentDissatisfiedRoundedIcon from '@material-ui/icons/SentimentDissatisfiedRounded';
-
 const useStyles = makeStyles(() => ({
   root: {
     padding: 0,
