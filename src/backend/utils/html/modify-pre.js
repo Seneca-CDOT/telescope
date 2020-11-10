4325fc7be9adf616dd85d60c48e4606910564ccf
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2020-11-10 17:58:35 -0500

    <code> tags are inserted into empty <pre> Added an else if changed wording in the comments changed the description of the tests changed a variable name rewrote logic for the br case Deleted redundant test renamed a function

--- src/backend/utils/html/modify-pre.js
+++ src/backend/utils/html/modify-pre.js
@@ -1,14 +1,14 @@
 /**
  * Take a <pre>...</pre> and return <pre><code>...</code></pre>
  */
-function insertCodeElement(pre, document) {
-  const innerElem = document.createElement('code');
-  innerElem.innerHTML = pre.innerHTML;
-  pre.innerHTML = innerElem.outerHTML;
+function insert(pre, document) {
+  const codeElem = document.createElement('code');
+  codeElem.innerHTML = pre.innerHTML;
+  pre.innerHTML = codeElem.outerHTML;
 }
 /**
  * Given a parsed JSDOM Object. find all <pre></pre> tags without child code elements and modify the
- * pre tag to have inner <code></code> tags which wraps around the previous content
+ * Pre tag to have inner <code></code> tags which wraps around the previous content
  * resulting in <pre><code>...</code></pre>
  */
 module.exports = function (dom) {
@@ -17,11 +17,12 @@ module.exports = function (dom) {
   }
 
   dom.window.document.querySelectorAll('pre').forEach((pre) => {
-    // if pre has no child elements
-    if (pre.children.length <= 0) insertCodeElement(pre, dom.window.document);
-
-    // pre has child nodes but they are <br> tags
-    if (pre.hasChildNodes() && pre.children[0].nodeName == 'BR')
-      insertCodeElement(pre, dom.window.document);
+    // no child elements
+    if (pre.children.length <= 0) insert(pre, dom.window.document);
+    // has child <br> elements
+    else if (pre.children.length > 0 && pre.children[0].nodeName === 'BR') {
+      pre.innerHTML = pre.innerHTML.replace(/<br>/g, '\n'); // trim
+      insert(pre, dom.window.document);
+    }
   });
 };
--- test/modify-pre.test.js
+++ test/modify-pre.test.js
@@ -7,7 +7,7 @@ function fixEmpties(htmlData) {
   return dom.window.document.body.innerHTML;
 }
 
-describe('modify empty pre tag tests', () => {
+describe('modify pre tag without <code> element tests', () => {
   test('html body without pre tags should not be changed', () => {
     const og = '<p>Hello World</p>';
     const res = fixEmpties(og);
@@ -24,37 +24,6 @@ describe('modify empty pre tag tests', () => {
     const og = '<pre>console.log("Hello World")</pre>';
     const res = fixEmpties(og);
     const fix = '<pre><code>console.log("Hello World")</code></pre>';
-    expect(res).toEqual(fix);
-  });
-
-  test('pre tags without inner <code></code> elements should be fixed', () => {
-    const og = `<pre>theme: ThemeData(
-      primarySwatch: Colors.blue,
-      accentColor: Colors.red,
-      scaffoldBackgroundColor: Colors.black,
-      textTheme: TextTheme(
-        //Title
-        headline1: TextStyle(color: Colors.white, fontSize: 22, fontWeight:FontWeight.w800),
-        //Author
-        bodyText1: TextStyle(color: Colors.white, fontSize: 23,fontWeight: FontWeight.w800,),
-        //Information
-        bodyText2: TextStyle(color: Colors.grey, fontSize: 20, letterSpacing: 0.9),
-      ),
-    ),</pre>`;
-    const res = fixEmpties(og);
-    const fix = `<pre><code>theme: ThemeData(
-      primarySwatch: Colors.blue,
-      accentColor: Colors.red,
-      scaffoldBackgroundColor: Colors.black,
-      textTheme: TextTheme(
-        //Title
-        headline1: TextStyle(color: Colors.white, fontSize: 22, fontWeight:FontWeight.w800),
-        //Author
-        bodyText1: TextStyle(color: Colors.white, fontSize: 23,fontWeight: FontWeight.w800,),
-        //Information
-        bodyText2: TextStyle(color: Colors.grey, fontSize: 20, letterSpacing: 0.9),
-      ),
-    ),</code></pre>`;
     expect(res).toEqual(fix);
   });
 });
