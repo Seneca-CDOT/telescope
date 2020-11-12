b5c3adba51a77718d9349cd9c434b60cd1f28fe4
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-11-12 18:05:52 -0500

    Change the font colour to theme.pallette.text.default
    Fixes 1173 change the font colour to pellette.text.default(#181818)

--- src/frontend/src/pages/error.jsx
+++ src/frontend/src/pages/error.jsx
@@ -72,7 +72,7 @@ const useStyles = makeStyles((theme) => ({
   fab: {
     position: 'relative',
     fontSize: '1.5rem',
-    color: '#D3D3D3',
+    color: theme.palette.text.default,
     bottom: -45,
     zIndex: 200,
     backgroundColor: theme.palette.secondary.light,
