1e31237b04927eb3b9d2b3e5d6eae05715b48df2
1746924261 <54863160+1746924261@users.noreply.github.com>
2020-11-18 17:47:57 -0500

    included error check and added Contribution function to simplified render code

--- src/frontend/src/components/GitHubContributorCard/GitHubContributorCard.jsx
+++ src/frontend/src/components/GitHubContributorCard/GitHubContributorCard.jsx
@@ -3,6 +3,10 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { Card, CardContent, Typography, Avatar, Grid } from '@material-ui/core';
 import useTelescopeContributor from '../../hooks/use-telescope-contributor';
 
+function Contributions({ contributions }) {
+  return contributions < 3 ? 'contributions.' : `${contributions} contributions.`;
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.primary.light,
@@ -25,7 +29,7 @@ const GitHubContributorCard = () => {
   const theme = useTheme();
   const { contributor, error } = useTelescopeContributor();
 
-  if (!contributor) {
+  if (error || !contributor) {
     return null;
   }
 
@@ -57,11 +61,8 @@ const GitHubContributorCard = () => {
                 className={classes.link}
               >
                 &nbsp;
-                {contributor.contributions > 2
-                  ? contributor.contributions + ' contributions'
-                  : 'contributions'}
+                <Contributions contributions={contributor.contributions} />
               </a>
-              .
             </Typography>
           </Grid>
         </Grid>
