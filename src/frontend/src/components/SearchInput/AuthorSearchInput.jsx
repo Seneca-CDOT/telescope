570f4f5dcbc5b31beb8e321e3cc7c309f63ab748
strawberries73 <39388566+strawberries73@users.noreply.github.com>
2020-11-22 15:40:21 -0500

    Added Input Adornment

--- src/frontend/src/components/SearchInput/AuthorSearchInput.jsx
+++ src/frontend/src/components/SearchInput/AuthorSearchInput.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
-function AuthorSearchInput({ text, onChange, classes }) {
+function AuthorSearchInput({ text, onChange, classes, IconButton, onSubmit }) {
   return (
     <>
       <input
@@ -10,6 +12,15 @@ function AuthorSearchInput({ text, onChange, classes }) {
         placeholder="How to Get Started in Open Source"
         inputProps={{ 'aria-label': 'search telescope' }}
         value={text}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment>
+              <IconButton type="submit" className={classes.iconButton} onClick={onSubmit}>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
         onChange={onChange}
       />
       <datalist id="search-suggestions"></datalist>
@@ -21,6 +32,8 @@ AuthorSearchInput.propTypes = {
   text: PropTypes.string,
   onChange: PropTypes.func,
   classes: PropTypes.object,
+  IconButton: PropTypes.func,
+  onSubmit: PropTypes.object,
 };
 
 export default AuthorSearchInput;
--- src/frontend/src/components/SearchInput/PostSearchInput.jsx
+++ src/frontend/src/components/SearchInput/PostSearchInput.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
-function PostSearchInput({ text, onChange, classes }) {
+function PostSearchInput({ text, onChange, classes, IconButton, onSubmit }) {
   return (
     <input
       className={classes.input}
       placeholder="How to Get Started in Open Source"
       inputProps={{ 'aria-label': 'search telescope' }}
       value={text}
-      onChange={onChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment>
+            <IconButton type="submit" className={classes.iconButton} onClick={onSubmit}>
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+      onChange={onChange.value}
     />
   );
 }
@@ -17,6 +28,8 @@ PostSearchInput.propTypes = {
   text: PropTypes.string,
   onChange: PropTypes.func,
   classes: PropTypes.object,
+  IconButton: PropTypes.func,
+  onSubmit: PropTypes.object,
 };
 
 export default PostSearchInput;
