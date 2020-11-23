0e907b3a48472a1201a3b3c5f4d66fbbc3b8f2ca
zjjiang2 <zjjiang2@myseneca.ca>
2020-11-23 01:49:47 -0500

    Removed isVisible from Post.jsx & Imported usePrevious

--- src/frontend/src/components/Posts/Posts.jsx
+++ src/frontend/src/components/Posts/Posts.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useSWRInfinite } from 'swr';
 import { Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { usePrevious } from 'react-use';
 import SentimentDissatisfiedRoundedIcon from '@material-ui/icons/SentimentDissatisfiedRounded';
 import Timeline from './Timeline.jsx';
 import useSiteMetaData from '../../hooks/use-site-metadata';
 import useFaviconBadge from '../../hooks/use-favicon-badge';
-import usePageLifecycle from '../../hooks/use-page-lifecycle';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -28,24 +28,10 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const usePrevious = (value) => {
-  const ref = React.useRef(null);
-  React.useEffect(() => {
-    // If data is loaded, store the id of the first post to ref
-    if (value !== undefined) {
-      ref.current = value[0][0].id;
-    }
-  });
-  return ref.current;
-};
-
 const REFRESH_INTERVAL = 5 * 60 * 1000; /* refresh data every 5 minutes */
 
 const Posts = () => {
   const classes = useStyles();
-  // Call setBadge(true) to show the favicon badge when the page is not visible
-  const setBadge = useFaviconBadge(false); // pass true here to badge from the start
-  const isVisible = usePageLifecycle();
   const { telescopeUrl } = useSiteMetaData();
   const { data, size, setSize, error } = useSWRInfinite(
     (index) => `${telescopeUrl}/posts?page=${index + 1}`,
@@ -55,17 +41,17 @@ const Posts = () => {
       refreshWhenHidden: true,
     }
   );
-
+  // Call setBadge(true) to show the favicon badge when the page is not visible
+  const setBadge = useFaviconBadge(false); // pass true here to badge from the start
+  const prevData = usePrevious(data);
   // Set to true to show badge, false otherwise.  Badge only shows when not visible.
-  const oldData = usePrevious(data);
   React.useEffect(() => {
-    if (oldData) {
+    if (prevData) {
+      const oldData = prevData[0][0].id;
       const newData = data[0][0].id;
       if (oldData !== newData) {
         setBadge(true);
-      }
-      if (isVisible) {
-        setBadge(false);
+        console.log('called');
       }
     }
   });
