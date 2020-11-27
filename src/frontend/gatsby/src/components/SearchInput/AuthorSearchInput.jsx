7a5390469eaa31c98763228645450518a94c4b4d
PedroFonsecaDEV <pedroporventura@gmail.com>
2020-11-27 01:52:31 -0500

    PostSearchInput done

--- src/frontend/gatsby/src/components/SearchInput/AuthorSearchInput.jsx
+++ src/frontend/gatsby/src/components/SearchInput/AuthorSearchInput.jsx
@@ -1,19 +1,19 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormControl, TextField } from '@material-ui/core';
 
 function AuthorSearchInput({ text, onChange, classes }) {
   return (
     <>
-      <FormControl fullWidth={true}>
-        <TextField
-          className={classes.input}
-          variant="outlined"
-          placeholder="How to Get Started in Open Source"
-          value={text}
-          onChange={onChange}
-        ></TextField>
-      </FormControl>
+      <input
+        className={classes.input}
+        list="search-suggestions"
+        placeholder="How to Get Started in Open Source"
+        inputProps={{ 'aria-label': 'search telescope' }}
+        value={text}
+        onChange={onChange}
+      />
+      <datalist id="search-suggestions" />
     </>
   );
 }
