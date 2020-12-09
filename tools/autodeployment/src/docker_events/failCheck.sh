ba275ebc900831d3bc8eb25577ead07b1c0ffe25
PedroFonsecaDEV <pedroporventura@gmail.com>
2020-12-09 18:04:32 -0500

    Comments on .sh

--- tools/autodeployment/src/docker_events/failCheck.sh
+++ tools/autodeployment/src/docker_events/failCheck.sh
@@ -1,3 +1,8 @@
+# This script listens to docker events and will report only the following events:
+# PAUSE, DIE and STOP.
+# The result of these actions is we received a JSON object with information about the container
+# whenever it STOPS for any reason.
+
 #!/bin/bash
 
 docker events --format '{{json .}}' --filter 'event=pause' --filter 'event=die' --filter 'event=stop' |
--- tools/autodeployment/src/docker_events/waitDocker.sh
+++ tools/autodeployment/src/docker_events/waitDocker.sh
@@ -1,3 +1,8 @@
+# This script listens to docker events and will report only the following events:
+# START, UNPAUSE and RESTART.
+# The result of these actions is we received a JSON object with information about
+# the container whenever it starts for any reason.
+
 #!/bin/bash
 
 docker events --format '{{json .}}' --filter 'event=start' --filter 'event=unpause' --filter 'event=restart' |
