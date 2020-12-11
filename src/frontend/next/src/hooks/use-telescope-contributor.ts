728ab8f41058eac0c498d0643e54f4058f2db59d
Phan Hieu Nguyen <fanhieu94@gmail.com>
2020-12-11 15:01:13 -0500

    attempt to port hooks to NextJS

---
+++ src/frontend/next/src/hooks/use-telescope-contributor.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const pickRandomContributor = (contributors: any) => {
+  return contributors[Math.floor(Math.random() * contributors.length)];
+};
+
+const useTelescopeContributor = () => {
+  const [contributor, setContributor] = useState(null);
+  const [error, setError] = useState();
+  const telescopeGitHubContributorsUrl =
+    'https://api.github.com/repos/Seneca-CDOT/telescope/contributors';
+
+  useEffect(() => {
+    const fetchContributors = async () => {
+      try {
+        const response = await fetch(telescopeGitHubContributorsUrl);
+        if (!response.ok) {
+          throw new Error('GitHub API response not OK');
+        }
+
+        const contributors = await response.json();
+        setContributor(pickRandomContributor(contributors));
+      } catch (err) {
+        setError(err);
+      }
+    };
+    fetchContributors();
+  }, []);
+
+  return { contributor, error };
+};
+
+export default useTelescopeContributor;
