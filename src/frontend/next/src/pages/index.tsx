4571802f45812045666aafcc67a1a8741d3bdd6f
jossiey <jyang223@myseneca.ca>
2020-12-10 20:57:27 -0500

    fixes issue#1446, achieve desktop header and mobile header in one header.js component

--- src/frontend/next/src/pages/index.tsx
+++ src/frontend/next/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Header from '../components/Header/header';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
