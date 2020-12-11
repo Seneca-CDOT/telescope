3d2cbdc8effb220b897ecc69d7bf5edf92fbf41d
Phan Hieu Nguyen <fanhieu94@gmail.com>
2020-12-11 17:03:17 -0500

    added some type-check interfaces

--- src/frontend/next/src/contexts/User/UserReducer/index.ts
+++ src/frontend/next/src/contexts/User/UserReducer/index.ts
@@ -1,11 +1,23 @@
-export const initialState = {
+interface State {
+  name?: string;
+  email?: string;
+  id?: string;
+  isAdmin: boolean;
+}
+
+interface Action {
+  type?: string;
+  payload?: any;
+}
+
+export const initialState: State = {
   name: '',
   email: '',
   id: '',
   isAdmin: false,
 };
 
-export const userReducer = (state = initialState, action: any) => {
+export const userReducer = (state: State = initialState, action: Action) => {
   switch (action.type) {
     case 'LOGIN_USER': {
       return { ...action.payload };
