526bc32f832daa7a53a2cf801902d9132bdb89cd
Henry Nguyen <phnguyen7@myseneca.ca>
2020-12-11 01:03:32 -0500

    Ported SearchInput to nextjs

---
+++ src/frontend/next/src/components/SearchInput/AuthorSearchInput.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function AuthorSearchInput({ text, onChange, classes }: any) {
+  return (
+    <>
+      <input
+        className={classes.input}
+        list="search-suggestions"
+        placeholder="How to Get Started in Open Source"
+        // inputProps={{ 'aria-label': 'search telescope' }}
+        aria-label="search telescope"
+        value={text}
+        onChange={onChange}
+      />
+      <datalist id="search-suggestions" />
+    </>
+  );
+}
+AuthorSearchInput.propTypes = {
+  text: PropTypes.string,
+  onChange: PropTypes.func,
+  classes: PropTypes.object,
+};
+export default AuthorSearchInput;
---
+++ src/frontend/next/src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import PostSearchInput from './PostSearchInput';
+import AuthorSearchInput from './AuthorSearchInput';
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    fontSize: '1.6rem',
+    '&:hover': {
+      border: '1px solid',
+      borderColor: theme.palette.primary.main,
+    },
+    '&:focus': {
+      border: '2px solid',
+      borderColor: theme.palette.primary.main,
+    },
+    '& > *': {
+      fontSize: '1.6rem !important',
+      color: theme.palette.text.primary,
+    },
+    height: '55px',
+    backgroundColor: theme.palette.background.default,
+    paddingLeft: '10px',
+    border: '1px solid #B3B6B7',
+    borderRadius: '7px',
+    outline: 'none',
+  },
+}));
+
+function SearchInput({ text, filter, onChange }: any) {
+  const classes = useStyles();
+  return filter === 'author' ? (
+    <AuthorSearchInput
+      classes={classes}
+      text={text}
+      onChange={(event) => onChange(event.target.value)}
+    />
+  ) : (
+    <PostSearchInput
+      classes={classes}
+      text={text}
+      onChange={(event) => onChange(event.target.value)}
+    />
+  );
+}
+
+SearchInput.propTypes = {
+  text: PropTypes.string,
+  onChange: PropTypes.func,
+  filter: PropTypes.string,
+};
+
+export default SearchInput;
