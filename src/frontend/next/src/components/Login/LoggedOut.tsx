485c6474a3ee94825526aedea54ebdfdef43b56c
tonyvugithub <55034244+tonyvugithub@users.noreply.github.com>
2021-01-19 20:52:39 -0500

    Ported Login component

---
+++ src/frontend/next/src/components/Login/LoggedOut.tsx
@@ -0,0 +1,30 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+
+import config from '../../config';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    float: 'right',
+    margin: '0 0.5rem 0 0.5rem',
+  },
+  link: {
+    textDecoration: 'none',
+    fontSize: '1.5rem',
+    color: theme.palette.primary.contrastText,
+    lineHeight: 1,
+  },
+}));
+
+const LoggedOut = () => {
+  const loginUrl = `${config.telescopeUrl}/auth/login`;
+  const classes = useStyles();
+
+  return (
+    <Button className={classes.button} href={loginUrl}>
+      LOGIN
+    </Button>
+  );
+};
+
+export default LoggedOut;
