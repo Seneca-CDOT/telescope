9a894550cad59a3cd76d36d7ecfc40a0e651147c
PedroFonsecaDEV <pedroporventura@gmail.com>
2021-01-21 04:14:07 -0500

    type created

--- src/frontend/next/src/components/SearchInput/SearchInput.tsx
+++ src/frontend/next/src/components/SearchInput/SearchInput.tsx
@@ -1,33 +1,41 @@
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import PostSearchInput from './PostSearchInput';
 import AuthorSearchInput from './AuthorSearchInput';
 
-const useStyles = makeStyles((theme) => ({
-  input: {
-    fontSize: '1.6rem',
-    '&:hover': {
-      border: '1px solid',
-      borderColor: theme.palette.primary.main,
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    input: {
+      fontSize: '1.6rem',
+      '&:hover': {
+        border: '1px solid',
+        borderColor: theme.palette.primary.main,
+      },
+      '&:focus': {
+        border: '2px solid',
+        borderColor: theme.palette.primary.main,
+      },
+      '& > *': {
+        fontSize: '1.6rem !important',
+        color: theme.palette.text.default,
+      },
+      height: '55px',
+      backgroundColor: theme.palette.background.default,
+      paddingLeft: '10px',
+      paddingRight: '60px',
+      border: '1px solid #B3B6B7',
+      borderRadius: '7px',
+      outline: 'none',
     },
-    '&:focus': {
-      border: '2px solid',
-      borderColor: theme.palette.primary.main,
-    },
-    '& > *': {
-      fontSize: '1.6rem !important',
-      color: theme.palette.text.default,
-    },
-    height: '55px',
-    backgroundColor: theme.palette.background.default,
-    paddingLeft: '10px',
-    paddingRight: '60px',
-    border: '1px solid #B3B6B7',
-    borderRadius: '7px',
-    outline: 'none',
-  },
-}));
+  })
+);
 
-function SearchInput({ text, filter, onChange }) {
+type searchInputProps = {
+  text: string;
+  onTextChange: Function;
+  searchFilter: string;
+};
+
+const SearchInput({ text, filter, onChange }) {
   const classes = useStyles();
   return filter === 'author' ? (
     <AuthorSearchInput
@@ -43,11 +51,5 @@ function SearchInput({ text, filter, onChange }) {
     />
   );
 }
-
-SearchInput.propTypes = {
-  text: PropTypes.string,
-  onChange: PropTypes.func,
-  filter: PropTypes.string,
-};
 
 export default SearchInput;
