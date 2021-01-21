3e953011fe885cb24dc0f54feb7437e6e48ec447
PedroFonsecaDEV <pedroporventura@gmail.com>
2021-01-21 04:25:20 -0500

    SearchInput - done

--- src/frontend/next/src/components/SearchInput/SearchInput.tsx
+++ src/frontend/next/src/components/SearchInput/SearchInput.tsx
@@ -1,33 +1,5 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import PostSearchInput from './PostSearchInput';
 import AuthorSearchInput from './AuthorSearchInput';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    input: {
-      fontSize: '1.6rem',
-      '&:hover': {
-        border: '1px solid',
-        borderColor: theme.palette.primary.main,
-      },
-      '&:focus': {
-        border: '2px solid',
-        borderColor: theme.palette.primary.main,
-      },
-      '& > *': {
-        fontSize: '1.6rem !important',
-        color: theme.palette.text.default,
-      },
-      height: '55px',
-      backgroundColor: theme.palette.background.default,
-      paddingLeft: '10px',
-      paddingRight: '60px',
-      border: '1px solid #B3B6B7',
-      borderRadius: '7px',
-      outline: 'none',
-    },
-  })
-);
 
 type searchInputProps = {
   text: string;
@@ -35,21 +7,12 @@ type searchInputProps = {
   searchFilter: string;
 };
 
-const SearchInput({ text, filter, onChange }) {
-  const classes = useStyles();
-  return filter === 'author' ? (
-    <AuthorSearchInput
-      classes={classes}
-      text={text}
-      onChange={(event) => onChange(event.target.value)}
-    />
+const SearchInput = ({ text, onTextChange, searchFilter }: searchInputProps) => {
+  return searchFilter === 'author' ? (
+    <AuthorSearchInput text={text} onChange={(event) => onTextChange(event.target.value)} />
   ) : (
-    <PostSearchInput
-      classes={classes}
-      text={text}
-      onChange={(event) => onChange(event.target.value)}
-    />
+    <PostSearchInput text={text} onChange={(event) => onTextChange(event.target.value)} />
   );
-}
+};
 
 export default SearchInput;
