228ad4905b5a9a0d9b36dc37d08d08d30789bc5a
Yuan-Hsi Lee <50813726+yuanLeeMidori@users.noreply.github.com>
2021-01-29 14:55:09 -0500

    fix #1494: Port adminbutton to [next] (#1619)
    * Port adminbutton to next
    
    * Add condition to avoid undefined return

--- src/frontend/next/src/components/AdminButtons.tsx
+++ src/frontend/next/src/components/AdminButtons.tsx
@@ -1,3 +1,40 @@
-const AdminButtons = () => <h3> AdminButtons</h3>;
+import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
+import FlagIcon from '@material-ui/icons/Flag';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { useUser } from './UserProvider';
+
+const useStyles = makeStyles(() => ({
+  iconDiv: {
+    float: 'right',
+  },
+  iconSpan: {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+}));
+
+function AdminButtons() {
+  const classes = useStyles();
+  const user = useUser();
+
+  if (!user.isAdmin) {
+    return null;
+  }
+  return (
+    <div className={classes.iconDiv}>
+      <span className={classes.iconSpan}>
+        <IconButton size="small">
+          <FlagIcon fontSize="large" />
+        </IconButton>
+      </span>
+      <span className={classes.iconSpan}>
+        <IconButton size="small">
+          <DeleteIcon fontSize="large" />
+        </IconButton>
+      </span>
+    </div>
+  );
+}
 
 export default AdminButtons;
