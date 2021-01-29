ab128967f4ced48908a83da1f7d435436fb4258f
yuanLeeMidori <midori3113133@gmail.com>
2021-01-29 08:52:55 -0500

    Port adminbutton to next

--- src/frontend/next/src/components/AdminButtons.tsx
+++ src/frontend/next/src/components/AdminButtons.tsx
@@ -1,3 +1,39 @@
-const AdminButtons = () => <h3> AdminButtons</h3>;
+import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
+import FlagIcon from '@material-ui/icons/Flag';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { useUser } from './UserProvider';
+
+const useStyles = makeStyles(() => ({
+  iconDiv: {
+    float: 'right',
+  },
+  iconSpan: {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+}));
+
+function AdminButtons() {
+  const classes = useStyles();
+  const user = useUser();
+
+  return (
+    user.isAdmin && (
+      <div className={classes.iconDiv}>
+        <span className={classes.iconSpan}>
+          <IconButton size="small">
+            <FlagIcon fontSize="large" />
+          </IconButton>
+        </span>
+        <span className={classes.iconSpan}>
+          <IconButton size="small">
+            <DeleteIcon fontSize="large" />
+          </IconButton>
+        </span>
+      </div>
+    )
+  );
+}
 
 export default AdminButtons;
