e7d58a9b6aec720edcf4b0db4898965e0ce8a1bd
Huy Nguyen <mrvietnamese.ca@gmail.com>
2021-02-05 13:59:47 -0500

    Updated

--- src/frontend/gatsby/src/components/SearchBar/SearchBar.jsx
+++ src/frontend/gatsby/src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-
 import {
   Grid,
   MenuItem,
