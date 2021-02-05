3e35ea765daaf55d9741d138162647e1b0c83944
izhuravlev <ilya.zhurik@gmail.com>
2021-02-05 11:35:19 +0300

    rebase

--- src/frontend/next/src/pages/index.tsx
+++ src/frontend/next/src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from 'next/head';
+import { Theme } from '@material-ui/core';
 import Banner from '../components/Banner';
 import BackToTopButton from '../components/BackToTopButton';
 import Posts from '../components/Posts';
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header/header';
+import ThemeToggleButton from '../components/ThemeToggleButton';
 
-const Home = () => {
+type Props = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+const Home = ({ theme, toggleTheme }: Props) => {
   return (
     <>
       <Head>
@@ -14,6 +20,7 @@ const Home = () => {
       </Head>
       <Header />
 
+      <ThemeToggleButton theme={theme} toggleTheme={toggleTheme} />
       <Banner />
       <BackToTopButton />
       <main className="main">
