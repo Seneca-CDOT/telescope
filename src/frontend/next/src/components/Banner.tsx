f398b22b498082cd74272811124d6e12ab6d6c44
Huy Nguyen <mrvietnamese.ca@gmail.com>
2021-02-08 03:40:44 -0500

    Update Banner [1616]

--- src/frontend/next/src/components/Banner.tsx
+++ src/frontend/next/src/components/Banner.tsx
@@ -1,87 +1,93 @@
 import { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles, Fab, Grid, Typography } from '@material-ui/core';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import { Fab, Grid, Typography } from '@material-ui/core';
 import useSiteMetadata from '../hooks/use-site-metadata';
 import BannerDynamicItems from './BannerDynamicItems';
 import ScrollAction from './ScrollAction';
 
-const useStyles = makeStyles((theme) => ({
-  h1: {
-    position: 'absolute',
-    color: theme.palette.primary.contrastText,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
-    fontSize: '12rem',
-    display: 'block',
-    top: theme.spacing(20),
-    left: theme.spacing(8),
-    [theme.breakpoints.between('xs', 'sm')]: {
-      fontSize: '4rem',
-      textAlign: 'left',
-      left: theme.spacing(4),
-      right: theme.spacing(4),
-      top: theme.spacing(14),
-    },
-    [theme.breakpoints.between('md', 'lg')]: {
-      fontSize: '8rem',
-    },
-    [theme.breakpoints.up('xl')]: {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    h1: {
+      position: 'absolute',
+      color: theme.palette.primary.contrastText,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
       fontSize: '12rem',
+      display: 'block',
+      top: theme.spacing(20),
+      left: theme.spacing(8),
+      [theme.breakpoints.between('xs', 'sm')]: {
+        fontSize: '4rem',
+        textAlign: 'left',
+        left: theme.spacing(4),
+        right: theme.spacing(4),
+        top: theme.spacing(14),
+      },
+      [theme.breakpoints.between('md', 'lg')]: {
+        fontSize: '8rem',
+      },
+      [theme.breakpoints.up('xl')]: {
+        fontSize: '12rem',
+      },
     },
-  },
-  heroBanner: {
-    height: '100vh',
-    overflow: 'hidden',
-  },
-  version: {
-    position: 'absolute',
-    opacity: 0.85,
-    bottom: theme.spacing(6),
-    left: theme.spacing(8),
-    fontSize: '1.75rem',
-    color: theme.palette.primary.contrastText,
-    [theme.breakpoints.between('xs', 'sm')]: {
-      left: theme.spacing(4),
-      right: theme.spacing(4),
+    heroBanner: {
+      height: '100vh',
+      overflow: 'hidden',
+    },
+    version: {
+      position: 'absolute',
+      fontFamily: 'Roboto',
+      opacity: 0.85,
+      bottom: theme.spacing(6),
+      left: theme.spacing(8),
       fontSize: '1.75rem',
+      color: theme.palette.primary.contrastText,
+      [theme.breakpoints.between('xs', 'sm')]: {
+        left: theme.spacing(4),
+        right: theme.spacing(4),
+        fontSize: '1.75rem',
+      },
+      [theme.breakpoints.between('md', 'lg')]: {
+        fontSize: '2rem',
+      },
+      [theme.breakpoints.up('xl')]: {
+        fontSize: '4rem',
+      },
+      textDecorationLine: 'none',
+      '&:hover': {
+        textDecorationLine: 'underline',
+      },
     },
-    [theme.breakpoints.between('md', 'lg')]: {
-      fontSize: '2rem',
+    icon: {
+      height: '5.6rem',
+      width: '5.6rem',
+      position: 'relative',
+      bottom: theme.spacing(20),
+      zIndex: 300,
+      margin: '0 auto',
+      [theme.breakpoints.between('xs', 'sm')]: {
+        left: '55%',
+        bottom: theme.spacing(18),
+      },
     },
-    [theme.breakpoints.up('xl')]: {
-      fontSize: '4rem',
+    anchor: {
+      position: 'relative',
+      top: '1rem',
     },
-    textDecorationLine: 'none',
-    '&:hover': {
-      textDecorationLine: 'underline',
+    container: {
+      backgroundColor: '#E5E5E5',
     },
-  },
-  icon: {
-    height: '5.6rem',
-    width: '5.6rem',
-    position: 'relative',
-    bottom: theme.spacing(20),
-    zIndex: 300,
-    margin: '0 auto',
-    [theme.breakpoints.between('xs', 'sm')]: {
-      left: '55%',
-      bottom: theme.spacing(18),
-    },
-  },
-  anchor: {
-    position: 'relative',
-    top: '1rem',
-  },
-  container: {
-    backgroundColor: '#E5E5E5',
-  },
-}));
+  })
+);
 
 export default function Banner() {
   const classes = useStyles();
   const { telescopeUrl } = useSiteMetadata();
-  const [gitInfo, setGitInfo] = useState({});
+  const [gitInfo, setGitInfo] = useState({
+    gitHubUrl: '',
+    sha: '',
+    version: '',
+  });
 
   useEffect(() => {
     async function getGitData() {
@@ -131,7 +137,7 @@ export default function Banner() {
           <div className={classes.icon}>
             <ScrollAction>
               <Fab color="secondary" aria-label="scroll-down">
-                <KeyboardArrowDownIcon fontSize="large" color="primary" />
+                <KeyboardArrowDownIcon fontSize="large" />
               </Fab>
             </ScrollAction>
           </div>
