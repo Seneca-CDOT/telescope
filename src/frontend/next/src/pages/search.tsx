c7a532038950ea2d039a8de8682046300b30db10
Chris Pinkney <hey@chrispinkney.com>
2021-02-17 23:16:15 -0500

    Removed unnecessary <Head> and <meta> tags

--- src/frontend/next/src/pages/search.tsx
+++ src/frontend/next/src/pages/search.tsx
@@ -1,14 +1,9 @@
-import Head from 'next/head';
 import SEO from '../components/SEO';
 import SearchPage from '../components/SearchPage';
 
 const Search = () => {
   return (
     <>
-      <Head>
-        <title>Search</title>
-        <meta property="og:title" content="Telescope" key="title" />
-      </Head>
       <SEO pageTitle="Search" />
       <SearchPage />
     </>
