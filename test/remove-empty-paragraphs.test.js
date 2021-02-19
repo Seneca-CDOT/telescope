8ba06ee7cb2a7473392f017c38f7c673badc1b74
Metropass <moho472@gmail.com>
2021-02-19 11:16:37 -0500

    Moved character values from comment into test data

---
+++ test/remove-empty-paragraphs.test.js
@@ -0,0 +1,35 @@
+const toDom = require('../src/backend/utils/html/dom');
+const removeEmptyParagraphs = require('../src/backend/utils/html/remove-empty-paragraphs');
+
+describe('Remove no content anchor tags', () => {
+  test('should remove <p></p>', () => {
+    const htmlData = toDom('<div><p></p></div>');
+    removeEmptyParagraphs(htmlData);
+
+    const expectedHtml = '<div></div>';
+    expect(htmlData.window.document.body.innerHTML).toEqual(expectedHtml);
+  });
+
+  test('should remove <p>   </p> (spaces)', () => {
+    const htmlData = toDom('<div><p>   </p></div>');
+    removeEmptyParagraphs(htmlData);
+
+    const expectedHtml = '<div></div>';
+    expect(htmlData.window.document.body.innerHTML).toEqual(expectedHtml);
+  });
+
+  test('should remove <p>&#9&#9</p> (tabs)', () => {
+    const htmlData = toDom('<div><p>&#9&#9</p></div>');
+    removeEmptyParagraphs(htmlData);
+
+    const expectedHtml = '<div></div>';
+    expect(htmlData.window.document.body.innerHTML).toEqual(expectedHtml);
+  });
+
+  test('should remove <p>&#x2800 &#x2800</p> (braille)', () => {
+    const htmlData = toDom('<div><p>&#x2800 &#x2800</p></div>');
+    removeEmptyParagraphs(htmlData);
+    const expectedHtml = '<div></div>';
+    expect(htmlData.window.document.body.innerHTML).toEqual(expectedHtml);
+  });
+});
