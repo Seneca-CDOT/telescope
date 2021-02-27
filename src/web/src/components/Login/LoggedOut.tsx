fb9cf13efecc664f3ffc4352a602c77d8d7086dc
Josue <josue.quilon-barrios@senecacollege.ca>
2021-02-27 03:35:22 -0500

    refactor: change header buttons' style and refactor code

--- src/web/src/components/Login/LoggedOut.tsx
+++ src/web/src/components/Login/LoggedOut.tsx
@@ -1,38 +1,19 @@
-import { makeStyles } from '@material-ui/core/styles';
-import { IconButton } from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import Link from 'next/link';
 import config from '../../config';
-
-const useStyles = makeStyles((theme) => ({
-  button: {
-    float: 'right',
-    margin: '0 0.5rem 0 0.5rem',
-  },
-  buttonText: {
-    textDecoration: 'none',
-    fontSize: '1.5rem',
-    color: theme.palette.primary.contrastText,
-    margin: '0 0.5rem 0 0.5rem',
-    fontFamily: 'Roboto, sans-serif',
-  },
-  icon: {
-    fontSize: '2.5rem',
-  },
-}));
+import HeaderButton from '../Header/HeaderButton';
 
 const LoggedOut = () => {
-  const classes = useStyles();
   const { loginUrl } = config;
 
   return (
-    <>
-      <Link href={loginUrl} passHref>
-        <IconButton color="inherit" className={classes.button} aria-label="log in" component="a">
-          <ExitToAppIcon className={classes.icon} />
-        </IconButton>
-      </Link>
-    </>
+    <HeaderButton
+      button={{
+        href: loginUrl,
+        title: 'Log In',
+        ariaLabel: 'log in',
+        Icon: ExitToAppIcon,
+      }}
+    />
   );
 };
 
