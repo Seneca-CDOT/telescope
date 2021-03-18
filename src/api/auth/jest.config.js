7f182669d35b13f93cccec25aac6d59395b1a2e2
Abdulbasid Guled <guled.basid@gmail.com>
2021-03-17 22:46:58 -0400

    Modified base jest setup to use env.development

--- src/api/auth/jest.config.js
+++ src/api/auth/jest.config.js
@@ -3,7 +3,7 @@ const baseConfig = require('../../../jest.config.base');
 module.exports = {
   ...baseConfig,
   rootDir: '../../..',
-  setupFiles: ['<rootDir>/jest.setup.js', '<rootDir>/src/api/auth/jest.setup.js'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: ['<rootDir>/src/api/auth/test/*.test.js'],
   collectCoverageFrom: ['<rootDir>/src/api/auth/src/**/*.js'],
 };
