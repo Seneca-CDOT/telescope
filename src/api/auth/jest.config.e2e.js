0dbed2476d79fc7734780d7c4ad1127eba64afd9
Abdulbasid Guled <guled.basid@gmail.com>
2021-03-19 13:42:26 -0400

    Removed setupFiles from auth jest config files

--- src/api/auth/jest.config.e2e.js
+++ src/api/auth/jest.config.e2e.js
@@ -3,7 +3,6 @@ const baseConfig = require('../../../jest.config.base');
 module.exports = {
   ...baseConfig,
   rootDir: '../../..',
-  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: ['<rootDir>/src/api/auth/test/e2e/**/*.test.js'],
   collectCoverageFrom: ['<rootDir>/src/api/auth/src/**/*.js'],
   // We use jest-playwright to do browser tests, https://github.com/playwright-community/jest-playwright
--- src/api/auth/jest.config.js
+++ src/api/auth/jest.config.js
@@ -3,7 +3,6 @@ const baseConfig = require('../../../jest.config.base');
 module.exports = {
   ...baseConfig,
   rootDir: '../../..',
-  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: ['<rootDir>/src/api/auth/test/*.test.js'],
   collectCoverageFrom: ['<rootDir>/src/api/auth/src/**/*.js'],
 };
