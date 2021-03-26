449322baadedd7f41354769b4bdce8737dc0a75c
c3ho <calvinho125@gmail.com>
2021-03-26 13:41:23 -0400

    changes according to review

--- src/api/parser/src/server.js
+++ src/api/parser/src/server.js
@@ -1,7 +1,7 @@
 const { logger, fetch } = require('@senecacdot/satellite');
 const service = require('.');
 
-const port = parseInt(process.env.PARSER_PORT || 8888, 10);
+const port = parseInt(process.env.PARSER_PORT || 10000, 10);
 
 // Start service
 service.start(port);
