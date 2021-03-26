3a080488ff3714bca68601805801ef8d0bf78f70
Chris Pinkney <hey@chrispinkney.com>
2021-03-25 20:24:05 -0400

    Added environment variables to prod docker file
    * Dev and Docker mode now working both locally and in prod environments
    * Overriding FIRESTORE_EMULATOR_HOST in jest.config.e2e.js to pass tests
    * Removed dotenv (no longer needed)
    * Using cross-env
    * Utilizing require instead of fs for firestore keys
    * Refactored firestore.js
    * Removed unused variables
    * Added comments, cleaned up key require in firestore.js, rebased

--- src/api/users/src/services/firestore.js
+++ src/api/users/src/services/firestore.js
@@ -1,53 +1,28 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-unresolved */
+
 const admin = require('firebase-admin');
 const { logger } = require('@senecacdot/satellite');
 
-const {
-  FIREBASE_TYPE,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_PRIVATE_KEY_ID,
-  FIREBASE_PRIVATE_KEY,
-  FIREBASE_CLIENT_EMAIL,
-  FIREBASE_CLIENT_ID,
-  FIREBASE_AUTH_URI,
-  FIREBASE_TOKEN_URI,
-  FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  FIREBASE_CLIENT_X509_CERT_URL,
-} = process.env;
-
-// If a private key exists in env.development, run in online mode
-// else run in emulator mode
-if (
-  (FIREBASE_PRIVATE_KEY,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_PRIVATE_KEY_ID,
-  FIREBASE_PRIVATE_KEY,
-  FIREBASE_CLIENT_EMAIL,
-  FIREBASE_CLIENT_ID,
-  FIREBASE_AUTH_URI,
-  FIREBASE_TOKEN_URI,
-  FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  FIREBASE_CLIENT_X509_CERT_URL)
-) {
+try {
+  // Docker is responsible for putting this file in the root of the container
   admin.initializeApp({
-    credential: admin.credential.cert({
-      type: FIREBASE_TYPE,
-      project_id: FIREBASE_PROJECT_ID,
-      private_key_id: FIREBASE_PRIVATE_KEY_ID,
-      private_key: FIREBASE_PRIVATE_KEY,
-      client_email: FIREBASE_CLIENT_EMAIL,
-      client_id: FIREBASE_CLIENT_ID,
-      auth_uri: FIREBASE_AUTH_URI,
-      token_uri: FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
-    }),
+    credential: admin.credential.cert(require('../../serviceAccountKey.json')),
   });
+
   logger.debug('Server running in online mode');
-} else {
+} catch (err) {
+  if (process.env.NODE_ENV === 'production') {
+    logger.error({ err }, 'Unable to load Firebase config');
+    process.exit(1);
+  }
+
+  // In development, fall-back to use the emulator
   admin.initializeApp({
     projectId: 'telescope',
     credential: admin.credential.applicationDefault(),
   });
+
   logger.debug('Server running in emulator mode');
 }
 
