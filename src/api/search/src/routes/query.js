9952b59064d6812aa02e3d0f6657736b84a53d77
izhuravlev <ilya.zhurik@gmail.com>
2021-04-09 18:20:55 +0300

    Fixes #1737 - adds Search Microservice

--- src/api/search/src/routes/query.js
+++ src/api/search/src/routes/query.js
@@ -1,4 +1,4 @@
-const { Router, logger } = require('@senecacdot/satellite');
+const { Router } = require('@senecacdot/satellite');
 const { validateQuery } = require('../bin/validation');
 const search = require('../search');
 
@@ -9,8 +9,7 @@ router.get('/', validateQuery, async (req, res) => {
     const { text, filter, page, perPage } = req.query;
     res.send(await search(text, filter, page, perPage));
   } catch (error) {
-    res.status(500).send(`There was an error while executing your query: ${error}`);
-    logger.error({ error }, 'Something went wrong with search indexing');
+    next(err);
   }
 });
 
