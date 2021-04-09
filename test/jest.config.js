7191554e0cfe4ada636c96e6637a6e06fef6e38a
David Humphrey <david.andrew.humphrey@gmail.com>
2021-04-08 22:30:33 -0400

    Fix how we mock fetch in Jest

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,6 +2,7 @@ const baseConfig = require('../jest.config.base');
 
 module.exports = {
   ...baseConfig,
+  setupFiles: ['<rootDir>/test/jest.setup.js'],
   rootDir: '../',
   testMatch: ['<rootDir>/test/**/*.test.js'],
   collectCoverageFrom: ['<rootDir>/src/backend/**/*.js'],
