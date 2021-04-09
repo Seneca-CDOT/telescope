fbaa57e37a0611c66845381e6275dc76866ccf60
c3ho <calvinho125@gmail.com>
2021-04-09 14:20:21 -0400

    removed entry point and commented out startParserQueue

--- src/api/parser/src/index.js
+++ src/api/parser/src/index.js
@@ -1,10 +1,11 @@
 const { Satellite } = require('@senecacdot/satellite');
 const { router } = require('bull-board');
-const startParserQueue = require('./parser-queue');
+// const startParserQueue = require('./parser-queue');
 
 const service = new Satellite();
 
-startParserQueue();
+// We're commenting startParserQueue() for now so it doesn't clash with our current backend, will uncomment when tests go in and we remove the backend.
+// startParserQueue();
 
 service.router.use('/', router);
 
