15ad89ea0758ca80cb722bbbc1683b6e1fb1eb07
c3ho <calvinho125@gmail.com>
2021-04-09 12:02:30 -0400

    added playright preset to jest.config.e2e

--- src/api/parser/jest.config.e2e.js
+++ src/api/parser/jest.config.e2e.js
@@ -5,4 +5,5 @@ module.exports = {
   rootDir: '../../..',
   testMatch: ['<rootDir>/src/api/parser/test/e2e/**/*.test.js'],
   collectCoverageFrom: ['<rootDir>/src/api/parser/src/**/*.js'],
+  preset: 'jest-playwright-preset',
 };
