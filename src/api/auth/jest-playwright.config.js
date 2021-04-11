473baf5817d4f5eb7b24a80dff46a3998ed9bee7
David Humphrey <david.andrew.humphrey@gmail.com>
2021-04-11 15:11:55 -0400

    Update jest-playwright configs to correct some bugs with Playwright testing

--- src/api/auth/jest-playwright.config.js
+++ src/api/auth/jest-playwright.config.js
@@ -1,2 +1,2 @@
 // Due to https://github.com/playwright-community/jest-playwright/issues/446
-// we put our jest-playwright config in the Telescope root, see ./jest-playwright.confing.js
+// we put our jest-playwright config in the Telescope root, see ./jest-playwright.config.js
