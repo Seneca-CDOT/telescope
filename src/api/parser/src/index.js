4562cd17ac053d9c788d97d0cb8df5fead60d579
David Humphrey <david.andrew.humphrey@gmail.com>
2021-04-14 15:16:49 -0400

    Add authorization middlware to parser service

--- src/api/parser/src/index.js
+++ src/api/parser/src/index.js
@@ -1,4 +1,4 @@
-const { Satellite } = require('@senecacdot/satellite');
+const { Satellite, isAuthenticated, isAuthorized } = require('@senecacdot/satellite');
 const { router } = require('bull-board');
 // const startParserQueue = require('./parser-queue');
 
@@ -7,6 +7,12 @@ const service = new Satellite();
 // We're commenting startParserQueue() for now so it doesn't clash with our current backend, will uncomment when tests go in and we remove the backend.
 // startParserQueue();
 
-service.router.use('/', router);
+// Only authenticated Telescope users, or other services, can hit this route
+service.router.get(
+  '/',
+  isAuthenticated(),
+  isAuthorized((req, user) => user.roles.includes('telescope') || user.roles.includes('service')),
+  router
+);
 
 module.exports = service;
