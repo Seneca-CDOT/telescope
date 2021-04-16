f765b25e084d19fc48f9041a681753c4ad7539f4
Royce Ayroso-Ong <royce.ayrosoong@gmail.com>
2021-04-16 13:13:01 -0400

    Fixes #1983: Improve SearchHelp logic

--- src/web/src/components/SearchResults.tsx
+++ src/web/src/components/SearchResults.tsx
@@ -51,7 +51,7 @@ const useStyles = makeStyles(() => ({
 
 const SearchResults = () => {
   const classes = useStyles();
-  const { textParam, filter } = useSearchValue();
+  const { textParam, filter, toggleHelp } = useSearchValue();
 
   const prepareUrl = (index: number) =>
     `${searchServiceUrl}?text=${encodeURIComponent(textParam)}&filter=${filter}&page=${index}`;
@@ -74,6 +74,13 @@ const SearchResults = () => {
   // Another page is being loaded when size is incremented but data[size - 1] is still undefined
   const loadingMoreData =
     !isReachingEnd && data && size > 0 && typeof data[size - 1] === 'undefined';
+
+  // If there is no posts or if the search bar is empty, then show the search help, otherwise hide it
+  if (!error && (isEmpty || textParam.length == 0)) {
+    toggleHelp(true);
+  } else {
+    toggleHelp(false);
+  }
 
   if (error) {
     return (
