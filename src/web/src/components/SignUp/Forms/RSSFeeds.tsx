e79163ec749de0a9bf1974a36a0bb7a41ae5a659
meneguini <mariana.meneguini@gmail.com>
2021-04-19 14:03:14 -0400

    - changed minHeight on mobile and desktop [GitHubAccount.tsx, RSSFeeds.tsx]

--- src/web/src/components/SignUp/Forms/RSSFeeds.tsx
+++ src/web/src/components/SignUp/Forms/RSSFeeds.tsx
@@ -22,7 +22,10 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: '0',
       position: 'relative',
       width: '100%',
-      minHeight: '100%',
+      minHeight: '80%',
+      [theme.breakpoints.down(600)]: {
+        minHeight: '75%',
+      },
     },
     container: {
       display: 'grid',
