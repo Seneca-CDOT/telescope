94e3602107252633883f69271419fdecf064c47c
Huy Nguyen <mrvietnamese.ca@gmail.com>
2021-04-21 16:24:31 -0400

    added advanced search, NOT YET

---
+++ src/web/src/components/AdvancedSearchDialog.tsx
@@ -0,0 +1,118 @@
+import useSearchValue from '../hooks/use-search-value';
+import CloseIcon from '@material-ui/icons/Close';
+import { Dialog, DialogContent, DialogTitle, IconButton } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React from 'react';
+
+interface Props {
+  setOpenDialog: Function;
+  openDialog: boolean;
+  searchTerm: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      transform: 'translateY(-230px)',
+    },
+    dialogTitle: {
+      display: 'flex',
+      flexDirection: 'column',
+      width: '758px',
+      height: '25px',
+      padding: 15,
+      background: 'pink',
+    },
+    dialogContent: {
+      background: 'cyan',
+      height: '300px',
+      color: 'black',
+      fontSize: '1.8rem',
+    },
+    input: {
+      width: '100%',
+      height: '25px',
+      boxSizing: 'border-box',
+      borderRadius: '20px',
+      padding: '5px 18px',
+      outline: 'none',
+      border: 'none',
+      marginBottom: '15px',
+    },
+    closeIcon: {
+      fontSize: '2.8rem',
+      position: 'absolute',
+      padding: '10px',
+      color: '#999999',
+    },
+    searchButton: {
+      padding: '10px 25px',
+      position: 'absolute',
+      right: '10px',
+      top: '10px',
+      border: 'solid 1px #1ea0f0',
+      borderRadius: '20px',
+      outline: 'none',
+      cursor: 'pointer',
+      color: '#fff',
+      background: '#1ea0f0',
+      transition: 'background .2s',
+      fontWeight: 'bold',
+      '&:hover': {
+        background: '#3da8e8',
+      },
+    },
+    title: {
+      display: 'inline',
+      position: 'absolute',
+      top: 0,
+      marginLeft: '20px',
+      fontSize: '1.8rem',
+    },
+  })
+);
+
+const AdvancedSearchDialog = (props: Props) => {
+  const classes = useStyles();
+  const { text, onTextChange, onSubmitHandler } = useSearchValue();
+
+  const handleClose = () => {
+    props.setOpenDialog(false);
+  };
+
+  return (
+    <Dialog
+      maxWidth="md"
+      className={classes.root}
+      BackdropProps={{ style: { backgroundColor: 'transparent' } }}
+      open={props.openDialog}
+      onClose={handleClose}
+    >
+      <DialogTitle className={classes.dialogTitle}>
+        <IconButton onClick={handleClose} aria-label="search">
+          <CloseIcon className={classes.closeIcon} />
+        </IconButton>
+
+        <p className={classes.title}>Advanced Search</p>
+
+        <button className={classes.searchButton}>Search</button>
+      </DialogTitle>
+
+      <DialogContent className={classes.dialogContent}>
+        Keywords:
+        <form onSubmit={(e) => onSubmitHandler(e)}>
+          <input
+            autoFocus
+            type="text"
+            value={text}
+            onChange={(e) => onTextChange(e.target.value)}
+            className={classes.input}
+          />
+        </form>
+        Search options:
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AdvancedSearchDialog;
--- src/web/src/components/SearchBar.tsx
+++ src/web/src/components/SearchBar.tsx
@@ -1,8 +1,10 @@
+import { useState, useRef } from 'react';
 import useSearchValue from '../hooks/use-search-value';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-import { Grid, FormControl, IconButton, Box } from '@material-ui/core';
+import { Grid, FormControl, IconButton, Box, Button } from '@material-ui/core';
 import ClearIcon from '@material-ui/icons/Clear';
+import AdvancedSearchDialog from './AdvancedSearchDialog';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,9 +27,10 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingLeft: '55px',
       background: '#EEEEEE',
       position: 'absolute',
-      transition: 'border .2s',
+      transition: 'box-shadow .3s, background .3s',
       '&:focus': {
         boxShadow: '0 0 5px 2px #CCCCCC',
+        background: '#FFFFFF',
       },
     },
     iconButton: {
@@ -57,47 +60,97 @@ const useStyles = makeStyles((theme: Theme) =>
       right: '5px',
       top: '1px',
     },
+    advancedSearchBtn: {
+      position: 'absolute',
+      top: '60px',
+      right: '15px',
+      width: 'auto',
+      padding: '5px 10px',
+      outline: 'none',
+      border: 'none',
+      background: 'transparent',
+      color: '#999999',
+      cursor: 'pointer',
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+    },
   })
 );
 
 const SearchBar = () => {
   const classes = useStyles();
+  const searchInput = useRef(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [dataToDialog, setDataToDialog] = useState({});
+
   const { text, onTextChange, onSubmitHandler } = useSearchValue();
 
+  function handleFocus() {
+    if (searchInput && searchInput.current) {
+      // @ts-ignore: Object is possibly 'null'.
+      searchInput.current.focus();
+    }
+  }
+
   return (
-    <Box className={classes.root}>
-      <FormControl fullWidth>
-        <Grid item xs={12} sm={10} lg={10}>
-          <form onSubmit={(e) => onSubmitHandler(e)}>
-            <input
-              autoFocus
-              className={classes.input}
-              value={text}
-              placeholder="Search..."
-              onChange={(e) => onTextChange(e.target.value)}
-            />
+    <div>
+      <Box className={classes.root}>
+        <FormControl fullWidth>
+          <Grid item xs={12} sm={10} lg={10}>
+            <form onSubmit={(e) => onSubmitHandler(e)}>
+              <input
+                autoFocus
+                ref={searchInput}
+                className={classes.input}
+                value={text}
+                placeholder="Search..."
+                onChange={(e) => onTextChange(e.target.value)}
+              />
 
-            <IconButton
-              onClick={(e) => {
-                if (text) onSubmitHandler(e);
-              }}
-              className={classes.iconButton}
-              aria-label="search"
-            >
-              <SearchIcon />
-            </IconButton>
+              <IconButton
+                onClick={(e) => {
+                  if (text) onSubmitHandler(e);
+                }}
+                className={classes.iconButton}
+                aria-label="search"
+              >
+                <SearchIcon />
+              </IconButton>
 
-            <IconButton
-              className={classes.clearIcon}
-              onClick={() => onTextChange('')}
-              aria-label="search"
-            >
-              <ClearIcon />
-            </IconButton>
-          </form>
-        </Grid>
-      </FormControl>
-    </Box>
+              {Boolean(text) && (
+                <IconButton
+                  className={classes.clearIcon}
+                  onClick={() => {
+                    onTextChange('');
+                    handleFocus();
+                  }}
+                  aria-label="clear"
+                >
+                  <ClearIcon />
+                </IconButton>
+              )}
+            </form>
+          </Grid>
+          <button
+            color="primary"
+            className={classes.advancedSearchBtn}
+            onClick={() => {
+              setDataToDialog(text);
+              setOpenDialog(true);
+            }}
+          >
+            Advanced Search
+          </button>
+        </FormControl>
+      </Box>
+
+      <AdvancedSearchDialog
+        searchTerm={dataToDialog}
+        openDialog={openDialog}
+        setOpenDialog={setOpenDialog}
+      />
+    </div>
   );
 };
 
