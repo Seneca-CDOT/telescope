1c6eebc3617bc98ca7c86c676a96b9fe46880c27
Abdulbasid Guled <guled.basid@gmail.com>
2021-04-23 12:42:21 -0400

    Added createError in search route

--- src/api/search/src/routes/query.js
+++ src/api/search/src/routes/query.js
@@ -1,4 +1,4 @@
-const { Router } = require('@senecacdot/satellite');
+const { Router, createError } = require('@senecacdot/satellite');
 const { validateQuery } = require('../bin/validation');
 const search = require('../search');
 
@@ -9,7 +9,7 @@ router.get('/', validateQuery, async (req, res, next) => {
     const { text, filter, page, perPage } = req.query;
     res.send(await search(text, filter, page, perPage));
   } catch (error) {
-    next(error);
+    next(createError(503, error));
   }
 });
 
