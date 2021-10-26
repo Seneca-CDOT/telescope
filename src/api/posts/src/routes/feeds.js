30a2855243371257c71cf10c9c74730593eba783
Josue <manekenpix@fastmail.com>
2021-10-25 23:21:35 -0400

    feat: add a planet microservice

---
+++ src/api/posts/src/routes/feeds.js
@@ -0,0 +1,186 @@
+const {
+  Router,
+  logger,
+  createError,
+  isAuthenticated,
+  isAuthorized,
+} = require('@senecacdot/satellite');
+const Feed = require('../data/feed');
+const { getFeeds } = require('../storage');
+const { validateNewFeed, validateFeedsIdParam } = require('../validation');
+
+const feeds = Router();
+
+const feedURL = `${process.env.POSTS_URL}/feeds` || '/';
+
+feeds.get('/', async (req, res, next) => {
+  let ids;
+
+  try {
+    ids = await getFeeds();
+  } catch (error) {
+    logger.error({ error }, 'Unable to get feeds from Redis');
+    next(error);
+  }
+
+  res.set('X-Total-Count', ids.length);
+  res.json(
+    ids
+      // Return id and url for a specific feed
+      .map((id) => ({
+        id,
+        url: `${feedURL}/${id}`,
+      }))
+  );
+});
+
+feeds.get('/:id', validateFeedsIdParam(), async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    // If the object we get back is empty, use 404
+    const feed = await Feed.byId(id);
+    if (!feed) {
+      res.status(404).json({
+        message: `Feed not found for id ${id}`,
+      });
+    } else {
+      res.json(feed);
+    }
+  } catch (error) {
+    logger.error({ error }, 'Unable to get feeds from Redis');
+    next(error);
+  }
+});
+
+feeds.put(
+  '/:id/flag',
+  isAuthenticated(),
+  isAuthorized((req, user) => {
+    // Or an admin, or another authorized microservice
+    return user.roles.includes('admin') || user.roles.includes('service');
+  }),
+  validateFeedsIdParam(),
+  async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const feed = await Feed.byId(id);
+      if (!feed) {
+        return res.status(404).json({
+          message: `Feed for Feed ID ${id} doesn't exist.`,
+        });
+      }
+      await feed.flag();
+      return res.status(200).json({
+        message: `Feed successfully flagged`,
+      });
+    } catch (error) {
+      logger.error({ error }, 'Unable to flag feed in Redis');
+      next(error);
+    }
+  }
+);
+
+feeds.post(
+  '/',
+  isAuthenticated(),
+  isAuthorized(
+    // A Seneca user can create a new Telescope user, but an existing Telescope
+    // user cannot, since they must already have one.
+    (req, user) => user.roles.includes('seneca') && !user.roles.includes('telescope')
+  ),
+  validateNewFeed(),
+  async (req, res, next) => {
+    const feedData = req.body;
+    const { user } = req;
+    feedData.user = user.id;
+    try {
+      if (!(feedData.url && feedData.author)) {
+        return res.status(400).json({ message: `URL and Author must be submitted` });
+      }
+      if (await Feed.byUrl(feedData.url)) {
+        return res.status(409).json({ message: `Feed for url ${feedData.url} already exists.` });
+      }
+      const feedId = await Feed.create(feedData);
+      return res
+        .status(201)
+        .json({ message: `Feed was successfully added.`, id: feedId, url: `/feeds/${feedId}` });
+    } catch (error) {
+      logger.error({ error }, 'Unable to add feed to Redis');
+      next(error);
+    }
+  }
+);
+
+feeds.delete(
+  '/cache',
+  isAuthenticated(),
+  isAuthorized((req, user) => {
+    // Or an admin, or another authorized microservice
+    return user.roles.includes('admin') || user.roles.includes('service');
+  }),
+  async (req, res, next) => {
+    try {
+      await Feed.clearCache();
+      return res.status(204).send();
+    } catch (error) {
+      logger.error({ error }, 'Unable to reset Feed data in Redis');
+      next(error);
+    }
+  }
+);
+
+feeds.delete(
+  '/:id',
+  validateFeedsIdParam(),
+  isAuthenticated(),
+  isAuthorized(
+    // A Seneca user can create a new Telescope user, but an existing Telescope
+    // user cannot, since they must already have one.
+    (req, user) => user.roles.includes('seneca') && !user.roles.includes('telescope')
+  ),
+  async (req, res, next) => {
+    const { user } = req;
+    const { id } = req.params;
+    try {
+      const feed = await Feed.byId(id);
+      if (!feed) {
+        return res.status(404).json({ message: `Feed for Feed ID ${id} doesn't exist.` });
+      }
+      if (!user.owns(feed)) {
+        return res.status(403).json({ message: `User does not own this feed.` });
+      }
+      await feed.delete();
+      return res.status(204).json({ message: `Feed ${id} was successfully deleted.` });
+    } catch (error) {
+      logger.error({ error }, 'Unable to delete feed to Redis');
+      next(error);
+    }
+  }
+);
+
+feeds.delete(
+  '/:id/flag',
+  isAuthenticated(),
+  isAuthorized((req, user) => {
+    // Or an admin, or another authorized microservice
+    return user.roles.includes('admin') || user.roles.includes('service');
+  }),
+  validateFeedsIdParam(),
+  async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const feed = await Feed.byId(id);
+      if (!feed) {
+        return res.status(404).json({ message: `Feed for Feed ID ${id} doesn't exist.` });
+      }
+      await feed.unflag();
+      return res.status(204).json({ message: `Feed ${id} was successfully unflagged.` });
+    } catch (error) {
+      logger.error({ error }, 'Unable to unflag feed in Redis');
+      next(error);
+    }
+  }
+);
+
+module.exports = feeds;
--- src/api/posts/src/validation.js
+++ src/api/posts/src/validation.js
@@ -1,4 +1,4 @@
-const { param, validationResult, query } = require('express-validator');
+const { check, param, validationResult, query } = require('express-validator');
 
 const validate = (rules) => {
   return async (req, res, next) => {
@@ -14,6 +14,30 @@ const validate = (rules) => {
   };
 };
 
+const feedValidationRules = [
+  check('user')
+    .exists({ checkFalsy: true })
+    .withMessage('username is required')
+    .bail()
+    .isString()
+    .withMessage('username must be string')
+    .bail(),
+  check('author')
+    .exists({ checkFalsy: true })
+    .withMessage('author is required')
+    .bail()
+    .isString()
+    .withMessage('author must be string')
+    .bail(),
+  check('url')
+    .isString()
+    .withMessage('url must be string')
+    .bail()
+    .isURL({ protocols: ['http', 'https'], require_valid_protocol: true, require_protocol: true })
+    .withMessage('url must be a valid url')
+    .bail(),
+];
+
 const postsQueryValidationRules = [
   query('per_page', 'per_page needs to be empty or a integer').custom(
     (value) => !value || Number.isInteger(+value)
@@ -27,7 +51,13 @@ const postsIdParamValidationRules = [
   param('id', 'Id Length is invalid').isLength({ min: 10, max: 10 }),
 ];
 
+const feedsIdParamValidationRules = [
+  param('id', 'Feeds Id Length is invalid').isLength({ min: 10, max: 10 }),
+];
+
 module.exports = {
+  validateNewFeed: () => validate(feedValidationRules),
+  validateFeedsIdParam: () => validate(feedsIdParamValidationRules),
   validatePostsQuery: () => validate(postsQueryValidationRules),
   validatePostsIdParam: () => validate(postsIdParamValidationRules),
 };
