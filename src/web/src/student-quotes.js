e7ebeb1dbec9653745e0239dcd86693a98515408
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-10-30 19:51:40 -0400

    added js with quotes from past

---
+++ src/web/src/student-quotes.js
@@ -0,0 +1,121 @@
+export const quotes = [
+  {
+    author: 'Shreena',
+    url: 'https://shreenasopensource.wordpress.com/2018/12/13/osd-600-reflection/',
+    quote:
+      'Looking back at these last 31/2 months I feel I grew as an open source developer. I can recall the first week of this class not even knowing what open source development was.',
+  },
+  {
+    author: 'Abdulbasid Guled',
+    url: 'https://dev.to/hyperthd/dps911-blog-2-telescope-1-5-ships-my-first-release-4a67',
+    quote:
+      "oh my goodness, there's so many commits we pushed in this release that it makes me wonder if my team and I have lives outside of open source.",
+  },
+  {
+    author: 'Julia McGeoghan',
+    url: 'https://medium.com/@juliakmdev/the-virtues-of-open-source-4cd3e080399d',
+    quote:
+      'It seemed too good to be true that I could just jump in and contribute code for popular development tools or even languages. It took a while for me to realize that I was ready and capable of doing so.',
+  },
+  {
+    author: 'Julia McGeoghan',
+    url: 'https://medium.com/@juliakmdev/the-virtues-of-open-source-4cd3e080399d',
+    quote:
+      'I’ve gotten to work with a team that seems dedicated to a strong development process and code quality, which in turn helps me build good habits when writing code.',
+  },
+  {
+    author: 'Abdulbasid Guled',
+    url:
+      'https://dev.to/hyperthd/4-months-of-open-source-what-i-ve-learned-from-taking-dps909-and-what-i-plan-to-do-about-dps911-next-month-1g62',
+    quote:
+      'Overall, my experiences with [open source development] were very positive. Even when I was down, I tried to work my way back up.',
+  },
+  {
+    author: 'Mohammed Ahmed',
+    url: 'https://moho472.medium.com/dps911-here-we-go-telescope-28e0b9a51f86',
+    quote:
+      'Wow, I finally get to start talking about Open Source again! I’m very excited just writing about this, cause I have a greater chance to work on Telescope, and my own projects.',
+  },
+  {
+    author: 'Royce Ayroso-Ong',
+    url: 'https://roycedev.medium.com/two-steps-forward-one-step-back-b0c105037a04',
+    quote:
+      'I took on responsibilities that I honestly did not think I could handle, and though I still have a lot of PRs to review and issues to solve, I believe it was a step in the right direction.',
+  },
+  {
+    author: 'Chris Pinkney',
+    url: 'https://dev.to/chrispinkney/going-down-with-the-ship-p51',
+    quote:
+      "Down with the Shipness, Everybody is working for the Shipend, and Stairway to Shipven. It's a joke about shipping software",
+  },
+  {
+    author: 'Abdulbasid Guled',
+    url:
+      'https://dev.to/hyperthd/4-months-of-open-source-what-i-ve-learned-from-taking-dps909-and-what-i-plan-to-do-about-dps911-next-month-1g62',
+    quote:
+      "I also need to keep reminding myself, that failure does not ever define who I am. It's how I come back from failure that shows what I'm really capable of.",
+  },
+  {
+    author: 'Chris Pinkney',
+    url: 'https://dev.to/chrispinkney/going-down-with-the-ship-p51',
+    quote:
+      'Open Source is amazing and I hope I can keep it going even after finishing this course.',
+  },
+  {
+    author: 'Ilya Zhuravlev',
+    url: 'https://ilya-zhurik.medium.com/bye-gatsby-episode-1-3fd63db08aa1',
+    quote:
+      'For example, the Login component suddenly realized that he is Special, and started a riot by not following the styling of the Header. How did I suppress the Riot? Well, I will tell you about it in my next blogpost ;)',
+  },
+  {
+    author: 'Calvin Ho',
+    url: 'https://c3ho.blogspot.com/2021/04/documentation.html',
+    quote:
+      'If it is one thing OSD has taught me, it was to research and adapt, and maybe there might be a light at the end of it all',
+  },
+  {
+    author: 'Oleg Mytryniuk',
+    url:
+      'https://osd600mytryniuk.wordpress.com/2017/03/16/open-source-experience-that-is-a-big-plus-in-my-mind/',
+    quote:
+      'I felt like people see me as a person who can make their project better, not as a person who does something wrong.',
+  },
+  {
+    author: 'Huda A',
+    url: 'https://hudascoding.wordpress.com/2018/10/31/hacktoberfest-beginning-to-a-good-start/',
+    quote:
+      'I have certainly learned that contribution to an open-source community is not limited to just coding! You can certainly help make technology better for people around the world through this! ',
+  },
+  {
+    author: 'Abdul Abdi',
+    url: 'https://abdulosd.blogspot.com/2020/12/the-end.html',
+    quote:
+      'I was also able to learn and gain confidence in approaching other developers for projects to work and overcome some of the imposter syndrome I feel sometimes.',
+  },
+  {
+    author: 'William To',
+    url:
+      'https://williamto10.wordpress.com/2019/12/09/project-stage-3-final-attempt-and-conclusion/',
+    quote:
+      'Even though we can fail, we always learn something during the process and come out better on the other side.',
+  },
+  {
+    author: 'Joel Azwar',
+    url: 'https://joelazwaros.blogspot.com/2020/11/hackvember-fest-pr4-days-late-conclusion.html',
+    quote:
+      "Wanting to code as opposed to having to code. And the end result just makes it all the more worth. Overall, I'm looking forward to more of this in the future.",
+  },
+  {
+    author: 'Ray Gervais',
+    url:
+      'https://raygervais.dev/articles/2017/04/a-conclusion-to-my-first-semester-dedicated-to-open-source-technologies/',
+    quote:
+      'Open Source is an idea, a culture; a vast culture full of subcultures, projects, and innovations which do change the world within the blink of a passing day.',
+  },
+  {
+    author: 'Hao Chen',
+    url: 'https://medium.com/haorc/1-open-source-hao-0-62267bbff596',
+    quote:
+      'What did I learn? 1. Trust your gut feeling; 2. Understand the problem, before looking for the cause!',
+  },
+];
