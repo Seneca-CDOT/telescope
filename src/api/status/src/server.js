53606fdb8a288da9d6386971730c65220e9b3924
Josue <manekenpix@fastmail.com>
2021-10-31 22:46:01 -0400

    fix: change csp to allow fonts and adjust paths to avoid conflict with traefik (#2428)
    Co-authored-by: David Humphrey <david.andrew.humphrey@gmail.com>

--- src/api/status/src/server.js
+++ src/api/status/src/server.js
@@ -1,23 +1,17 @@
 const { Satellite } = require('@senecacdot/satellite');
-const serveStatic = require('serve-static');
-
-const { check } = require('./services');
+const { static } = require('express');
+const path = require('path');
 
 const satelliteOptions = {
   helmet: {
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrcElem: ["'self'", 'https://kit.fontawesome.com'],
-        styleSrcElem: [
-          "'self'",
-          "'unsafe-inline'",
-          'https://fonts.googleapis.com',
-          'https://fonts.gstatic.com',
-        ],
-        connectSrc: ["'self'", 'https://ka-f.fontawesome.com'],
-        fontSrc: ["'self'", 'https://*'],
-        imgSrc: ["'self'", 'data:*', 'https://*'],
+        scriptSrc: ["'self'", '*.fontawesome.com'],
+        styleSrc: ["'self'", "'unsafe-inline'", '*.fontawesome.com', 'fonts.googleapis.com'],
+        connectSrc: ["'self'", '*.fontawesome.com'],
+        fontSrc: ["'self'", 'data:', 'https:', '*.fontawesome.com'],
+        imgSrc: ["'self'", 'data:', 'https:'],
       },
     },
   },
@@ -26,17 +20,7 @@ const satelliteOptions = {
 const service = new Satellite(satelliteOptions);
 
 // Static web assets can be cached for a long time
-service.router.use(serveStatic('public', { immutable: true, maxAge: '1y' }));
-
-service.router.get('/status', (req, res) => {
-  check()
-    .then((status) => {
-      // This status response shouldn't be cached (we need current status info)
-      res.set('Cache-Control', 'no-store, max-age=0');
-      return res.json(status);
-    })
-    .catch((err) => res.status(500).json({ error: err.message }));
-});
+service.router.use('/', static(path.join(__dirname, '../public')));
 
 const port = parseInt(process.env.STATUS_PORT || 1111, 10);
 service.start(port);
