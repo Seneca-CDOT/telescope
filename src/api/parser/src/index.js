89b0dc8ea62af3e52e95b00cdd35781a2783ba51
David Humphrey <david.andrew.humphrey@gmail.com>
2021-11-02 22:04:52 -0400

    Disable parser service until we fix #2111

--- src/api/parser/src/index.js
+++ src/api/parser/src/index.js
@@ -1,5 +1,6 @@
 const { Satellite, isAuthenticated, isAuthorized } = require('@senecacdot/satellite');
-const { router } = require('bull-board');
+// TODO: https://github.com/Seneca-CDOT/telescope/issues/2345
+// const { router } = require('bull-board');
 // const startParserQueue = require('./parser-queue');
 
 const service = new Satellite();
@@ -7,12 +8,13 @@ const service = new Satellite();
 // We're commenting startParserQueue() for now so it doesn't clash with our current backend, will uncomment when tests go in and we remove the backend.
 // startParserQueue();
 
+// Re-enable as part of // TODO: https://github.com/Seneca-CDOT/telescope/issues/2345
 // Only authenticated Telescope users, or other services, can hit this route
-service.router.get(
-  '/',
-  isAuthenticated(),
-  isAuthorized((req, user) => user.roles.includes('telescope') || user.roles.includes('service')),
-  router
-);
+// service.router.get(
+//   '/',
+//   isAuthenticated(),
+//   isAuthorized((req, user) => user.roles.includes('telescope') || user.roles.includes('service')),
+//   router
+// );
 
 module.exports = service;
