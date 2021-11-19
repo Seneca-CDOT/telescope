41082583a2fa7ff1905783ed5e410d9f597c3f0f
Hung <hnguyen97@myseneca.ca>
2021-11-19 17:46:27 -0500

    Revert "Merge branch 'master' into postsCount"
    This reverts commit 92086112a31692d93219a1975c2c2d87f4ba0426, reversing
    changes made to dec2f4724b0a362e436f3960ca19d20bd8cf682b.

--- src/api/status/src/server.js
+++ src/api/status/src/server.js
@@ -1,7 +1,6 @@
 const { Satellite } = require('@senecacdot/satellite');
 const { static } = require('express');
 const path = require('path');
-const host = process.env.API_HOST || 'localhost';
 
 const satelliteOptions = {
   helmet: {
@@ -19,7 +18,7 @@ const satelliteOptions = {
         connectSrc: [
           "'self'",
           '*.fontawesome.com',
-          `${host.replace(/(^\w+:|^)\/\//, '')}:4000`,
+          `${process.env.API_HOST.replace(/(^\w+:|^)\/\//, '')}:4000`,
           '*.github.com',
         ],
         fontSrc: ["'self'", 'data:', 'https:', '*.fontawesome.com'],
@@ -31,16 +30,8 @@ const satelliteOptions = {
 
 const service = new Satellite(satelliteOptions);
 
-// If a PATH_PREFIX is defined, serve our static content there, and redirect / -> PATH_PREFIX.
-// We do this in development to extra path routing that Traefik adds in production (e.g., /v1/status/*)
-if (process.env.PATH_PREFIX) {
-  service.router.use(process.env.PATH_PREFIX, static(path.join(__dirname, '../public')));
-  service.router.get('/', (req, res) => {
-    res.redirect(process.env.PATH_PREFIX);
-  });
-} else {
-  service.router.use('/', static(path.join(__dirname, '../public')));
-}
+// Static web assets can be cached for a long time
+service.router.use('/', static(path.join(__dirname, '../public')));
 
 const port = parseInt(process.env.STATUS_PORT || 1111, 10);
 service.start(port);
