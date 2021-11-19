28972de097fe892f6aad8745554bc54aa3600b8e
tuanthanh2067@gmail.com <tuanthanh2067@gmail.com>
2021-11-18 20:01:22 -0500

    use URL location origin

--- src/api/status/public/js/api.js
+++
@@ -1,1 +0,0 @@
-export const fetchFeeds = () => fetch(`https://dev.api.telescope.cdot.systems/v1/posts/feeds`);
--- src/api/status/public/js/feed/index.js
+++ src/api/status/public/js/feed/index.js
@@ -1,12 +1,18 @@
-const { fetchFeeds } = require('../api');
+const apiHost = new URL(location).origin;
 
-export async function getFeedCount() {
+async function getFeedCount() {
   try {
-    const data = await fetchFeeds();
-    const feedCount = data.headers.get('X-Total-Count');
+    const data = await fetch(`${apiHost}/v1/posts/feeds`, {
+      method: 'HEAD',
+    });
+    const feedCount = data.headers.get('x-total-count');
     const feedCountElement = document.getElementById('feeds-count');
-    feedCountElement.innerText = feedCount;
+    feedCountElement.innerText = feedCount ?? 'N/A';
   } catch (err) {
     console.error(err);
   }
 }
+
+window.onload = () => {
+  getFeedCount();
+};
