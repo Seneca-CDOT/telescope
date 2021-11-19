66e225e8d7d6bf3d44f8bc2db5567ec6c6143393
Sirinoks <shinigamoo@gmail.com>
2021-11-18 23:10:36 -0500

    Redirect fixed

--- src/api/status/src/server.js
+++ src/api/status/src/server.js
@@ -1,6 +1,7 @@
 const { Satellite } = require('@senecacdot/satellite');
 const { static } = require('express');
 const path = require('path');
+const host = process.env.API_HOST || 'localhost';
 
 const satelliteOptions = {
   helmet: {
@@ -18,7 +19,7 @@ const satelliteOptions = {
         connectSrc: [
           "'self'",
           '*.fontawesome.com',
-          `${process.env.API_HOST.replace(/(^\w+:|^)\/\//, '')}:4000`,
+          `${host.replace(/(^\w+:|^)\/\//, '')}:4000`,
           '*.github.com',
         ],
         fontSrc: ["'self'", 'data:', 'https:', '*.fontawesome.com'],
@@ -31,11 +32,15 @@ const satelliteOptions = {
 const service = new Satellite(satelliteOptions);
 
 // For local dev, allow specifying a different path prefix (i.e., to mimic Traefik routing with our <base> tag)
-const staticPathPrefix = process.env.PATH_PREFIX || '/';
-service.router.use(staticPathPrefix, static(path.join(__dirname, '../public')));
 
 // Static web assets can be cached for a long time
-service.router.use('/', static(path.join(__dirname, '../public')));
+service.router.use(process.env.PATH_PREFIX || '/', static(path.join(__dirname, '../public')));
+
+if (process.env.PATH_PREFIX !== '/') {
+  service.router.get('/', (req, res) => {
+    res.redirect(process.env.PATH_PREFIX);
+  });
+}
 
 const port = parseInt(process.env.STATUS_PORT || 1111, 10);
 service.start(port);
