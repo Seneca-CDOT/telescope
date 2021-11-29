27f625ff0d91b7d49b3116a086f2e0879953cbee
Duke Manh <51073515+DukeManh@users.noreply.github.com>
2021-11-29 17:52:05 -0500

    Update to NextJS 12 (#2513)

--- src/web/src/components/AboutFooter.test.tsx
+++ src/web/src/components/AboutFooter.test.tsx
@@ -1,8 +1,9 @@
 import { render, cleanup } from '@testing-library/react';
-import AboutFooter from './AboutFooter';
 
 afterEach(cleanup);
 
+// Auto-hoisting of jest.mock calls is currently not working https://github.com/vercel/next.js/discussions/31152
+// So we have to import AboutFooter after jest.mock
 jest.mock(
   './GitHubContributorCard',
   () =>
@@ -12,6 +13,8 @@ jest.mock(
 );
 
 it('renders correctly', () => {
+  // eslint-disable-next-line global-require
+  const AboutFooter = require('./AboutFooter').default;
   const { asFragment } = render(<AboutFooter />);
   expect(asFragment()).toMatchSnapshot();
 });
