ff02418ca8e15e62e9e1fcd3c4c8c4e04f713d83
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-12-14 21:52:00 -0500

    added advanced search query for elasticsearch
    added advanced route with params
    
    separated query validation based on route pathname
    
    cleaned up the files
    
    added validation
    
    cleaned up validator
    
    added oneof in declarations
    
    fixed validation
    
    changed query fields and added if statements for options passed
    
    changed description for advanced search
    
    fixed date format to correct format

--- src/api/search/src/routes/query.js
+++ src/api/search/src/routes/query.js
@@ -1,6 +1,6 @@
 const { Router, createError } = require('@senecacdot/satellite');
 const { validateQuery } = require('../bin/validation');
-const search = require('../search');
+const { search, advancedSearch } = require('../search');
 
 const router = Router();
 
@@ -8,6 +8,15 @@ router.get('/', validateQuery, async (req, res, next) => {
   try {
     const { text, filter, page, perPage } = req.query;
     res.send(await search(text, filter, page, perPage));
+  } catch (error) {
+    next(createError(503, error));
+  }
+});
+
+// route for advanced
+router.get('/advanced', validateQuery, async (req, res, next) => {
+  try {
+    res.send(await advancedSearch(req.query));
   } catch (error) {
     next(createError(503, error));
   }
