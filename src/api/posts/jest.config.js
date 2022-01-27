89c1bf0cc83f09e97d61b17d868fa5eae4daf3ad
rclee91 <32626950+rclee91@users.noreply.github.com>
2022-01-27 16:13:29 -0500

    Adding setupFiles for Post test config

--- src/api/posts/jest.config.js
+++ src/api/posts/jest.config.js
@@ -2,6 +2,7 @@ const baseConfig = require('../../../jest.config.base');
 
 module.exports = {
   ...baseConfig,
+  setupFiles: ['<rootDir>/src/api/posts/jest.setup.js'],
   rootDir: '../../..',
   testMatch: ['<rootDir>/src/api/posts/test/*.test.js'],
   collectCoverageFrom: ['<rootDir>/src.api/posts/src/**/*.js'],
