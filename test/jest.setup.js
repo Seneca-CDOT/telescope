e244d2172907acc1251ed002668e65b551fcddd0
rclee91 <32626950+rclee91@users.noreply.github.com>
2022-01-29 11:02:53 -0500

    Moving test config variables to test/jest.setup.js - Removed cross-env 7.0.3 dev dependency - Added pnpm-lock.yaml to .gitignore

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -2,3 +2,10 @@ const fetch = require('jest-fetch-mock');
 
 // Mock fetch for the Telescope 1.0 back-end tests
 jest.setMock('node-fetch', fetch);
+
+// Config variables for testing
+process.env = {
+  ...process.env,
+  NODE_ENV: 'test',
+  FEED_URL_INTERVAL_MS: '200',
+};
