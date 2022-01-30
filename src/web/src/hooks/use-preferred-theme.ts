ef6a4c3e709873c943ed23fabee2099075f35614
DukeManh <manhducdkcb@gmail.com>
2022-01-30 12:00:16 -0500

    Add a default state to useMedia hook

--- src/web/src/hooks/use-preferred-theme.ts
+++ src/web/src/hooks/use-preferred-theme.ts
@@ -5,7 +5,7 @@ import { useLocalStorage, useMedia } from 'react-use';
  * loads of the app, and initial logic to get the browser's preferred colour.
  */
 export default function usePreferredTheme() {
-  const isDarkThemePreferred = useMedia('(prefers-color-scheme: dark)');
+  const isDarkThemePreferred = useMedia('(prefers-color-scheme: dark)', false);
   const [preferredTheme, setPreferredTheme] = useLocalStorage(
     'preference:theme',
     isDarkThemePreferred ? 'dark' : 'light'
