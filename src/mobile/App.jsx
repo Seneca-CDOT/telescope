98fe3c01689901820a62d48d446310be20aaeb1c
tpmai22 <thienphuoc.0108@gmail.com>
2022-02-03 23:07:01 -0500

    Splash screen background color,add safe padding

--- src/mobile/App.jsx
+++ src/mobile/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,9 +13,8 @@ const styles = StyleSheet.create({
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Telescope mobile app!</Text>
-      <StatusBar />
-    </View>
+    </SafeAreaView>
   );
 }
