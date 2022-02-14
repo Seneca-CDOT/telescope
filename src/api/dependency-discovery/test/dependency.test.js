16376aeb9de2c71d109c84aa41a082550e73b4e5
Tue Nguyen <tnguyen187@myseneca.ca>
2022-02-14 10:49:33 -0500

    Create test for dependency-discovery service

---
+++ src/api/dependency-discovery/test/dependency.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const { app } = require('../src');
+
+const depsList = [
+  '@apidevtools/json-schema-ref-parser',
+  '@babel/code-frame',
+  '@babel/compat-data',
+  '@babel/core", "@babel/generator',
+  '@babel/helper-annotate-as-pure',
+  '@babel/helper-builder-binary-assignment-operator-visitor',
+  '@babel/helper-compilation-targets',
+  '@babel/helper-create-class-features-plugin',
+  '@babel/helper-create-regexp-features-plugin',
+];
+
+jest.mock('../src/dependency-list', () => {
+  return jest.fn().mockImplementation(() => {
+    return Promise.resolve(depsList);
+  });
+});
+
+describe('GET /projects', () => {
+  test('Should return 200 and an array of dependencies', async () => {
+    const res = await request(app).get('/projects');
+    expect(res.status).toBe(200);
+    expect(typeof res.body).toEqual('object');
+    expect(res.body.length).toBe(depsList.length);
+  });
+});
