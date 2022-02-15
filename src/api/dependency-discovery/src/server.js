402a696b1c8342c9bf61d4fab9b1d86611fe80cc
Gerardo Arriaga Rendon <gerardo.arriaga13@gmail.com>
2022-02-15 10:48:33 -0500

    Dockerize dependency-discovery

--- src/api/dependency-discovery/src/server.js
+++ src/api/dependency-discovery/src/server.js
@@ -1,5 +1,5 @@
 const service = require('.');
 
-const port = parseInt(process.env.DEP_DISCOVERY_PORT || 10500, 10);
+const port = parseInt(process.env.DEPENDENCY_DISCOVERY_PORT || 10500, 10);
 
 service.start(port);
