e1d7c96eb998ffca900bf046f043c978fffb774f
Abdulbasid Guled <guled.basid@gmail.com>
2022-02-15 16:36:57 -0500

    Default latest commit url to master branch in development (#2856)

--- src/backend/web/routes/health.js
+++ src/backend/web/routes/health.js
@@ -4,7 +4,7 @@ const healthcheck = require('express-healthcheck');
 // We want the current version from package.json
 const { version } = require('../../../../package.json');
 // As well as the current git sha
-const sha = process.env.GIT_COMMIT || 'undefined';
+const sha = process.env.GIT_COMMIT || 'master';
 // And a github URL to this commit
 const gitHubUrl = `https://github.com/Seneca-CDOT/telescope/commit/${sha}`;
 
