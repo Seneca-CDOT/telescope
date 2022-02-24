9da64616a1b17878e718e833897140a6e4dd8538
Cindy Le <cindyledev@gmail.com>
2022-02-23 21:57:04 -0500

    update aws-ip.sh script to work with current environment

--- tools/aws-ip.sh
+++ tools/aws-ip.sh
@@ -1,8 +1,11 @@
 ipv4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
 sed -r \
-  -e  "s@(.+=)http://localhost:8000(/[^ ]*)*@\1http://$ipv4:3000\2@g" \
-  -e  "s@(.+=)http://localhost:3000(/[^ ]*)*@\1http://$ipv4:8000\2@g" \
+  -e  "s@(.+=)http://localhost:8000(/[^ ]*)*@\1http://$ipv4:8000\2@g" \
+  -e  "s@(.+=)http://localhost:3000(/[^ ]*)*@\1http://$ipv4:3000\2@g" \
+  -e  "s@(.+=)http://localhost:8081(/[^ ]*)*@\1http://$ipv4:8081\2@g" \
+  -e  "s@(.+=)http://kong:8000(/[^ ]*)*@\1http://$ipv4:8911\2@g" \
   -e  "s@(.+=)http://localhost([^:]*)@\1http://$ipv4:8443\2@g" \
+  -e  "s@(.+=)localhost([^:]*)@\1$ipv4\2@g" \
   -e  "s@development\.yml@gitpod\.yml@" \
   config/env.development > .env
