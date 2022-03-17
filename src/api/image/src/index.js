c8ed678d2ae413f4244add9a2f6b55cf77336fb9
Josue <manekenpix@fastmail.com>
2022-03-16 20:22:11 -0400

    fix: change the cross origin resource policy in the image service to cross-origin

--- src/api/image/src/index.js
+++ src/api/image/src/index.js
@@ -3,7 +3,13 @@ const { Satellite } = require('@senecacdot/satellite');
 const image = require('./routes/image');
 const gallery = require('./routes/gallery');
 
-const service = new Satellite();
+const options = {
+  helmet: {
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+  },
+};
+
+const service = new Satellite(options);
 
 service.router.use('/gallery', gallery);
 service.router.use('/', image);
