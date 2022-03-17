387f410fb9d9cccab05846aca65f9257bcf4da54
DukeManh <manhducdkcb@gmail.com>
2022-03-16 20:10:14 -0400

    Use Kong http port

--- tools/aws-ip.sh
+++ tools/aws-ip.sh
@@ -4,7 +4,7 @@ sed -r \
   -e  "s@(.+=)http://localhost:8000(/[^ ]*)*@\1http://$ipv4:8000\2@g" \
   -e  "s@(.+=)http://localhost:3000(/[^ ]*)*@\1http://$ipv4:3000\2@g" \
   -e  "s@(.+=)http://localhost:8081(/[^ ]*)*@\1http://$ipv4:8081\2@g" \
-  -e  "s@(.+=)http://kong:8000(/[^ ]*)*@\1http://$ipv4:8911\2@g" \
+  -e  "s@(.+=)http://localhost:8911(/[^ ]*)*@\1http://$ipv4:8911\2@g" \
   -e  "s@(.+=)http://localhost([^:]*)@\1http://$ipv4:8443\2@g" \
   -e  "s@(.+=)localhost([^:]*)@\1$ipv4\2@g" \
   -e  "s@development\.yml@gitpod\.yml@" \
