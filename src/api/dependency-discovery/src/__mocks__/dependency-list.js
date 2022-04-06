30ea1df7f3200e08e9f36580dab69ff83d184c1f
Gerardo Arriaga Rendon <gerardo.arriaga13@gmail.com>
2022-04-06 13:26:18 -0400

    Add initial tests for /github/:project

--- src/api/dependency-discovery/src/__mocks__/dependency-list.js
+++ src/api/dependency-discovery/src/__mocks__/dependency-list.js
@@ -1,10 +1,23 @@
 let depsList = [];
+let depsGitHubIssuesList = {};
 
 module.exports = {
   __setMockDepList: (newDepsList) => {
     depsList = newDepsList;
   },
+  __setMockDepsGitHubIssuesList: (newDepsGitHubIssuesList) => {
+    depsGitHubIssuesList = newDepsGitHubIssuesList;
+  },
   getDependencyList: jest.fn().mockImplementation(() => {
     return Promise.resolve(depsList);
+  }),
+  isPackageDependency: jest.fn().mockImplementation((dependencyName) => {
+    return Promise.resolve(depsList.includes(dependencyName));
+  }),
+  getGitHubIssues: jest.fn().mockImplementation((dependencyName) => {
+    if (depsGitHubIssuesList[dependencyName]) {
+      return Promise.resolve(depsGitHubIssuesList[dependencyName]);
+    }
+    return Promise.resolve([]);
   }),
 };
