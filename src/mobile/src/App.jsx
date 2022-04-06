1efce84a53aa03013540a9b8934fb36ab9a864dc
tpmai <thienphuoc.0108@gmail.com>
2022-04-05 20:16:38 -0400

    Using supabase to share student quotes between Telescope web and mobile app (#3346)

--- src/mobile/src/App.jsx
+++ src/mobile/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-native-url-polyfill/auto';
 import { NavigationContainer } from '@react-navigation/native';
 import { registerRootComponent } from 'expo';
 
