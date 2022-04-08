733a30ae775591e850a5f7bcf0ea6369cb110bba
Gerardo Arriaga Rendon <gerardo.arriaga13@gmail.com>
2022-04-08 02:45:12 -0400

    Add initial tests for /projects/:project

--- src/api/dependency-discovery/src/__mocks__/dependency-list.js
+++ src/api/dependency-discovery/src/__mocks__/dependency-list.js
@@ -1,9 +1,13 @@
 let depsList = [];
+let depsNpmPackageList = {};
 let depsGitHubIssuesList = {};
 
 module.exports = {
   __setMockDepList: (newDepsList) => {
     depsList = newDepsList;
+  },
+  __setMockDepsNpmPackageList: (newDepsNpmPackageList) => {
+    depsNpmPackageList = newDepsNpmPackageList;
   },
   __setMockDepsGitHubIssuesList: (newDepsGitHubIssuesList) => {
     depsGitHubIssuesList = newDepsGitHubIssuesList;
@@ -13,6 +17,12 @@ module.exports = {
   }),
   isPackageDependency: jest.fn().mockImplementation((dependencyName) => {
     return Promise.resolve(depsList.includes(dependencyName));
+  }),
+  getNpmPackageInfo: jest.fn().mockImplementation((dependencyName) => {
+    if (depsNpmPackageList[dependencyName]) {
+      return Promise.resolve(depsNpmPackageList[dependencyName]);
+    }
+    return Promise.reject(new Error('NPM Package cannot be fetched.'));
   }),
   getGitHubIssues: jest.fn().mockImplementation((dependencyName) => {
     if (depsGitHubIssuesList[dependencyName]) {
--- src/api/dependency-discovery/test/dependency.test.js
+++ src/api/dependency-discovery/test/dependency.test.js
@@ -14,6 +14,25 @@ const depsList = [
   'faker-package',
 ];
 
+const depsNpmPackageList = {
+  '@apidevtools/json-schema-ref-parser': {
+    id: '@apidevtools/json-schema-ref-parser',
+    license: 'MIT',
+    gitRepository: {
+      type: 'git',
+      url: 'https://github.com/algolia/algoliasearch-client-javascript',
+    },
+  },
+  'faker-package': {
+    id: 'faker-package',
+    license: 'Apache License 2.0',
+    gitRepository: {
+      type: 'git',
+      url: 'https://github.com/faker-org/faker',
+    },
+  },
+};
+
 const depsGitHubIssuesList = {
   '@babel/code-frame': [
     {
@@ -40,7 +59,11 @@ const depsGitHubIssuesList = {
 };
 
 jest.mock('../src/dependency-list');
-const { __setMockDepList, __setMockDepsGitHubIssuesList } = require('../src/dependency-list');
+const {
+  __setMockDepList,
+  __setMockDepsNpmPackageList,
+  __setMockDepsGitHubIssuesList,
+} = require('../src/dependency-list');
 
 describe('GET /projects', () => {
   beforeEach(() => {
@@ -52,6 +75,41 @@ describe('GET /projects', () => {
     expect(res.status).toBe(200);
     expect(typeof res.body).toEqual('object');
     expect(res.body.length).toBe(depsList.length);
+  });
+});
+
+describe('GET /projects/:project', () => {
+  beforeEach(() => {
+    __setMockDepList(depsList);
+    __setMockDepsNpmPackageList(depsNpmPackageList);
+  });
+
+  test('Should return 200 and an object with the npm package info', async () => {
+    const dependencyName = 'faker-package';
+    const npmPackageInfo = depsNpmPackageList[dependencyName];
+    const res = await request(app).get(`/projects/${dependencyName}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(npmPackageInfo);
+  });
+
+  test('Should return 200 and an object with the npm package info if dependency name has namespace', async () => {
+    const dependencyName = '@apidevtools/json-schema-ref-parser';
+    const npmPackageInfo = depsNpmPackageList[dependencyName];
+    const res = await request(app).get(`/projects/${dependencyName}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(npmPackageInfo);
+  });
+
+  test('Should return 404 if dependency does not exist', async () => {
+    const dependencyName = 'this-is-not-a-dependency';
+    const res = await request(app).get(`/projects/${dependencyName}`);
+    expect(res.status).toBe(404);
+  });
+
+  test("Should return 500 if dependency couldn't fetch npm package", async () => {
+    const dependencyName = '@babel/code-frame';
+    const res = await request(app).get(`/projects/${dependencyName}`);
+    expect(res.status).toBe(500);
   });
 });
 
