73118b0f8b619b9c746c751c1a53c755bd3d7d35
David Humphrey <david.andrew.humphrey@gmail.com>
2022-04-15 11:13:34 -0400

    Make parser arg optional, rename main function for github-url-parser

--- src/github-url-parser/test/index.test.js
+++ src/github-url-parser/test/index.test.js
@@ -1,9 +1,9 @@
-const { githubUrlParser } = require('../dist/main');
+const { parseGitHubUrls } = require('../dist/main');
 const { parser } = require('./util');
 
-describe('githubUrlParser', () => {
+describe('parseGitHubUrls', () => {
   test('Should throw if missing DOMParser', () => {
-    expect(() => githubUrlParser('abc')).toThrow();
+    expect(() => parseGitHubUrls('abc')).toThrow();
   });
 
   test('Should return expected GitHub Info for a string of HTML', () => {
@@ -12,7 +12,7 @@ describe('githubUrlParser', () => {
       <a href="https://github.com/Seneca-CDOT/telescope/issues/3452">Telescope Issue #3452</a>
       <a href="https://github.com/Seneca-CDOT/telescope/pull/3455/commits/82f16594caa2d81442accd2a891a3d514f2ff39b">Telescope PR commit 82f1659</a>
       `;
-    expect(githubUrlParser(html, parser())).toEqual({
+    expect(parseGitHubUrls(html, parser())).toEqual({
       users: ['Seneca-CDOT'],
       repos: ['Seneca-CDOT/telescope'],
       commits: [
