a4458358fc630de4abda5c62299fd4d808e0043d
Duc Manh <51073515+DukeManh@users.noreply.github.com>
2022-05-01 23:27:12 -0400

    Wait on SAML login to be ready (#3569)
    * Wait for SAML and Postgres on CI

--- src/api/sso/jest.config.e2e.js
+++ src/api/sso/jest.config.e2e.js
@@ -1,34 +1,14 @@
-// eslint-disable-next-line
-const { waitForPostgres } = require('@jcoreio/wait-for-postgres');
-
 const baseConfig = require('../../../jest.config.base');
 
-module.exports = async () => {
-  try {
-    console.log('Waiting for Supabase postgres container to become ready...');
-    await waitForPostgres({
-      host: 'localhost',
-      user: 'postgres',
-      password: process.env.POSTGRES_PASSWORD,
-      database: 'postgres',
-      timeout: 60 * 1000,
-    });
-  } catch (err) {
-    console.warn('Unable to connect to postgres container');
-    throw err;
-  }
-
-  console.log('Supabase postgres container ready.');
-  return {
-    ...baseConfig,
-    rootDir: '../../..',
-    testMatch: ['<rootDir>/src/api/sso/test/e2e/**/*.test.js'],
-    collectCoverageFrom: ['<rootDir>/src/api/sso/src/**/*.js'],
-    // We use jest-playwright to do browser tests, https://github.com/playwright-community/jest-playwright
-    // See the jest-playwright config in the Telescope root ./jest-playwright.config.js
-    preset: 'jest-playwright-preset',
-    // Try reducing the number of tests that can run at once down to 1
-    maxConcurrency: 1,
-    maxWorkers: 1,
-  };
-};
+module.exports = () => ({
+  ...baseConfig,
+  rootDir: '../../..',
+  testMatch: ['<rootDir>/src/api/sso/test/e2e/**/*.test.js'],
+  collectCoverageFrom: ['<rootDir>/src/api/sso/src/**/*.js'],
+  // We use jest-playwright to do browser tests, https://github.com/playwright-community/jest-playwright
+  // See the jest-playwright config in the Telescope root ./jest-playwright.config.js
+  preset: 'jest-playwright-preset',
+  // Try reducing the number of tests that can run at once down to 1
+  maxConcurrency: 1,
+  maxWorkers: 1,
+});
