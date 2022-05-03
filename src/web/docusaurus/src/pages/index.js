5b6d895a4bf83bca8e238f3bcc860f9c37938de4
416rehman <github@ahmadz.ai>
2022-05-02 21:44:30 -0400

    eslint

--- src/web/docusaurus/src/pages/index.js
+++ src/web/docusaurus/src/pages/index.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from 'react';
-import clsx from 'clsx';
+import React from 'react';
+// import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
+// import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+// import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="./overview">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+// function HomepageHeader() {
+//   const { siteConfig } = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link className="button button--secondary button--lg" to="./overview">
+//             Docusaurus Tutorial - 5min ⏱️
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -31,7 +31,6 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      {/*<HomepageHeader />*/}
       <main>
         <HomepageFeatures />
       </main>
