9d3ee0a11332e5c4b313da15b5d5ce72d77d0d4d
Max N <smth@gmail.com>
2022-10-04 22:07:26 +0700

    relative path link added

--- src/web/docusaurus/docusaurus.config.js
+++ src/web/docusaurus/docusaurus.config.js
@@ -120,7 +120,7 @@ const config = async () => {
               items: [
                 {
                   label: 'Documentation',
-                  href: 'https://dev.telescope.cdot.systems/docs/overview/',
+                  href: '/docs/overview',
                 },
                 {
                   label: 'Slack',
