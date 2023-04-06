640f9b00180e6fca8ff665f760dadce9ae99c3f6
Josue <manekenpix@fastmail.com>
2023-04-06 18:48:32 -0400

    remove telescope mobile app

--- src/mobile/src/utils/createUrl.js
+++
@@ -1,9 +0,0 @@
-import Constants from 'expo-constants';
-
-const POST_URL = Constants.manifest.extra.postsUrl;
-
-const createPostUrl = (perPage, page) => {
-  return `${POST_URL}?per_page=${perPage}&page=${page}`;
-};
-
-export default createPostUrl;
